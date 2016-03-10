c2353f9c2df2102776532b1e952a95107937c65b
wknet123 <wknet123@gmail.com>
2016-03-10 18:05:12 +0800

    added configure_swagger.md and shell script.

---
+++ docs/prepare-swagger.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+SERVER_IP=10.117.170.65
+set -e
+echo "Doing some clean up..."
+rm -f *.tar.gz
+echo "Downloading Swagger UI release package..."
+wget https://github.com/swagger-api/swagger-ui/archive/v2.1.4.tar.gz -O swagger.tar.gz
+echo "Untarring Swagger UI package to the static file path..."
+tar -C ../static/vendors -zxf swagger.tar.gz swagger-ui-2.1.4/dist
+echo "Executing some processes..."
+sed -i 's/http:\/\/petstore\.swagger\.io\/v2\/swagger\.json/http:\/\/'$SERVER_IP'\/static\/resources\/yaml\/swagger\.yaml/g' \
+ ../static/vendors/swagger-ui-2.1.4/dist/index.html
+mkdir -p ../static/resources/yaml
+cp swagger.yaml ../static/resources/yaml
+sed -i 's/host: localhost/host: '$SERVER_IP'/g' ../static/resources/yaml/swagger.yaml
+echo "Finish preparation for the Swagger UI."
