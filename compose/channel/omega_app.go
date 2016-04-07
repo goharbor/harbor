b75da6987a3e5fd05193ddaf0d694b936e63cff3
kevin xu <cming.xu@gmail.com>
2016-04-07 19:25:04 +0800

    [MOD] http client for omega app output

--- compose/channel/omega_app.go
+++ compose/channel/omega_app.go
@@ -10,7 +10,10 @@ import (
 	"strings"
 )
 
-const AUTH_HEADER = "ChannelConfigorization"
+const AUTH_HEADER = "Authorization"
+const (
+	OMEGA_APP_CREATE_API = ""
+)
 
 type OmegaClient struct {
 	HttpClient    *http.Client
@@ -54,7 +57,7 @@ func (output *OmegaAppOutput) get(path string, values url.Values) error {
 	log.Println("GET: " + path)
 	log.Println("params payload: " + values.Encode())
 
-	req, _ := http.NewRequest("GET", path, nil)
+	req, _ := http.NewRequest("GET", output.expandPath(path, values), nil)
 	output._auth(req)
 	resp, err := output.Client.HttpClient.Do(req)
 
@@ -73,7 +76,7 @@ func (output *OmegaAppOutput) post(path string, json string) error {
 	log.Println("POST: " + path)
 	log.Println("JSON payload: " + json)
 
-	req, _ := http.NewRequest("POST", path, strings.NewReader(json))
+	req, _ := http.NewRequest("POST", output.expandPath(path, url.Values{}), strings.NewReader(json))
 	output._auth(req)
 	resp, err := output.Client.HttpClient.Do(req)
 
@@ -86,6 +89,21 @@ func (output *OmegaAppOutput) post(path string, json string) error {
 	}
 
 	return nil
+}
+
+func (output *OmegaAppOutput) expandPath(path string, values url.Values) string {
+	host := output.Client.ChannelConfig.AppApiUrl
+	if strings.HasSuffix(host, "/") {
+		host = strings.TrimRight(host, "/")
+	}
+
+	if strings.HasPrefix(path, "/") {
+		path = strings.TrimLeft(path, "/")
+	}
+
+	query := values.Encode()
+
+	return host + "/" + path + "?" + query
 }
 
 func (output *OmegaAppOutput) _auth(req *http.Request) {
