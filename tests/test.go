a83199e21620ff09df20f9e158d51b2ff3ceece0
yhua <a@b.com>
2016-05-18 15:22:12 +0800

    test go

---
+++ tests/test.go
@@ -0,0 +1,34 @@
+// Fetch prints the content found at a URL.
+package main
+
+import (
+    "fmt"
+    "io/ioutil"
+    "net/http"
+    "os"
+    "strings"
+)
+
+func main() {
+    for _, url := range os.Args[1:] {
+        resp, err := http.Get(url)
+        if err != nil {
+            fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+            os.Exit(1)
+        }
+        b, err := ioutil.ReadAll(resp.Body)
+        resp.Body.Close()
+        if err != nil {
+            fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+            os.Exit(1)
+        }
+//        fmt.Printf("%s", b)
+        if strings.Contains(string(b), "Harborxxx") {
+          fmt.Printf("sucess!\n")
+        } else {
+            os.Exit(1)
+        }
+
+    }
+}
+
