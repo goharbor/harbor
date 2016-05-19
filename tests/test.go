ab53e8ce569ea2e9a75b39f1c3596253b7703910
yhua <a@b.com>
2016-05-19 19:31:51 +0800

    add hostcfg.sh(8)

--- tests/test.go
+++ tests/test.go
@@ -24,8 +24,8 @@ func main() {
             fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
             os.Exit(1)
         }
-        fmt.Printf("%s", b)
-        if strings.Contains(string(b), "html") {
+//        fmt.Printf("%s", b)
+        if strings.Contains(string(b), "harbor") {
           fmt.Printf("sucess!\n")
         } else {
             os.Exit(1)
