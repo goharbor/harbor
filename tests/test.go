12934ffe6d7675845562bd965d1b9c14b6ecab61
yhua <a@b.com>
2016-05-19 19:18:02 +0800

    add hostcfg.sh(7)

--- tests/test.go
+++ tests/test.go
@@ -11,7 +11,7 @@ import (
 )
 
 func main() {
-    time.sleep(60*time.second)
+    time.Sleep(60*time.Second)
     for _, url := range os.Args[1:] {
         resp, err := http.Get(url)
         if err != nil {
