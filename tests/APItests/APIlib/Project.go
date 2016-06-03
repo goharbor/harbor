a70152d67405b90a35c62b06505a60992ed69d62
yhua <a@b.com>
2016-06-03 13:00:08 +0800

    test for import

--- tests/APItests/APIlib/Project.go
+++ tests/APItests/APIlib/Project.go
@@ -1,6 +1,15 @@
 package HarborAPI
 
-type Password struct {
-	OldPassword string `json:"old_password,omitempty"`
-	NewPassword string `json:"new_password,omitempty"`
+import ()
+
+type Project struct {
+	ProjectId    int32  `json:"id,omitempty"`
+	OwnerId      int32  `json:"owner_id,omitempty"`
+	ProjectName  string `json:"project_name,omitempty"`
+	CreationTime string `json:"creation_time,omitempty"`
+	Deleted      int32  `json:"deleted,omitempty"`
+	UserId       int32  `json:"user_id,omitempty"`
+	OwnerName    string `json:"owner_name,omitempty"`
+	Public       bool   `json:"public,omitempty"`
+	Togglable    bool   `json:"togglable,omitempty"`
 }
