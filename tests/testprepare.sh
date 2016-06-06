6a00b571ae54c9f7b3438b6910475f3e3298ab36
yhua <a@b.com>
2016-06-06 18:09:37 +0800

    test for API test prepare(2)

--- tests/testprepare.sh
+++ tests/testprepare.sh
@@ -2,7 +2,7 @@ IP=`ip addr s eth0 |grep "inet "|awk '{print $2}' |awk -F "/" '{print $1}'`
 #echo $IP
 docker pull hello-world
 docker pull docker
-docker login -u admin -p Harbor12345 $IP
+#docker login -u admin -p Harbor12345 $IP
 
 docker tag hello-world $IP/library/hello-world
 docker push $IP/library/hello-world
