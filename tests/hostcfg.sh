f3ed963c75ac0f19419e9284988ba97689527c96
yhua123 <he_feng_yun@126.com>
2016-06-15 12:50:00 +0800

    Update hostcfg.sh
    sudo

--- tests/hostcfg.sh
+++ tests/hostcfg.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 IP=`ip addr s eth0 |grep "inet "|awk '{print $2}' |awk -F "/" '{print $1}'`
-#echo $IP
-sed "s/reg.mydomain.com/$IP/" -i Deploy/harbor.cfg
+echo $IP
+sudo sed "s/reg.mydomain.com/$IP/" -i Deploy/harbor.cfg
 cat Deploy/harbor.cfg
