7fbdf4e01800bed6d7065f58b9d35d2f663b3b90
yhua123 <he_feng_yun@126.com>
2016-06-15 12:57:04 +0800

    Update hostcfg.sh
    change reg.mydomain.com to reg.mydomain.org

--- tests/hostcfg.sh
+++ tests/hostcfg.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 IP=`ip addr s eth0 |grep "inet "|awk '{print $2}' |awk -F "/" '{print $1}'`
-echo $IP
-sudo sed "s/reg.mydomain.com/$IP/" -i Deploy/harbor.cfg
-cat Deploy/harbor.cfg
+#echo $IP
+sudo sed "s/reg.mydomain.org/$IP/" -i Deploy/harbor.cfg
