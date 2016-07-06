b5c7729cd90821d478a022bcc106daa240d05943
riverxing <1129059033@qq.com>
2016-07-06 16:43:56 +0800

    add new files

---
+++ git/config.go
@@ -0,0 +1,31 @@
+package git
+
+import (
+	"os"
+)
+
+type Config struct {
+	Workspace   string
+	Project     string
+	Imagename   string
+	Gituri      string
+	Storemethod string
+	Branch      string
+}
+
+var config Config
+
+func Pairs() Config {
+	return config
+}
+func InitParam() {
+	initConfig(os.Getenv("HARBOR_GIT_WORKSPACE"), os.Getenv("HARBOR_GIT_PROJECT"), os.Getenv("HARBOR_GIT_IMAGENAME"), os.Getenv("HARBOR_GIT_GITURI"), os.Getenv("HARBOR_GIT_STOREMETHOD"), os.Getenv("HARBOR_GIT_BRANCH"))
+}
+func initConfig(workspace, project, imagename, gituri, storemethod, branch string) {
+	config.Workspace = workspace
+	config.Project = project
+	config.Imagename = imagename
+	config.Gituri = gituri
+	config.Storemethod = storemethod
+	config.Branch = branch
+}
