f2610ef3ea81422d83d31cc64171bad02aee205f
yhua123 <he_feng_yun@126.com>
2016-07-06 14:00:48 +0800

    Update changepassword.go
    change comment

--- controllers/changepassword.go
+++ controllers/changepassword.go
@@ -1,11 +1,11 @@
 package controllers
 
-// AccountPwdSettingController handles request to /account_setting
+// ChangePasswordController handles request to /change_password
 type ChangePasswordController struct {
 	BaseController
 }
 
-// Get renders the account settings page
+// Get renders the change password page
 func (asc *ChangePasswordController) Get() {
 	asc.Forward("page_title_reset_password", "change-password.htm")
 }
