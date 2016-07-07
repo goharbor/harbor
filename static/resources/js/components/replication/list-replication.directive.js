7eb74c1e2e6d3496231cfe6d066e95300fc0b3c9
kunw <kunw@vmware.com>
2016-07-08 00:03:32 +0800

    add license headers into files, replace vendors JS files with minified editions.

--- static/resources/js/components/replication/list-replication.directive.js
+++ static/resources/js/components/replication/list-replication.directive.js
@@ -1,3 +1,17 @@
+/*
+    Copyright (c) 2016 VMware, Inc. All Rights Reserved.
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+        
+        http://www.apache.org/licenses/LICENSE-2.0
+        
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
 (function() {
   
   'use strict';
@@ -236,34 +250,7 @@
     return directive;
     
     function link(scope, element, attrs, ctrl) {
-      /*     
-      var uponPaneHeight = element.find('#upon-pane').height();
-      var handleHeight = element.find('.split-handle').height() + element.find('.split-handle').offset().top + element.find('.well').height() - 24;
-      
-      var maxDownPaneHeight = 245;
-            
-      element.find('.split-handle').on('mousedown', mousedownHandler);
-      
-      function mousedownHandler(e) {
-        e.preventDefault();
-        $(document).on('mousemove', mousemoveHandler);    
-        $(document).on('mouseup', mouseupHandler);
-      }
-      
-      function mousemoveHandler(e) {
-        if(element.find('#down-pane').height() <= maxDownPaneHeight) {
-          element.find('#upon-pane').css({'height' : (uponPaneHeight - (handleHeight - e.pageY)) + 'px'});
-          element.find('#down-pane').css({'height' : (uponPaneHeight + (handleHeight - e.pageY - 196)) + 'px'});  
-        }else{
-          element.find('#down-pane').css({'height' : (maxDownPaneHeight) + 'px'});
-          $(document).off('mousemove');
-        }	
-      }
-      function mouseupHandler(e) {
-        $(document).off('mousedown');
-        $(document).off('mousemove');
-      }
-      */
+
       ctrl.lastPolicyId = -1;          
       
       scope.$watch('vm.replicationPolicies', function(current) { 
