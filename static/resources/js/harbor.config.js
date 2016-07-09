c55e88567998f168fef12808b6dea5a73b9656ce
kunw <kunw@vmware.com>
2016-07-09 16:38:54 +0800

    update for compatiblity with IE11.

--- static/resources/js/harbor.config.js
+++ static/resources/js/harbor.config.js
@@ -21,6 +21,19 @@
       $interpolateProvider.endSymbol('//');
     })
     .config(function($httpProvider) { 
+      //initialize get if not there
+      if (!$httpProvider.defaults.headers.get) {
+          $httpProvider.defaults.headers.get = {};    
+      }    
+  
+      // Answer edited to include suggestions from comments
+      // because previous version of code introduced browser-related errors
+  
+      //disable IE ajax request caching
+      $httpProvider.defaults.headers.get['If-Modified-Since'] = 'Mon, 26 Jul 1997 05:00:00 GMT';
+      // extra
+      $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
+      $httpProvider.defaults.headers.get['Pragma'] = 'no-cache';
       $httpProvider.defaults.headers.common = {'Accept': 'application/json, text/javascript, */*; q=0.01'};     
       $httpProvider.interceptors.push('redirectInterceptor');
     })
