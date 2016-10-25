8f758120d927779b6fbd5485dc1fe3949ef304b0
kunw <kunw@vmware.com>
2016-10-25 11:39:04 +0800

    Fixed toggling admin options.

--- src/ui/static/resources/js/components/user/toggle-admin.directive.js
+++ src/ui/static/resources/js/components/user/toggle-admin.directive.js
@@ -36,25 +36,22 @@
         .error(toggleAdminFailed);        
     }    
     
+    function toggleStatus() {
+      vm.isAdmin = !vm.isAdmin;
+      vm.enabled = vm.isAdmin ? 0 : 1;
+    }
+    
     function toggleAdminSuccess(data, status) {
-      if(vm.isAdmin) {
-        vm.isAdmin = false;
-      }else{
-        vm.isAdmin = true;
-      }
       console.log('Toggled userId:' + vm.userId + ' to admin:' + vm.isAdmin);
+      toggleStatus();
     }
 
     function toggleAdminFailed(data, status) {
+      console.log('Failed to toggle admin:' + data);
+      toggleStatus();
       $scope.$emit('modalTitle', $filter('tr')('error'));
       $scope.$emit('modalMessage', $filter('tr')('failed_to_toggle_admin'));
       $scope.$emit('raiseError', true);
-      if(vm.isAdmin) {
-        vm.isAdmin = false;
-      }else{
-        vm.isAdmin = true;
-      }
-      console.log('Failed to toggle admin:' + data);
     }    
   }
   
