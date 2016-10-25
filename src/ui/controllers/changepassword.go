45803b53e15023997e70a8c672d7c72858da1d45
kunw <kunw@vmware.com>
2016-10-25 16:20:42 +0800

    Updates for blocking account settings in LDAP auth mode.

--- src/ui/controllers/changepassword.go
+++ src/ui/controllers/changepassword.go
@@ -1,5 +1,9 @@
 package controllers
 
+import (
+	"net/http"
+)
+
 // ChangePasswordController handles request to /change_password
 type ChangePasswordController struct {
 	BaseController
@@ -7,5 +11,8 @@ type ChangePasswordController struct {
 
 // Get renders the change password page
 func (asc *ChangePasswordController) Get() {
+	if asc.AuthMode != "db_auth" {
+		asc.CustomAbort(http.StatusForbidden, "")
+	}
 	asc.Forward("page_title_change_password", "change-password.htm")
 }
