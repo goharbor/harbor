9126ee1258c6b09ffc67c4d175ad057521a37445
Steven Zou <szou@vmware.com>
2017-02-21 20:04:44 +0800

    Complete account settings component

--- harbor-app/src/app/shared/session-user.ts
+++ harbor-app/src/app/shared/session-user.ts
@@ -3,7 +3,9 @@ export class SessionUser {
     user_id: number;
     username: string;
     email: string;
-    role_name: string;
-    role_id: number;
-    has_admin_role: number;
+    realname: string;
+    role_name?: string;
+    role_id?: number;
+    has_admin_role?: number;
+    comment: string;
 }
