c2c1be253d6afc13089a87c4c460bb6bbccee1ba
Wenkai Yin <yinw@vmware.com>
2017-04-24 10:54:57 +0800

    1.abstract authenticator interface 2.implement interface for database authenticator

--- src/ui/auth/db/db.go
+++ src/ui/auth/db/db.go
@@ -15,10 +15,70 @@
 package db
 
 import (
-	"github.com/vmware/harbor/src/ui/auth"
+	"context"
+	"fmt"
+
+	"github.com/vmware/harbor/src/common"
 	"github.com/vmware/harbor/src/common/dao"
 	"github.com/vmware/harbor/src/common/models"
+	"github.com/vmware/harbor/src/common/utils/log"
+	"github.com/vmware/harbor/src/ui/auth"
 )
+
+// Name of database authenticator
+const Name = "database"
+
+// TODO remove the annotation
+/*
+func init() {
+	auth.Register(Name, &databaseAuthenticatorFactory{})
+}
+*/
+type database struct{}
+
+// Authenticate users with the records in database and add user information
+// into the context
+// authInfo should contain princial and credential
+// TODO add test case
+func (d *database) Authenticate(ctx context.Context,
+	authInfo ...string) (context.Context, error) {
+
+	if ctx == nil {
+		ctx = context.Background()
+	}
+
+	if len(authInfo) != 2 {
+		return ctx, fmt.Errorf("invalid authInfo")
+	}
+
+	principal := authInfo[0]
+	if len(principal) == 0 {
+		return ctx, fmt.Errorf("invalid principal")
+	}
+
+	credential := authInfo[1]
+
+	// TODO refactor AuthModel
+	m := models.AuthModel{
+		Principal: principal,
+		Password:  credential,
+	}
+
+	user, err := dao.LoginByDb(m)
+	if err != nil {
+		return ctx, err
+	}
+
+	ctx = context.WithValue(ctx, common.CtxKeyUser, user)
+	log.Infof("user %s authenticated by database has been added into context", user.Username)
+	return ctx, nil
+}
+
+type databaseAuthenticatorFactory struct{}
+
+func (d *databaseAuthenticatorFactory) Create(parameter interface{}) (auth.Authenticator, error) {
+	return &database{}, nil
+}
 
 // Auth implements Authenticator interface to authenticate user against DB.
 type Auth struct{}
@@ -33,5 +93,5 @@ func (d *Auth) Authenticate(m models.AuthModel) (*models.User, error) {
 }
 
 func init() {
-	auth.Register("db_auth", &Auth{})
+	auth.RegisterOld("db_auth", &Auth{})
 }
