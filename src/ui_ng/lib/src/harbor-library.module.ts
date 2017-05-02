25004bbd1fc2fef6535b7f331d7be92b0c305a39
Steven Zou <szou@vmware.com>
2017-05-02 15:12:39 +0800

    Enhance the module configuration of harbor ui

--- src/ui_ng/lib/src/harbor-library.module.ts
+++ src/ui_ng/lib/src/harbor-library.module.ts
@@ -2,18 +2,55 @@ import { NgModule, ModuleWithProviders, Provider } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SYSTEMINFO_DIRECTIVES } from './system/index';
 import { SERVICE_CONFIG, IServiceConfig } from './service.config';
+import {
+  AccessLogService,
+  AccessLogDefaultService,
+  EndpointService,
+  EndpointDefaultService,
+  ReplicationService,
+  ReplicationDefaultService,
+  RepositoryService,
+  RepositoryDefaultService,
+  TagService,
+  TagDefaultService
+} from './service/index';
 
+/**
+ * Declare default service configuration
+ */
 export const DefaultServiceConfig: IServiceConfig = {
   systemInfoEndpoint: "/api/system",
   repositoryBaseEndpoint: "",
   logBaseEndpoint: "",
   targetBaseEndpoint: "",
-  replicationRuleEndpoint:"",
+  replicationRuleEndpoint: "",
   replicationJobEndpoint: ""
 };
 
+/**
+ * Define the configuration for harbor shareable module
+ * 
+ * @export
+ * @interface HarborModuleConfig
+ */
 export interface HarborModuleConfig {
-  config?: Provider
+  //Service endpoints
+  config?: Provider,
+
+  //Service implementation for log
+  logService?: Provider,
+
+  //Service implementation for endpoint
+  endpointService?: Provider,
+
+  //Service implementation for replication
+  replicationService?: Provider,
+
+  //Service implementation for repository
+  repositoryService?: Provider,
+
+  //Service implementation for tag
+  tagService?: Provider
 }
 
 @NgModule({
@@ -29,7 +66,12 @@ export class HarborLibraryModule {
     return {
       ngModule: HarborLibraryModule,
       providers: [
-        config.config || { provide: SERVICE_CONFIG, useValue: DefaultServiceConfig }
+        config.config || { provide: SERVICE_CONFIG, useValue: DefaultServiceConfig },
+        config.logService || { provide: AccessLogService, useClass: AccessLogDefaultService },
+        config.endpointService || { provide: EndpointService, useClass: EndpointDefaultService },
+        config.replicationService || { provide: ReplicationService, useClass: ReplicationDefaultService },
+        config.repositoryService || { provide: RepositoryService, useClass: RepositoryDefaultService },
+        config.tagService || { provide: TagService, useClass: TagDefaultService }
       ]
     };
   }
@@ -38,7 +80,12 @@ export class HarborLibraryModule {
     return {
       ngModule: HarborLibraryModule,
       providers: [
-        config.config || { provide: SERVICE_CONFIG, useValue: DefaultServiceConfig }
+        config.config || { provide: SERVICE_CONFIG, useValue: DefaultServiceConfig },
+        config.logService || { provide: AccessLogService, useClass: AccessLogDefaultService },
+        config.endpointService || { provide: EndpointService, useClass: EndpointDefaultService },
+        config.replicationService || { provide: ReplicationService, useClass: ReplicationDefaultService },
+        config.repositoryService || { provide: RepositoryService, useClass: RepositoryDefaultService },
+        config.tagService || { provide: TagService, useClass: TagDefaultService }
       ]
     };
   }
