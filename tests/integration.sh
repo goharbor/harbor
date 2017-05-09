035b33140bd518ddacd6fdb973594f6c29c06a3e
wangyan <wangyan@vmware.com>
2017-05-08 19:18:17 -0700

    Support upload results into GS

--- tests/integration.sh
+++ tests/integration.sh
@@ -41,31 +41,31 @@ outfile="integration_logs_"$DRONE_BUILD_NUMBER"_"$DRONE_COMMIT".zip"
 
 zip -9 $outfile output.xml log.html package.list *container-logs.zip *.log
 
-# # GC credentials
-# keyfile="/root/vic-ci-logs.key"
-# botofile="/root/.boto"
-# echo -en $GS_PRIVATE_KEY > $keyfile
-# chmod 400 $keyfile
-# echo "[Credentials]" >> $botofile
-# echo "gs_service_key_file = $keyfile" >> $botofile
-# echo "gs_service_client_id = $GS_CLIENT_EMAIL" >> $botofile
-# echo "[GSUtil]" >> $botofile
-# echo "content_language = en" >> $botofile
-# echo "default_project_id = $GS_PROJECT_ID" >> $botofile
-#
-# if [ -f "$outfile" ]; then
-#   gsutil cp $outfile gs://vic-ci-logs
-#   echo "----------------------------------------------"
-#   echo "Download test logs:"
-#   echo "https://console.cloud.google.com/m/cloudstorage/b/vic-ci-logs/o/$outfile?authuser=1"
-#   echo "----------------------------------------------"
-# else
-#   echo "No log output file to upload"
-# fi
-#
-# if [ -f "$keyfile" ]; then
-#   rm -f $keyfile
-# fi
-#
+# GC credentials
+keyfile="/root/harbor-ci-logs.key"
+botofile="/root/.boto"
+echo -en $GS_PRIVATE_KEY > $keyfile
+chmod 400 $keyfile
+echo "[Credentials]" >> $botofile
+echo "gs_service_key_file = $keyfile" >> $botofile
+echo "gs_service_client_id = $GS_CLIENT_EMAIL" >> $botofile
+echo "[GSUtil]" >> $botofile
+echo "content_language = en" >> $botofile
+echo "default_project_id = $GS_PROJECT_ID" >> $botofile
+
+if [ -f "$outfile" ]; then
+  gsutil cp $outfile gs://harbor-ci-logs
+  echo "----------------------------------------------"
+  echo "Download test logs:"
+  echo "https://console.cloud.google.com/m/cloudstorage/b/harbor-ci-logs/o/$outfile?authuser=1"
+  echo "----------------------------------------------"
+else
+  echo "No log output file to upload"
+fi
+
+if [ -f "$keyfile" ]; then
+  rm -f $keyfile
+fi
+
 
 exit $rc
