17813a75f0d4b4547fc2f65ed29fa4b05fdac7aa
Wenkai Yin <yinw@vmware.com>
2017-05-19 17:06:14 +0800

    refactor member API

--- src/ui/api/utils.go
+++ src/ui/api/utils.go
@@ -33,32 +33,6 @@ import (
 	"github.com/vmware/harbor/src/ui/config"
 )
 
-func checkProjectPermission(userID int, projectID int64) bool {
-	roles, err := listRoles(userID, projectID)
-	if err != nil {
-		log.Errorf("error occurred in getProjectPermission: %v", err)
-		return false
-	}
-	return len(roles) > 0
-}
-
-// TODO remove
-func hasProjectAdminRole(userID int, projectID int64) bool {
-	roles, err := listRoles(userID, projectID)
-	if err != nil {
-		log.Errorf("error occurred in getProjectPermission: %v", err)
-		return false
-	}
-
-	for _, role := range roles {
-		if role.RoleID == models.PROJECTADMIN {
-			return true
-		}
-	}
-
-	return false
-}
-
 //sysadmin has all privileges to all projects
 func listRoles(userID int, projectID int64) ([]models.Role, error) {
 	roles := make([]models.Role, 0, 1)
