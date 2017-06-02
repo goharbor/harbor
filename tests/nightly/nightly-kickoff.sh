b329576180c4958ab9cfb9e50a79ccf66390bb28
wangyan <wangyan@vmware.com>
2017-06-01 19:54:01 -0700

    update

--- tests/nightly/nightly-kickoff.sh
+++ tests/nightly/nightly-kickoff.sh
@@ -1,16 +1,17 @@
+# sync with latest master code.
+git fetch upstream
+git reset --hard upstream/drone
+git config credential.helper store
+git push origin drone-nightly
+
+cd tests/nightly
 tokenfile=nightly-tokenfile
 
 # append string into the token file.
 echo "$(date) $(ls -1 | wc -l)" >> $tokenfile
+echo "------------------------" >> $tokenfile
 
 cd ../..
-
-# sync with latest master code.
-git fetch upstream
-git reset --hard upstream/master
-git config credential.helper store
-git push origin drone-nightly
-
 git checkout drone-nightly
 git add tests/nightly/$tokenfile
 git commit -m "nightly run"
