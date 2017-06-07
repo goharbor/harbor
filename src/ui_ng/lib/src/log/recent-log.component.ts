aa0212460df0ed80b3d157c26f7eb0e272b09d04
Steven Zou <szou@vmware.com>
2017-06-07 14:36:55 +0800

    Enable server-driven pagination for recent logs

--- src/ui_ng/lib/src/log/recent-log.component.ts
+++ src/ui_ng/lib/src/log/recent-log.component.ts
@@ -15,14 +15,16 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import {
     AccessLogService,
-    AccessLog
+    AccessLog,
+    AccessLogItem,
+    RequestQueryParams
 } from '../service/index';
 import { ErrorHandler } from '../error-handler/index';
 import { Observable } from 'rxjs/Observable';
 import { toPromise, CustomComparator } from '../utils';
 import { LOG_TEMPLATE, LOG_STYLES } from './recent-log.template';
 
-import { Comparator } from 'clarity-angular';
+import { Comparator, State } from 'clarity-angular';
 
 @Component({
     selector: 'hbr-log',
@@ -31,13 +33,13 @@ import { Comparator } from 'clarity-angular';
 })
 
 export class RecentLogComponent implements OnInit {
-    recentLogs: AccessLog[];
-    logsCache: AccessLog[];
-    onGoing: boolean = false;
-    lines: number = 10; //Support 10, 25 and 50
+    recentLogs: AccessLogItem[] = [];
+    logsCache: AccessLog;
+    loading: boolean = true;
     currentTerm: string;
 
-    loading: boolean;
+    pageSize: number = 15;
+    currentPage: number = 0;
 
     opTimeComparator: Comparator<AccessLog> = new CustomComparator<AccessLog>('op_time', 'date');
 
@@ -46,67 +48,140 @@ export class RecentLogComponent implements OnInit {
         private errorHandler: ErrorHandler) { }
 
     ngOnInit(): void {
-        this.retrieveLogs();
     }
 
-    handleOnchange($event: any) {
-        this.currentTerm = '';
-        if ($event && $event.target && $event.target["value"]) {
-            this.lines = $event.target["value"];
-            if (this.lines < 10) {
-                this.lines = 10;
-            }
-            this.retrieveLogs();
-        }
-    }
-
-    public get logNumber(): number {
-        return this.recentLogs ? this.recentLogs.length : 0;
+    public get totalCount(): number {
+        return this.logsCache && this.logsCache.metadata ? this.logsCache.metadata.xTotalCount : 0;
     }
 
     public get inProgress(): boolean {
-        return this.onGoing;
+        return this.loading;
     }
 
     public doFilter(terms: string): void {
         if (terms.trim() === "") {
-            this.recentLogs = this.logsCache.filter(log => log.username != "");
+            //Clear search results
+            this.recentLogs = this.logsCache.data.filter(log => log.username != "");
             return;
         }
         this.currentTerm = terms;
-        this.recentLogs = this.logsCache.filter(log => this.isMatched(terms, log));
+        this.recentLogs = this.logsCache.data.filter(log => this.isMatched(terms, log));
     }
 
     public refresh(): void {
-        this.retrieveLogs();
+        this.currentTerm = "";
+        this.currentPage = 0;
+        this.load({});
     }
 
-    retrieveLogs(): void {
-        if (this.lines < 10) {
-            this.lines = 10;
+    load(state: State) {
+        let pageNumber: number = this._calculatePage(state);
+        if (pageNumber !== this.currentPage) {
+            //load data
+            let params: RequestQueryParams = new RequestQueryParams();
+            params.set("page", '' + pageNumber);
+            params.set("page_size", '' + this.pageSize);
+
+            this.loading = true;
+            toPromise<AccessLog>(this.logService.getRecentLogs(params))
+                .then(response => {
+                    this.logsCache = response; //Keep the data
+                    this.recentLogs = this.logsCache.data.filter(log => log.username != "");//To display
+
+                    //Do customized filter
+                    this._doFilter(state);
+
+                    //Do customized sorting
+                    this._doSorting(state);
+
+                    this.currentPage = pageNumber;
+
+                    this.loading = false;
+                })
+                .catch(error => {
+                    this.loading = false;
+                    this.errorHandler.error(error);
+                });
+        } else {
+            this.recentLogs = this.logsCache.data.filter(log => log.username != "");//Reset data
+
+            //Do customized filter
+            this._doFilter(state);
+
+            //Do customized sorting
+            this._doSorting(state);
         }
-
-        this.onGoing = true;
-        this.loading = true;
-        toPromise<AccessLog[]>(this.logService.getRecentLogs(this.lines))
-            .then(response => {
-                this.onGoing = false;
-                this.loading = false;
-                this.logsCache = response; //Keep the data
-                this.recentLogs = this.logsCache.filter(log => log.username != "");//To display
-            })
-            .catch(error => {
-                this.onGoing = false;
-                this.loading = false;
-                this.errorHandler.error(error);
-            });
     }
 
-    isMatched(terms: string, log: AccessLog): boolean {
+    isMatched(terms: string, log: AccessLogItem): boolean {
         let reg = new RegExp('.*' + terms + '.*', 'i');
         return reg.test(log.username) ||
             reg.test(log.repo_name) ||
             reg.test(log.operation) ||
             reg.test(log.repo_tag);
+    }
+
+    _calculatePage(state: State): number {
+        if (!state || !state.page) {
+            return 1;
+        }
+
+        return Math.ceil((state.page.to + 1) / state.page.size);
+    }
+
+    _doFilter(state: State): void {
+        if (!this.recentLogs || this.recentLogs.length === 0) {
+            return;
+        }
+
+        if (!state || !state.filters || state.filters.length === 0) {
+            return;
+        }
+
+        state.filters.forEach((filter: {
+            property: string;
+            value: string;
+        }) => {
+            this.recentLogs = this.recentLogs.filter(logItem => this._regexpFilter(filter["value"], logItem[filter["property"]]));
+        });
+    }
+
+    _regexpFilter(terms: string, testedValue: any): boolean {
+        let reg = new RegExp('.*' + terms + '.*', 'i');
+        return reg.test(testedValue);
+    }
+
+    _doSorting(state: State): void {
+        if (!this.recentLogs || this.recentLogs.length === 0) {
+            return;
+        }
+
+        if (!state || !state.sort) {
+            return;
+        }
+
+        this.recentLogs = this.recentLogs.sort((a: AccessLogItem, b: AccessLogItem) => {
+            let comp: number = 0;
+            if (typeof state.sort.by !== "string") {
+                comp = state.sort.by.compare(a, b);
+            } else {
+                let propA = a[state.sort.by.toString()], propB = b[state.sort.by.toString()];
+                if (typeof propA === "string") {
+                    comp = propA.localeCompare(propB);
+                } else {
+                    if (propA > propB) {
+                        comp = 1;
+                    } else if (propA < propB) {
+                        comp = -1;
+                    }
+                }
+            }
+
+            if (state.sort.reverse) {
+                comp = -comp;
+            }
+
+            return comp;
+        });
     }
 }
