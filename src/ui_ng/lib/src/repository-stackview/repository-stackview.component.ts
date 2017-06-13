71f3480932d2a521c1ddeb1fcd2e2ad03196acc9
Steven Zou <szou@vmware.com>
2017-06-13 16:09:46 +0800

    Improve repo-tag-stack view

--- src/ui_ng/lib/src/repository-stackview/repository-stackview.component.ts
+++ src/ui_ng/lib/src/repository-stackview/repository-stackview.component.ts
@@ -5,9 +5,13 @@ import { Comparator } from 'clarity-angular';
 import { REPOSITORY_STACKVIEW_TEMPLATE } from './repository-stackview.component.html';
 import { REPOSITORY_STACKVIEW_STYLES } from './repository-stackview.component.css';
 
-import { Repository } from '../service/interface';
+import {
+  Repository,
+  SystemInfo,
+  SystemInfoService,
+  RepositoryService
+} from '../service/index';
 import { ErrorHandler } from '../error-handler/error-handler';
-import { RepositoryService } from '../service/repository.service';
 
 import { toPromise, CustomComparator } from '../utils';
 
@@ -21,7 +25,7 @@ import { Subscription } from 'rxjs/Subscription';
 @Component({
   selector: 'hbr-repository-stackview',
   template: REPOSITORY_STACKVIEW_TEMPLATE,
-  styles: [ REPOSITORY_STACKVIEW_STYLES ],
+  styles: [REPOSITORY_STACKVIEW_STYLES],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RepositoryStackviewComponent implements OnInit {
@@ -33,20 +37,30 @@ export class RepositoryStackviewComponent implements OnInit {
 
   lastFilteredRepoName: string;
   repositories: Repository[];
+  systemInfo: SystemInfo;
 
   @ViewChild('confirmationDialog')
   confirmationDialog: ConfirmationDialogComponent;
 
   pullCountComparator: Comparator<Repository> = new CustomComparator<Repository>('pull_count', 'number');
-  
+
   tagsCountComparator: Comparator<Repository> = new CustomComparator<Repository>('tags_count', 'number');
 
   constructor(
     private errorHandler: ErrorHandler,
     private translateService: TranslateService,
     private repositoryService: RepositoryService,
-    private ref: ChangeDetectorRef){}
-  
+    private systemInfoService: SystemInfoService,
+    private ref: ChangeDetectorRef) { }
+
+  public get registryUrl(): string {
+    return this.systemInfo ? this.systemInfo.registry_url : "";
+  }
+
+  public get withNotary(): boolean {
+    return this.systemInfo ? this.systemInfo.with_notary : false;
+  }
+
   confirmDeletion(message: ConfirmationAcknowledgement) {
     if (message &&
       message.source === ConfirmationTargets.REPOSITORY &&
@@ -55,19 +69,24 @@ export class RepositoryStackviewComponent implements OnInit {
       toPromise<number>(this.repositoryService
         .deleteRepository(repoName))
         .then(
-          response => {
-            this.refresh();
-            this.translateService.get('REPOSITORY.DELETED_REPO_SUCCESS')
-                .subscribe(res=>this.errorHandler.info(res));
+        response => {
+          this.refresh();
+          this.translateService.get('REPOSITORY.DELETED_REPO_SUCCESS')
+            .subscribe(res => this.errorHandler.info(res));
         }).catch(error => this.errorHandler.error(error));
     }
   }
 
   ngOnInit(): void {
-    if(!this.projectId) {
+    if (!this.projectId) {
       this.errorHandler.error('Project ID cannot be unset.');
       return;
-    }      
+    }
+    //Get system info for tag views
+    toPromise<SystemInfo>(this.systemInfoService.getSystemInfo())
+      .then(systemInfo => this.systemInfo = systemInfo)
+      .catch(error => this.errorHandler.error(error));
+
     this.lastFilteredRepoName = '';
     this.retrieve();
   }
@@ -76,10 +95,10 @@ export class RepositoryStackviewComponent implements OnInit {
     toPromise<Repository[]>(this.repositoryService
       .getRepositories(this.projectId, this.lastFilteredRepoName))
       .then(
-        repos => this.repositories = repos,
-        error => this.errorHandler.error(error));
-    let hnd = setInterval(()=>this.ref.markForCheck(), 100);
-    setTimeout(()=>clearInterval(hnd), 1000);
+      repos => this.repositories = repos,
+      error => this.errorHandler.error(error));
+    let hnd = setInterval(() => this.ref.markForCheck(), 100);
+    setTimeout(() => clearInterval(hnd), 1000);
   }
 
   doSearchRepoNames(repoName: string) {
