7c0e79d10a770d0e08ca096abafef1d19cb8edbd
Steven Zou <szou@vmware.com>
2017-06-22 17:46:54 +0800

    update ui builder to 1.2.6

--- make/dev/nodeclarity/entrypoint.sh
+++ make/dev/nodeclarity/entrypoint.sh
@@ -18,7 +18,8 @@ if [ ! -z "$npm_proxy" -a "$npm_proxy" != " " ]; then
 	npm config set proxy $npm_proxy
 fi
 
-cp ./src/package.json .
+mv ./src/package.json ./
+cat ./package.json
 npm install
 
 ./node_modules/.bin/ngc -p tsconfig-aot.json
