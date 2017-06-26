6e9ec3f22c11b8dbb77a88052f0f844b8236c9a9
Wenkai Yin <yinw@vmware.com>
2017-06-26 14:15:01 +0800

    refactor statistics API

--- src/ui_ng/src/app/shared/statictics/statistics.ts
+++ src/ui_ng/src/app/shared/statictics/statistics.ts
@@ -14,8 +14,8 @@
 export class Statistics {
     constructor() {}
     
-    my_project_count: number;
-    my_repo_count: number;
+    private_project_count: number;
+    private_repo_count: number;
     public_project_count: number;
     public_repo_count: number;
     total_project_count: number;
