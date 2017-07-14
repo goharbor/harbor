fae35add8f552efd292662fa008e34f54d947624
Steven Zou <szou@vmware.com>
2017-07-14 15:58:35 +0800

    fix issue 2759

--- src/ui_ng/lib/src/replication/index.ts
+++ src/ui_ng/lib/src/replication/index.ts
@@ -1,6 +1,8 @@
 import { Type } from '@angular/core';
 import { ReplicationComponent } from './replication.component';
 
+export * from './replication.component';
+
 export const REPLICATION_DIRECTIVES: Type<any>[] = [
   ReplicationComponent
 ];
