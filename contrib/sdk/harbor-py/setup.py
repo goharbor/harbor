05079758be60b43cbb8e31d2c9021f52a65b7278
Casey Peel <cpeel@blacksky.com>
2017-08-03 13:41:38 -0700

    Correctly specify dependency packages
    Include the requests package as part of setup.py, so pip will
    download and install it as a dependency.

--- contrib/sdk/harbor-py/setup.py
+++ contrib/sdk/harbor-py/setup.py
@@ -68,7 +68,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    #install_requires=['peppercorn'],
+    install_requires=['requests>2.2.0'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
