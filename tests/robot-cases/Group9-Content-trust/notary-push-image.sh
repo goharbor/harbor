6a7bea44e569fe9ffe74c91e0f86d8637ee34fd7
wangyan <wangyan@vmware.com>
2017-09-18 15:46:40 +0800

    cherry pick notary issue

--- tests/robot-cases/Group9-Content-trust/notary-push-image.sh
+++ tests/robot-cases/Group9-Content-trust/notary-push-image.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
-docker pull tomcat
+docker pull tomcat:latest
 
 IP=`ip addr s eth0 |grep "inet "|awk '{print $2}' |awk -F "/" '{print $1}'`
+PASSHRASE='Harbor12345'
+
+echo $IP
+
 export DOCKER_CONTENT_TRUST=1
 export DOCKER_CONTENT_TRUST_SERVER=https://$IP:4443
 
-docker login -u admin -p Harbor12345 $IP
+export NOTARY_ROOT_PASSPHRASE=$PASSHRASE
+export NOTARY_TARGETS_PASSPHRASE=$PASSHRASE
+export NOTARY_SNAPSHOT_PASSPHRASE=$PASSHRASE
+export DOCKER_CONTENT_TRUST_ROOT_PASSPHRASE=$PASSHRASE
+export DOCKER_CONTENT_TRUST_REPOSITORY_PASSPHRASE=$PASSHRASE
+export DOCKER_CONTENT_TRUST_OFFLINE_PASSPHRASE=$PASSHRASE
+export DOCKER_CONTENT_TRUST_TAGGING_PASSPHRASE=$PASSHRASE
 
+docker login -u admin -p Harbor12345 $IP
 docker tag tomcat $IP/library/tomcat:latest
-python ./tests/robot-cases/Group9-Content-trust/notary-push-image.py
+docker push $IP/library/tomcat:latest
+
