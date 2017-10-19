1d89951289d3a7d7a1708e9f62876389e5ed6bba
Ri Xu <xuri.me@gmail.com>
2017-10-19 18:25:01 +0800

    fix the build break.
    Signed-off-by: Ri Xu <xuri.me@gmail.com>

--- src/common/utils/registry/repository_test.go
+++ src/common/utils/registry/repository_test.go
@@ -127,7 +127,7 @@ func TestPullBlob(t *testing.T) {
 		t.Fatalf("failed to read from reader: %v", err)
 	}
 
-	if !bytes.Compare(b, blob) {
+	if bytes.Compare(b, blob) != 0 {
 		t.Errorf("unexpected blob: %s != %s", string(b), string(blob))
 	}
 }
@@ -278,7 +278,7 @@ func TestPullManifest(t *testing.T) {
 		t.Errorf("unexpected media type of manifest: %s != %s", md, mediaType)
 	}
 
-	if !bytes.Compare(payload, manifest) {
+	if bytes.Compare(payload, manifest) != 0 {
 		t.Errorf("unexpected manifest: %s != %s", string(payload), string(manifest))
 	}
 }
