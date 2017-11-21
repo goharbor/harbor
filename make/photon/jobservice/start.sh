b3e0af23820042595dbf5465c3296e26b1075f45
Tan Jiang <jiangd@vmware.com>
2017-11-21 19:31:15 +0800

    Fix permission issue in job_log directory

--- make/photon/jobservice/start.sh
+++ make/photon/jobservice/start.sh
@@ -5,5 +5,8 @@ fi
 if [ -d /var/log/jobs ]; then
     chown -R 10000:10000 /var/log/jobs/
 fi
+if [ -d /var/log/jobs/scan_job ]; then
+    chmod +x /var/log/jobs/scan_job
+fi
 sudo -E -u \#10000 "/harbor/harbor_jobservice"
 
