9c07108028a3b4001ad791a31b3da69b7455ebcb
wangyan <wangyan@vmware.com>
2017-12-21 00:38:29 -0800

    add debug code to get vm ip

--- tests/nightly-test/utils/govc_utils.py
+++ tests/nightly-test/utils/govc_utils.py
@@ -16,8 +16,9 @@ def getvmip(vc_url, vc_user, vc_password, vm_name, timeout=600) :
             result = subprocess.check_output(cmd,shell=True)
             print "######"
             print result
+            print result == ''
             print "######"            
-            if result is not None and result is not "photon-machine":
+            if result is not '' and result is not "photon-machine":
                 print result
                 return 0
         except Exception, e:
