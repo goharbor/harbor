ad624d493a6075f4bc4f341787ca20b3101cd184
wangyan <wangyan@vmware.com>
2017-12-21 23:06:53 -0800

    fix execute test bug

--- tests/nightly-test/launch.py
+++ tests/nightly-test/launch.py
@@ -89,4 +89,4 @@ if harbor_ova_endpoint is not None:
         sys.exit(-1)
 
 print "All test done, then to execute TC"
-execute_test_ova(harbor_ova_endpoint, ova_password, test_suite)
+test_executor.execute_test_ova(harbor_ova_endpoint, ova_password, test_suite)
