0baa56478b0a1cfe4ee9f33dfcf04b3a617eb17a
wangyan <wangyan@vmware.com>
2017-12-21 23:40:55 -0800

    modify code path

--- tests/nightly-test/utils/test_executor.py
+++ tests/nightly-test/utils/test_executor.py
@@ -5,13 +5,14 @@ import time
 
 # Needs have docker installed.
 def execute_test_ova(harbor_endpoint, harbor_root_pwd, test_suite, harbor_pwd='Harbor12345') :
-    cmd = "docker run -it --privileged -v /harbor/workspace/harbor_nightly_test:/drone -w /drone vmware/harbor-e2e-engine:1.38 pybot -v ip:%s -v HARBOR_PASSWORD:%s -v SSH_PWD:%s " % (harbor_endpoint, harbor_pwd, harbor_root_pwd)
+    cmd = "docker run -it --privileged -v /harbor/workspace/harbor_nightly_test_yan:/drone -w /drone vmware/harbor-e2e-engine:1.38 pybot -v ip:%s -v HARBOR_PASSWORD:%s -v SSH_PWD:%s " % (harbor_endpoint, harbor_pwd, harbor_root_pwd)
     if test_suite == 'Nightly':
         cmd = cmd + "/drone/tests/robot-cases/Group10-Nightly/Nightly.robot"
 
     if test_suite == 'Longevity':
         cmd = cmd + "/drone/tests/robot-cases/Group10-Longevity/Longevity.robot"
     
+    print cmd
     rc = os.system(cmd)
     collect_log()
     return rc
