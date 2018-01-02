08cef2a88c3f2e9e067e0493edf4e0629ad71ea6
wangyan <wangyan@vmware.com>
2018-01-01 19:37:24 -0800

    update code

--- tests/nightly-test/shellscript/getvmip.sh
+++ tests/nightly-test/shellscript/getvmip.sh
@@ -4,4 +4,8 @@ export GOVC_USERNAME=$2
 export GOVC_PASSWORD=$3
 export GOVC_INSECURE=1
 export GOVC_URL=$1
+
+echo $GOVC_URL
+echo $GOVC_PASSWORD
+
 govc vm.info -json $4  | jq -r .VirtualMachines[].Guest.HostName
