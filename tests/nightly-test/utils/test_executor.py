6b3cac6ec9ac55596e86419baaa6912617447794
wangyan <wangyan@vmware.com>
2018-01-01 22:39:10 -0800

    update data

--- tests/nightly-test/utils/test_executor.py
+++ tests/nightly-test/utils/test_executor.py
@@ -9,7 +9,7 @@ import json
 
 # Needs have docker installed.
 def execute_test_ova(harbor_endpoint, harbor_root_pwd, test_suite, harbor_pwd='Harbor12345') :
-    cmd = "docker run -it --privileged -v /harbor/workspace/harbor_nightly_test_yan:/drone -w /drone vmware/harbor-e2e-engine:1.38 pybot -v ip:%s -v HARBOR_PASSWORD:%s -v SSH_PWD:%s " % (harbor_endpoint, harbor_pwd, harbor_root_pwd)
+    cmd = "docker run -i --privileged -v /harbor/workspace/harbor_nightly_test_yan:/drone -w /drone vmware/harbor-e2e-engine:1.38 pybot -v ip:%s -v HARBOR_PASSWORD:%s -v SSH_PWD:%s " % (harbor_endpoint, harbor_pwd, harbor_root_pwd)
     if test_suite == 'Nightly':
         cmd = cmd + "/drone/tests/robot-cases/Group11-Nightly/Nightly.robot"
 
