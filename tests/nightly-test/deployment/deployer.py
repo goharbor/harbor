4206d33f7e4beddc82ecfde37e33ca9bf20a82fa
wangyan <wangyan@vmware.com>
2018-01-02 21:20:40 -0800

    fix issue

--- tests/nightly-test/deployment/deployer.py
+++ tests/nightly-test/deployment/deployer.py
@@ -5,6 +5,9 @@ import sys
 import time
 import subprocess
 from datetime import datetime
+dir_path = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(dir_path + '../utils')
+import govc_utils
 
 class Deployer(object):
     __metaclass__ = abc.ABCMeta
