c93e034c03186cd0e9bf379e7eae8de9b1336402
wangyan <wangyan@vmware.com>
2018-01-04 03:36:17 -0800

    remove testing code for debug

--- tests/nightly-test/test.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-dir_path = os.path.dirname(os.path.realpath(__file__))
-sys.path.append(dir_path + '/utils')
-
-import test_executor
-
-test_executor.execute_test_ova('sc-rdops-vm10-dhcp-60-192.eng.vmware.com', 'zhu88jie', 'Nightly')
