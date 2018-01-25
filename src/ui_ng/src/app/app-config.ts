a1c850cfdbb5cf5f395ad35d48d7894f685d31b2
Deng, Qian <dengqian0826@gmail.com>
2018-01-25 17:28:56 +0800

    Display storage statistic only in filesystem mode

--- src/ui_ng/src/app/app-config.ts
+++ src/ui_ng/src/app/app-config.ts
@@ -14,25 +14,6 @@
 import { ClairDBStatus } from 'harbor-ui';
 
 export class AppConfig {
-    constructor() {
-        //Set default value
-        this.with_notary = false;
-        this.with_admiral = false;
-        this.with_clair = false;
-        this.admiral_endpoint = "";
-        this.auth_mode = "db_auth";
-        this.registry_url = "";
-        this.project_creation_restriction = "everyone";
-        this.self_registration = true;
-        this.has_ca_root = false;
-        this.harbor_version = "1.2.0";//default
-        this.clair_vulnerability_status = {
-            overall_last_update: 0,
-            details: []
-        };
-        this.next_scan_all = 0;
-    }
-
     with_notary: boolean;
     with_admiral: boolean;
     with_clair: boolean;
@@ -45,4 +26,25 @@ export class AppConfig {
     harbor_version: string;
     clair_vulnerability_status?: ClairDBStatus;
     next_scan_all: number;
+    registry_storage_provider_name: string;
+
+    constructor() {
+        // Set default value
+        this.with_notary = false;
+        this.with_admiral = false;
+        this.with_clair = false;
+        this.admiral_endpoint = "";
+        this.auth_mode = "db_auth";
+        this.registry_url = "";
+        this.project_creation_restriction = "everyone";
+        this.self_registration = true;
+        this.has_ca_root = false;
+        this.harbor_version = "1.2.0";
+        this.clair_vulnerability_status = {
+            overall_last_update: 0,
+            details: []
+        };
+        this.next_scan_all = 0;
+        this.registry_storage_provider_name = "";
+    }
 }
