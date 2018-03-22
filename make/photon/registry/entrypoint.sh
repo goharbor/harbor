adbd367dc5c3104608bde2a52cedc6d37548efc6
Jesse Hu <huh@vmware.com>
2018-03-22 22:19:36 +0800

    Enhance registry docker file to make it work with NFS server

--- make/photon/registry/entrypoint.sh
+++ make/photon/registry/entrypoint.sh
@@ -7,10 +7,16 @@ if [ -d /etc/registry ]; then
 fi
 if [ -d /var/lib/registry ]; then
     chown 10000:10000 -R /var/lib/registry
-fi  
+fi
 if [ -d /storage ]; then
-    chown 10000:10000 -R /storage
-fi  
+    if ! stat -c '%u:%g' /storage | grep -q '10000:10000' ; then
+        # 10000 is the id of harbor user/group.
+        # Usually NFS Server does not allow changing owner of the export directory,
+        # so need to skip this step and requires NFS Server admin to set its owner to 10000.
+        chown 10000:10000 -R /storage
+    fi
+fi
+
 case "$1" in
     *.yaml|*.yml) set -- registry serve "$@" ;;
     serve|garbage-collect|help|-*) set -- registry "$@" ;;
