bb292aab96cc7c14b047ff0869157f7dcb9fe0f5
Steven Zou <szou@vmware.com>
2018-04-16 12:12:11 +0800

    Fix the failure api test cases because API implementation changed

--- tests/apitests/api-testing/models/project.go
+++ tests/apitests/api-testing/models/project.go
@@ -3,7 +3,7 @@ package models
 //Project : For /api/projects
 type Project struct {
 	Name     string    `json:"project_name"`
-	Metadata *Metadata `json:"metadata, omitempty"`
+	Metadata *Metadata `json:"metadata,omitempty"`
 }
 
 //Metadata : Metadata for project
