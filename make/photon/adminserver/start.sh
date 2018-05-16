009e857587d26d5e5f634cdb47adf9bbc27d8af9
Tan Jiang <jiangd@vmware.com>
2018-05-16 14:35:14 +0800

    chown only to the folder to store the config.json
    Narrow down the scope of `chown` in adminserver because the
    /etc/adminserver/config/ is the location to store the config.json file.
    And /etc/adminserver/key should be readonly.

--- make/photon/adminserver/start.sh
+++ make/photon/adminserver/start.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-if [ -d /etc/adminserver ]; then
-    chown -R 10000:10000 /etc/adminserver
+
+#In the case when the config store is set to filesystem, the directory has to be writable.
+if [ -d /etc/adminserver/config ]; then
+    chown -R 10000:10000 /etc/adminserver/config
 fi
 sudo -E -u \#10000 "/harbor/harbor_adminserver"
