f2cd29d4f5bba384bd46bed274a3a46dba0d2cb4
Wenkai Yin <yinw@vmware.com>
2018-07-06 18:02:00 +0800

    update

--- tests/integration.sh
+++ tests/integration.sh
@@ -122,12 +122,18 @@ function publishImage {
     docker images
     docker login -u $DOCKER_HUB_USERNAME -p $DOCKER_HUB_PASSWORD
     # rename the images with tag "dev" and push to Docker Hub
-    docker images | sed -n "s|\(vmware/[.-_a-z0-9]*\)\s*\($Harbor_Assets_Version\).*|docker tag \1:\2 \1:dev;docker push \1:dev|p" | bash
-    ehco "Images are published successfully"
+    # docker images | sed -n "s|\(vmware/[.-_a-z0-9]*\)\s*\($Harbor_Assets_Version\).*|docker tag \1:\2 \1:dev;docker push \1:dev|p" | bash
+    docker images | sed -n "s|\(vmware/[.-_a-z0-9]*\)\s*\($Harbor_Assets_Version\).*|docker tag \1:\2 \1:dev|p" | bash
+    echo "Images are published successfully"
+    docker images
 }
 
 echo "========================================"
-package_offline_installer
+set -e
+docker pull hello-world
+docker tag hello-world:latest vmware/harbor-test:v1.5.0
+echo $DOCKER_HUB_USERNAME
+echo $DOCKER_HUB_PASSWORD
 publishImage
 exit 0
 
