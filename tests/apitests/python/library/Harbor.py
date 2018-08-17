4483d98a1ea7a4979508590f4475eb255a628c1d
Wenkai Yin <yinw@vmware.com>
2018-08-17 16:05:55 +0800

    Cherry-pick the replication label filter E2E test cases to branch 1.6.0 (#5650)
    * Add E2E API test cases for replication based on labels (#5639)
    
    This commit creates a new E2E API test case to verify the replication based on label
    
    Signed-off-by: Wenkai Yin <yinw@vmware.com>
    
    * Fix module not found error in API test
    
    Reload the python path to fix the module not found error in API E2E test
    
    Signed-off-by: Wenkai Yin <yinw@vmware.com>

---
+++ tests/apitests/python/library/Harbor.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+import site
+reload(site)
+import project
+import label
+import registry
+import replication
+import repository
+import swagger_client
+
+class Harbor(project.Project, label.Label, 
+    registry.Registry, replication.Replication,
+    repository.Repository):
+    pass
