acd7ec231079b085f41531b6061eb80e816689b1
Steven Zou <szou@vmware.com>
2018-08-27 16:18:07 +0800

    Fix issues related with chart storage - inject custom CA bundle into chart repo - update prepare script to inject credentials
    Signed-off-by: Steven Zou <szou@vmware.com>

--- make/photon/chartserver/docker-entrypoint.sh
+++ make/photon/chartserver/docker-entrypoint.sh
@@ -7,6 +7,22 @@ if [ -d /chart_storage ]; then
     chown 10000:10000 -R /chart_storage
 fi
 
-# Start the server process
+#Config the custom ca bundle
+if [ -f /etc/chartserver/custom-ca-bundle.crt ]; then
+    if grep -q "Photon" /etc/lsb-release; then
+        if [ ! -f /etc/pki/tls/certs/ca-bundle.crt.original ]; then
+            cp /etc/pki/tls/certs/ca-bundle.crt /etc/pki/tls/certs/ca-bundle.crt.original
+        fi
+
+        echo "Appending custom ca bundle ..."
+        cp /etc/pki/tls/certs/ca-bundle.crt.original /etc/pki/tls/certs/ca-bundle.crt
+        cat /etc/chartserver/custom-ca-bundle.crt >> /etc/pki/tls/certs/ca-bundle.crt
+        echo "Done."
+    else
+        echo "Current OS is not Photon, skip appending ca bundle"
+    fi
+fi
+
+#Start the server process
 sudo -E -H -u \#10000 sh -c "/chartserver/chartm" #Parameters are set by ENV
 set +e
