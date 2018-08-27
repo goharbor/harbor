0a624853654b95c5d02a16bb12d0153087a5b622
陈德 <chende@caicloud.io>
2018-08-27 15:18:19 +0800

    Add comment to source code
    Signed-off-by: 陈德 <chende@caicloud.io>

--- src/adminserver/systeminfo/imagestorage/filesystem/driver.go
+++ src/adminserver/systeminfo/imagestorage/filesystem/driver.go
@@ -57,6 +57,11 @@ func (d *driver) Cap() (*storage.Capacity, error) {
 		return nil, err
 	}
 
+	// When container is run in MacOS, `bsize` obtained by `statfs` syscall is not the fundamental block size,
+	// but the `iosize` (optimal transfer block size) instead, it's usually 1024 times larger than the `bsize`.
+	// for example `4096 * 1024`. To get the correct block size, we should use `frsize`. But `frsize` isn't
+	// guaranteed to be supported everywhere, so we need to check whether it's supported before use it.
+	// For more details, please refer to: https://github.com/docker/for-mac/issues/2136
 	bSize := uint64(stat.Bsize)
 	field := reflect.ValueOf(&stat).Elem().FieldByName("Frsize")
 	if field.IsValid() {
