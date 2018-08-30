34f19f437d46e691165da8ee2a689bc6a9b59e54
Steven Zou <szou@vmware.com>
2018-08-30 11:26:46 +0800

    Delete all the versions of the specified chart - add API route - add DELETE chart method in utility handler of chart controller - add UT case for the newly added methods - update swagger.yaml to refelct the new change
    Signed-off-by: Steven Zou <szou@vmware.com>

--- src/chartserver/utility_handler.go
+++ src/chartserver/utility_handler.go
@@ -5,6 +5,13 @@ import (
 	"fmt"
 	"net/url"
 	"strings"
+	"sync"
+
+	helm_repo "k8s.io/helm/pkg/repo"
+)
+
+const (
+	maxDeletionThreads = 10
 )
 
 //UtilityHandler provides utility methods
@@ -34,4 +41,100 @@ func (uh *UtilityHandler) GetChartsByNs(namespace string) ([]*ChartInfo, error) 
 	}
 
 	return uh.chartOperator.GetChartList(content)
+}
+
+//DeleteChart deletes all the chart versions of the specified chart under the namespace.
+func (uh *UtilityHandler) DeleteChart(namespace, chartName string) error {
+	if len(strings.TrimSpace(namespace)) == 0 {
+		return errors.New("empty namespace when deleting chart")
+	}
+
+	if len(strings.TrimSpace(chartName)) == 0 {
+		return errors.New("empty chart name when deleting chart")
+	}
+
+	path := fmt.Sprintf("/api/%s/charts/%s", namespace, chartName)
+	url := fmt.Sprintf("%s%s", uh.backendServerAddress.String(), path)
+
+	content, err := uh.apiClient.GetContent(url)
+	if err != nil {
+		return err
+	}
+
+	allVersions, err := uh.chartOperator.GetChartVersions(content)
+	if err != nil {
+		return err
+	}
+
+	//Let's delete the versions in parallel
+	//The number of goroutine is controlled by the const maxDeletionThreads
+	qSize := len(allVersions)
+	if qSize > maxDeletionThreads {
+		qSize = maxDeletionThreads
+	}
+	tokenQueue := make(chan struct{}, qSize)
+	errChan := make(chan error, 1)
+	waitGroup := new(sync.WaitGroup)
+	waitGroup.Add(len(allVersions))
+
+	//Append initial tokens
+	for i := 0; i < qSize; i++ {
+		tokenQueue <- struct{}{}
+	}
+
+	//Collect errors
+	errs := make([]error, 0)
+	errWrapper := make(chan error, 1)
+	go func() {
+		defer func() {
+			//pass to the out func
+			if len(errs) > 0 {
+				errWrapper <- fmt.Errorf("%v", errs)
+			}
+			close(errWrapper)
+		}()
+
+		for deletionErr := range errChan {
+			errs = append(errs, deletionErr)
+		}
+	}()
+
+	//Schedule deletion tasks
+	for _, deletingVersion := range allVersions {
+		//Apply for token first
+		//If no available token, pending here
+		<-tokenQueue
+
+		//Got one token
+		go func(deletingVersion *helm_repo.ChartVersion) {
+			defer func() {
+				//return the token back
+				tokenQueue <- struct{}{}
+
+				//done
+				waitGroup.Done()
+			}()
+
+			if err := uh.deleteChartVersion(namespace, chartName, deletingVersion.GetVersion()); err != nil {
+				errChan <- err
+			}
+		}(deletingVersion)
+	}
+
+	//Wait all goroutines are done
+	waitGroup.Wait()
+	//Safe to quit error collection goroutine
+	close(errChan)
+
+	err = <-errWrapper
+
+	return err
+}
+
+//deleteChartVersion deletes the specified chart version
+func (uh *UtilityHandler) deleteChartVersion(namespace, chartName, version string) error {
+	path := fmt.Sprintf("/api/%s/charts/%s/%s", namespace, chartName, version)
+	url := fmt.Sprintf("%s%s", uh.backendServerAddress.String(), path)
+
+	return uh.apiClient.DeleteContent(url)
 }
--- src/chartserver/utility_handler_test.go
+++ src/chartserver/utility_handler_test.go
@@ -42,3 +42,111 @@ func TestGetChartsByNs(t *testing.T) {
 		t.Fatalf("expect 2 items but got %d", len(charts))
 	}
 }
+
+//Test the function DeleteChart
+func TestDeleteChart(t *testing.T) {
+	mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch r.RequestURI {
+		case "/api/repo1/charts/harbor":
+			if r.Method == http.MethodGet {
+				w.Write([]byte(chartVersionsOfHarbor))
+				return
+			}
+		case "/api/repo1/charts/harbor/0.2.0",
+			"/api/repo1/charts/harbor/0.2.1":
+			if r.Method == http.MethodDelete {
+				w.WriteHeader(http.StatusOK)
+				return
+			}
+		}
+
+		w.WriteHeader(http.StatusNotImplemented)
+		w.Write([]byte("not supported"))
+	}))
+	defer mockServer.Close()
+
+	serverURL, err := url.Parse(mockServer.URL)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	theController, err := NewController(serverURL)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err := theController.GetUtilityHandler().DeleteChart("repo1", "harbor"); err != nil {
+		t.Fatal(err)
+	}
+}
+
+var chartVersionsOfHarbor = `
+[
+    {
+        "name": "harbor",
+        "home": "https://github.com/vmware/harbor",
+        "sources": [
+            "https://github.com/vmware/harbor/tree/master/contrib/helm/harbor"
+        ],
+        "version": "0.2.1",
+        "description": "An Enterprise-class Docker Registry by VMware",
+        "keywords": [
+            "vmware",
+            "docker",
+            "registry",
+            "harbor"
+        ],
+        "maintainers": [
+            {
+                "name": "Jesse Hu",
+                "email": "huh@vmware.com"
+            },
+            {
+                "name": "paulczar",
+                "email": "username.taken@gmail.com"
+            }
+        ],
+        "engine": "gotpl",
+        "icon": "https://raw.githubusercontent.com/vmware/harbor/master/docs/img/harbor_logo.png",
+        "appVersion": "1.5.0",
+        "urls": [
+            "charts/harbor-0.2.1.tgz"
+        ],
+        "created": "2018-08-29T10:26:29.625749155Z",
+        "digest": "2538edf4ddb797af8e025f3bd6226270440110bbdb689bad48656a519a154236"
+    },
+    {
+        "name": "harbor",
+        "home": "https://github.com/vmware/harbor",
+        "sources": [
+            "https://github.com/vmware/harbor/tree/master/contrib/helm/harbor"
+        ],
+        "version": "0.2.0",
+        "description": "An Enterprise-class Docker Registry by VMware",
+        "keywords": [
+            "vmware",
+            "docker",
+            "registry",
+            "harbor"
+        ],
+        "maintainers": [
+            {
+                "name": "Jesse Hu",
+                "email": "huh@vmware.com"
+            },
+            {
+                "name": "paulczar",
+                "email": "username.taken@gmail.com"
+            }
+        ],
+        "engine": "gotpl",
+        "icon": "https://raw.githubusercontent.com/vmware/harbor/master/docs/img/harbor_logo.png",
+        "appVersion": "1.5.0",
+        "urls": [
+            "charts/harbor-0.2.0.tgz"
+        ],
+        "created": "2018-08-29T10:26:21.141611102Z",
+        "digest": "fc8aae8dade9f0dfca12e9f1085081c49843d30a063a3fa7eb42497e3ceb277c"
+    }
+]
+`
