ba4762095f9aa2a3bddd4bd53a627c5394ef5110
Qian Deng <dengq@vmware.com>
2018-09-07 15:06:15 +0800

    Update dockerfile of portal based on angular6 upgrade
    1. Update entrypoint to reflect angular 6 upgrade
    
    Signed-off-by: Qian Deng <dengq@vmware.com>

--- make/photon/portal/entrypoint.sh
+++ make/photon/portal/entrypoint.sh
@@ -10,15 +10,9 @@ npm install
 
 ## Build harbor-ui and link it
 rm -rf /build_dir/lib/dist
-npm run build:lib
+npm run build_lib
 chmod -R +xr /build_dir/lib/dist
-cd /build_dir/lib/dist
-npm link
-cd /build_dir
-npm link harbor-ui
+npm run link_lib
 
 ## Build production
-npm run build:prod
-
-## Unlink
-npm unlink harbor-ui
+npm run release
