bb73e64f5d56bdfac6c62c037162933307b0483a
Steven Zou <szou@vmware.com>
2018-09-27 15:11:20 +0800

    Revert the changes to the travis pipeline to fix the build failure
    Signed-off-by: Steven Zou <szou@vmware.com>

--- tests/travis/api_run.sh
+++ tests/travis/api_run.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-source gskey.sh
+#source gskey.sh
 
-sudo gsutil version -l
+#sudo gsutil version -l
 
-harbor_logs_bucket="harbor-ci-logs"
+#harbor_logs_bucket="harbor-ci-logs"
 # GC credentials
-keyfile="/home/travis/harbor-ci-logs.key"
-botofile="/home/travis/.boto"
-echo -en $GS_PRIVATE_KEY > $keyfile
-sudo chmod 400 $keyfile
-echo "[Credentials]" >> $botofile
-echo "gs_service_key_file = $keyfile" >> $botofile
-echo "gs_service_client_id = $GS_CLIENT_EMAIL" >> $botofile
-echo "[GSUtil]" >> $botofile
-echo "content_language = en" >> $botofile
-echo "default_project_id = $GS_PROJECT_ID" >> $botofile
+#keyfile="/home/travis/harbor-ci-logs.key"
+#botofile="/home/travis/.boto"
+#echo -en $GS_PRIVATE_KEY > $keyfile
+#sudo chmod 400 $keyfile
+#echo "[Credentials]" >> $botofile
+#echo "gs_service_key_file = $keyfile" >> $botofile
+#echo "gs_service_client_id = $GS_CLIENT_EMAIL" >> $botofile
+#echo "[GSUtil]" >> $botofile
+#echo "content_language = en" >> $botofile
+#echo "default_project_id = $GS_PROJECT_ID" >> $botofile
 
 # GS util
-function uploader {
-    sudo gsutil cp $1 gs://$2/$1
-    sudo gsutil -D setacl public-read gs://$2/$1 &> /dev/null
-}
+#function uploader {
+#    sudo gsutil cp $1 gs://$2/$1
+#    sudo gsutil -D setacl public-read gs://$2/$1 &> /dev/null
+#}
 
-set +e
+#set +e
 
 docker ps
 # run db auth api cases
@@ -36,15 +36,15 @@ if [ "$1" = 'LDAP' ]; then
 fi
 
 ## --------------------------------------------- Upload Harbor CI Logs -------------------------------------------
-outfile="integration_logs_$TRAVIS_BUILD_NUMBER_$TRAVIS_COMMIT.tar.gz"
-sudo tar -zcvf $outfile output.xml log.html /var/log/harbor/*
-if [ -f "$outfile" ]; then
-    uploader $outfile $harbor_logs_bucket
-    echo "----------------------------------------------"
-    echo "Download test logs:"
-    echo "https://storage.googleapis.com/harbor-ci-logs/$outfile"
-    echo "----------------------------------------------"
-else
-    echo "No log output file to upload"
-fi
+#outfile="integration_logs_$TRAVIS_BUILD_NUMBER_$TRAVIS_COMMIT.tar.gz"
+#sudo tar -zcvf $outfile output.xml log.html /var/log/harbor/*
+#if [ -f "$outfile" ]; then
+#    uploader $outfile $harbor_logs_bucket
+#    echo "----------------------------------------------"
+#    echo "Download test logs:"
+#    echo "https://storage.googleapis.com/harbor-ci-logs/$outfile"
+#    echo "----------------------------------------------"
+#else
+#    echo "No log output file to upload"
+#fi
 
