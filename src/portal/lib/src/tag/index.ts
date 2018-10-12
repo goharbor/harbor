fd9e3dc2b88725cc2c593422dc9510023ee19350
Frank Kung <kfanjian@gmail.com>
2018-10-12 18:41:45 +0800

    Add build history component to Harbor UI Add build histroy section and navbar to tag-detail component Add i18n items of build history Format code.
    Signed-off-by: Frank Kung <kfanjian@gmail.com>

--- src/portal/lib/src/tag/index.ts
+++ src/portal/lib/src/tag/index.ts
@@ -1,11 +1,14 @@
-import { Type } from '@angular/core';
-import { TagComponent } from './tag.component';
-import { TagDetailComponent } from './tag-detail.component';
+import { Type } from "@angular/core";
+import { TagComponent } from "./tag.component";
+import { TagDetailComponent } from "./tag-detail.component";
+import { TagHistoryComponent } from "./tag-history.component";
 
-export * from './tag.component';
-export * from './tag-detail.component';
+export * from "./tag.component";
+export * from "./tag-detail.component";
+export * from "./tag-history.component";
 
 export const TAG_DIRECTIVES: Type<any>[] = [
   TagComponent,
-  TagDetailComponent
+  TagDetailComponent,
+  TagHistoryComponent
 ];
