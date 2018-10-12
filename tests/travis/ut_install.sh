4e8506de34d2bb7f022c25e9b96dbb6dfd6aa07a
Wenkai Yin <yinw@vmware.com>
2018-10-12 14:11:35 +0800

    Update golint repository used in travis
    The golint repository is updated, this commit changes it with the latest version
    
    Signed-off-by: Wenkai Yin <yinw@vmware.com>

--- tests/travis/ut_install.sh
+++ tests/travis/ut_install.sh
@@ -12,7 +12,7 @@ sudo apt-get update && sudo apt-get install -y libldap2-dev
 go get -d github.com/docker/distribution
 go get -d github.com/docker/libtrust
 go get -d github.com/lib/pq
-go get github.com/golang/lint/golint
+go get golang.org/x/lint/golint
 go get github.com/GeertJohan/fgt
 go get github.com/dghubble/sling
 go get github.com/stretchr/testify
