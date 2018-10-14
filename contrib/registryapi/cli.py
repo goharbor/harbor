bfc67083be28bf4ade5c82e9601e5d531868dc3e
s.mansoori <sepideh.nejat@gmail.com>
2018-10-14 13:35:20 +0330

    Remove old and unused tags in repositories
    Signed-off-by: s.mansoori <sepideh.nejat@gmail.com>

--- contrib/registryapi/cli.py
+++ contrib/registryapi/cli.py
@@ -7,7 +7,9 @@
 import argparse
 import sys
 import json
+from datetime import datetime
 from registry import RegistryApi
+from registry import RegistryException
 
 
 class ApiProxy(object):
@@ -22,6 +24,8 @@ class ApiProxy(object):
         self.register_callback("manifest", "list", self.list_manifest)
         self.register_callback("manifest", "delete", self.delete_manifest)
         self.register_callback("manifest", "get", self.get_manifest)
+        self.register_callback("tag", "remove_list", self.remove_tags_by_date)
+        self.register_callback("tag", "list_date", self.list_tag_date)
 
     def register_callback(self, target, action, func):
         """ register real actions """
@@ -32,7 +36,7 @@ class ApiProxy(object):
 
     def execute(self, target, action):
         """ execute """
-        print json.dumps(self.callbacks[target][action](), indent=4, sort_keys=True)
+        print json.dumps(self.callbacks[target][action](), indent=4, sort_keys=True, default=datetime_converter)
 
     def list_repo(self):
         """ list repo """
@@ -60,6 +64,46 @@ class ApiProxy(object):
             manifests.append({i: content})
         return manifests
 
+    def list_tag_date(self, tags=None):
+        """ sort tags """
+        manifests = list()
+        if tags is None:
+            tags = self.registry.getTagList(self.args.repo)["tags"]
+            if tags is None:
+                return None
+        tags_nums = len(tags)
+        for i in range(tags_nums):
+            if i % 10 == 0:
+                print "progress: {}%".format(100 * i / tags_nums)
+            try:
+                content = self.registry.getManifestWithConf(self.args.repo, tags[i])['configContent']['created']
+                tag_time = datetime.strptime(content.split('T')[0], "%Y-%m-%d")
+                manifests.append({'name': tags[i], 'date': tag_time})
+            except RegistryException:
+                pass
+        return manifests
+
+    def remove_tags_by_date(self):
+        self.args.num = None
+        repos = self.list_repo()
+        for repo in repos['repositories']:
+            print "repository: %s" % repo
+            tags = self.registry.getTagList(repo)["tags"]
+            tags_nums = len(tags)
+            print "number of tags in repo: %s" % str(tags_nums)
+            if tags_nums > int(self.args.number):
+                self.args.repo = repo
+                manifests = self.list_tag_date(tags)
+                sort_tags = sorted(manifests, key=lambda manifests: manifests['date'])
+                for i in range(tags_nums - int(self.args.number)):
+                    if not self.args.dryrun:
+                        self.args.tag = sort_tags[i].get('name')
+                        self.delete_tag()
+                    else:
+                        print "dry-run mode (no tags will be removed)"
+                    print "tag %s removed" % sort_tags[i].get('name')
+        return 'tags successfully removed'
+
     def delete_manifest(self):
         """ delete manifest """
         return self.registry.deleteManifest(self.args.repo, self.args.ref)
@@ -69,8 +113,13 @@ class ApiProxy(object):
         return self.registry.getManifestWithConf(self.args.repo, self.args.tag)
 
 
-# since just a script tool, we do not construct whole target->action->args 
-# structure with oo abstractions which has more flexibility, just register 
+def datetime_converter(o):
+    if isinstance(o, datetime):
+        return o.__str__()
+
+
+# since just a script tool, we do not construct whole target->action->args
+# structure with oo abstractions which has more flexibility, just register
 # parser directly
 def get_parser():
     """ return a parser """
@@ -78,11 +127,11 @@ def get_parser():
 
     parser.add_argument('--username', action='store', required=True, help='username')
     parser.add_argument('--password', action='store', required=True, help='password')
-    parser.add_argument('--registry_endpoint', action='store', required=True, 
+    parser.add_argument('--registry_endpoint', action='store', required=True,
             help='registry endpoint')
 
     subparsers = parser.add_subparsers(dest='target', help='target to operate on')
-    
+
     # repo target
     repo_target_parser = subparsers.add_parser('repo', help='target repository')
     repo_target_subparsers = repo_target_parser.add_subparsers(dest='action',
@@ -100,14 +149,20 @@ def get_parser():
     tag_delete_parser = tag_target_subparsers.add_parser('delete', help='delete tag')
     tag_delete_parser.add_argument('--repo', action='store', required=True, help='delete tags')
     tag_delete_parser.add_argument('--tag', action='store', required=True,
-            help='tag reference')
+                                   help='tag reference')
+    tag_list_delete_parser = tag_target_subparsers.add_parser('remove_list', help='remove list tags')
+    tag_list_delete_parser.add_argument('--number', action='store', required=True, help='remove list tags')
+    tag_list_delete_parser.add_argument('--dryrun', action='store_true', required=False, help='remove list tags')
+
+    tag_list_date_parser = tag_target_subparsers.add_parser('list_date', help='list tags by date created')
+    tag_list_date_parser.add_argument('--repo', action='store', required=True, help='list tags by date created')
 
     # manifest target
     manifest_target_parser = subparsers.add_parser('manifest', help='target manifest')
     manifest_target_subparsers = manifest_target_parser.add_subparsers(dest='action',
             help='manifest subcommand')
     manifest_list_parser = manifest_target_subparsers.add_parser('list', help='list manifests')
-    manifest_list_parser.add_argument('--repo', action='store', required=True, 
+    manifest_list_parser.add_argument('--repo', action='store', required=True,
             help='list manifests')
     manifest_delete_parser = manifest_target_subparsers.add_parser('delete', help='delete manifest')
     manifest_delete_parser.add_argument('--repo', action='store', required=True,
