f65947fe06dec33f9d4963b69f4e2bf0078e1f83
wang yan <wangyan@vmware.com>
2018-10-17 18:55:40 +0800

    Enable gcs deploy on travis
    At the gcs side, enable the Interoperability API to let gsutil
    to use key and secret to access storage.
    Use travis to encrypt the key and secret so that only travis could
    decode these values.
    
    Signed-off-by: wang yan <wangyan@vmware.com>

--- tests/travis/api_run.sh
+++ tests/travis/api_run.sh
@@ -1,29 +1,25 @@
 #!/bin/bash
 
-#source gskey.sh
+sudo gsutil version -l
 
-#sudo gsutil version -l
+harbor_logs_bucket="harbor-ci-logs"
 
-#harbor_logs_bucket="harbor-ci-logs"
 # GC credentials
-#keyfile="/home/travis/harbor-ci-logs.key"
-#botofile="/home/travis/.boto"
-#echo -en $GS_PRIVATE_KEY > $keyfile
-#sudo chmod 400 $keyfile
-#echo "[Credentials]" >> $botofile
-#echo "gs_service_key_file = $keyfile" >> $botofile
-#echo "gs_service_client_id = $GS_CLIENT_EMAIL" >> $botofile
-#echo "[GSUtil]" >> $botofile
-#echo "content_language = en" >> $botofile
-#echo "default_project_id = $GS_PROJECT_ID" >> $botofile
+botofile="/home/travis/.boto"
+echo "[Credentials]" >> $botofile
+echo "gs_access_key_id = $gs_access_key_id" >> $botofile
+echo "gs_secret_access_key = $gs_secret_access_key" >> $botofile
+echo "[GSUtil]" >> $botofile
+echo "content_language = en" >> $botofile
+echo "default_project_id = $default_project_id" >> $botofile
 
 # GS util
-#function uploader {
-#    sudo gsutil cp $1 gs://$2/$1
-#    sudo gsutil -D setacl public-read gs://$2/$1 &> /dev/null
-#}
+function uploader {
+    sudo gsutil cp $1 gs://$2/$1
+    sudo gsutil -D setacl public-read gs://$2/$1 &> /dev/null
+}
 
-#set +e
+set +e
 
 docker ps
 # run db auth api cases
@@ -36,15 +32,14 @@ if [ "$1" = 'LDAP' ]; then
 fi
 
 ## --------------------------------------------- Upload Harbor CI Logs -------------------------------------------
-#outfile="integration_logs_$TRAVIS_BUILD_NUMBER_$TRAVIS_COMMIT.tar.gz"
-#sudo tar -zcvf $outfile output.xml log.html /var/log/harbor/*
-#if [ -f "$outfile" ]; then
-#    uploader $outfile $harbor_logs_bucket
-#    echo "----------------------------------------------"
-#    echo "Download test logs:"
-#    echo "https://storage.googleapis.com/harbor-ci-logs/$outfile"
-#    echo "----------------------------------------------"
-#else
-#    echo "No log output file to upload"
-#fi
-
+outfile="integration_logs_$TRAVIS_BUILD_NUMBER_$TRAVIS_COMMIT.tar.gz"
+sudo tar -zcvf $outfile output.xml log.html /var/log/harbor/*
+if [ -f "$outfile" ]; then
+    uploader $outfile $harbor_logs_bucket
+    echo "----------------------------------------------"
+    echo "Download test logs:"
+    echo "https://storage.googleapis.com/harbor-ci-logs/$outfile"
+    echo "----------------------------------------------"
+else
+    echo "No log output file to upload"
+fi
