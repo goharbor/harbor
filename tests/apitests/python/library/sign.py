b1a545a762d3e4b6be7cc7ce409642e027be17e5
danfengliu <danfengl@vmware.com>
2018-11-08 18:30:03 +0800

    add os path Signed-off-by: danfengliu <danfengl@vmware.com>

--- tests/apitests/python/library/sign.py
+++ tests/apitests/python/library/sign.py
@@ -4,6 +4,7 @@ import commands
 import os
 
 def set_sign_env(registry_ip, project_name, image, tag):
+    print "current path:", os.getcwd()
     cmd = r"../../robot-cases/Group3-Upgrade/sign_image.sh {} {} {} {} ".format(registry_ip, project_name, image, tag)
     result_code, info = commands.getstatusoutput(cmd)
     if result_code != 0:
