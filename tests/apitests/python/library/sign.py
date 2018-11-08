719b087b0f05d1a00121ffd84b3a163b77d65d9a
danfengliu <danfengl@vmware.com>
2018-11-08 17:45:06 +0800

    modify sign.py Signed-off-by: danfengliu <danfengl@vmware.com>

--- tests/apitests/python/library/sign.py
+++ tests/apitests/python/library/sign.py
@@ -1,21 +1,10 @@
 # -*- coding: utf-8 -*-
 
 import commands
-import os,sys
-
-try:
-    import docker
-except ImportError:
-    import pip
-    pip.main(['install', 'docker'])
-    import docker
+import os
 
 def set_sign_env(registry_ip, project_name, image, tag):
-    result_code, info = commands.getstatusoutput(r"chmod 777 sign_image.sh")
-    if result_code != 0:
-        raise Exception("image %s:%s exists" % (result_code, info))
-
-    cmd = r"./sign_image.sh {} {} {} {} ".format(registry_ip, project_name, image, tag)
+    cmd = r"../../robot-cases/Group3-Upgrade/sign_image.sh {} {} {} {} ".format(registry_ip, project_name, image, tag)
     result_code, info = commands.getstatusoutput(cmd)
     if result_code != 0:
         raise Exception("image %s:%s exists" % (result_code, info))
--- tests/apitests/python/test_sign_image.py
+++ tests/apitests/python/test_sign_image.py
@@ -29,13 +29,13 @@ class TestProjects(unittest.TestCase):
     @unittest.skipIf(TEARDOWN == True, "Test data should be remain in the harbor.")
     def test_ClearData(self):
         #1. Delete repository(RA) by user(UA);
-        self.repo.delete_repoitory(TestProjects.repo_name, **TestProjects.USER_add_g_lbl_CLIENT)
+        self.repo.delete_repoitory(TestProjects.repo_name, **TestProjects.USER_sign_image_CLIENT)
 
         #2. Delete project(PA);
-        self.project.delete_project(TestProjects.project_add_g_lbl_id, **TestProjects.USER_add_g_lbl_CLIENT)
+        self.project.delete_project(TestProjects.project_sign_image_id, **TestProjects.USER_sign_image_CLIENT)
 
         #3. Delete user(UA);
-        self.user.delete_user(TestProjects.user_add_g_lbl_id, **TestProjects.ADMIN_CLIENT)
+        self.user.delete_user(TestProjects.user_sign_image_id, **TestProjects.ADMIN_CLIENT)
 
     def testSignImage(self):
         """
@@ -62,27 +62,26 @@ class TestProjects(unittest.TestCase):
         TestProjects.ADMIN_CLIENT=dict(endpoint = url, username = admin_user, password =  admin_pwd)
 
         #1. Create user-001
-        TestProjects.user_add_g_lbl_id, user_add_g_lbl_name = self.user.create_user_success(user_password = user_001_password, **TestProjects.ADMIN_CLIENT)
+        TestProjects.user_sign_image_id, user_sign_image_name = self.user.create_user_success(user_password = user_001_password, **TestProjects.ADMIN_CLIENT)
 
-        TestProjects.USER_add_g_lbl_CLIENT=dict(endpoint = url, username = user_add_g_lbl_name, password = user_001_password)
+        TestProjects.USER_sign_image_CLIENT=dict(endpoint = url, username = user_sign_image_name, password = user_001_password)
 
         #2. Create private project-001
-        TestProjects.project_add_g_lbl_id, project_add_g_lbl_name = self.project.create_project(metadata = {"public": "false"}, **TestProjects.ADMIN_CLIENT)
+        TestProjects.project_sign_image_id, project_sign_image_name = self.project.create_project(metadata = {"public": "false"}, **TestProjects.ADMIN_CLIENT)
 
         #3. Add user-001 as a member of project-001 with project-admin role
-        self.project.add_project_members(TestProjects.project_add_g_lbl_id, TestProjects.user_add_g_lbl_id, **TestProjects.ADMIN_CLIENT)
+        self.project.add_project_members(TestProjects.project_sign_image_id, TestProjects.user_sign_image_id, **TestProjects.ADMIN_CLIENT)
 
         #4. Get private project of uesr-001, uesr-001 can see only one private project which is project-001
         self.project.projects_should_exist(dict(public=False), expected_count = 1,
-            expected_project_id = TestProjects.project_add_g_lbl_id, **TestProjects.USER_add_g_lbl_CLIENT)
+            expected_project_id = TestProjects.project_sign_image_id, **TestProjects.USER_sign_image_CLIENT)
 
-        #image = "hello-world"
-        image = "mysql"
+        image = "hello-world"
         src_tag = "latest"
         #5. Create a new repository(RA) and tag(TA) in project(PA) by user(UA);
-        TestProjects.repo_name, tag = create_repository(project_add_g_lbl_name, harbor_server, user_add_g_lbl_name, user_001_password, image, src_tag)
-        set_sign_env(harbor_server, project_add_g_lbl_name, image, tag)
+        TestProjects.repo_name, tag = create_repository(project_sign_image_name, harbor_server, user_sign_image_name, user_001_password, image, src_tag)
+        set_sign_env(harbor_server, project_sign_image_name, image, tag)
 
-        self.repo.signature_should_exist(TestProjects.repo_name, tag, **TestProjects.USER_add_g_lbl_CLIENT)
+        self.repo.signature_should_exist(TestProjects.repo_name, tag, **TestProjects.USER_sign_image_CLIENT)
 if __name__ == '__main__':
     unittest.main()
