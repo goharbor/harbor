e3fbca56d4ab538c490ee88de4a6a168bd6b33d8
danfengliu <danfengl@vmware.com>
2018-11-09 15:41:44 +0800

    modify sign_image.py 1 Signed-off-by: danfengliu <danfengl@vmware.com>

--- tests/apitests/python/library/sign.py
+++ tests/apitests/python/library/sign.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from subprocess
+import subprocess
 
 def set_sign_env(registry_ip, project_name, image, tag):
     result_code = subprocess.call(["/home/travis/gopath/src/github.com/goharbor/harbor/tests/robot-cases/Group3-Upgrade/sign_image.sh", registry_ip, project_name, image, tag], shell=False)
