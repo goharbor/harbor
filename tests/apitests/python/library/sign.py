76fcbefec649f5bddecb954188f0c28f10d9e76b
danfengliu <danfengl@vmware.com>
2018-11-09 13:39:58 +0800

    modify path Signed-off-by: danfengliu <danfengl@vmware.com>

--- tests/apitests/python/library/sign.py
+++ tests/apitests/python/library/sign.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
 
-import commands
 import os
+import subprocess
 
 def set_sign_env(registry_ip, project_name, image, tag):
-    print "current path:", os.getcwd()
-    cmd = r"/home/travis/gopath/src/github.com/goharbor/harbor/test/robot-cases/Group3-Upgrade/sign_image.sh {} {} {} {} ".format(registry_ip, project_name, image, tag)
-    result_code, info = commands.getstatusoutput(cmd)
+    result_code = subprocess.call(["/home/travis/gopath/src/github.com/goharbor/harbor/tests/robot-cases/Group3-Upgrade/sign_image.sh", registry_ip, project_name, image, tag])
     if result_code != 0:
         raise Exception("image %s:%s exists" % (result_code, info))
 
