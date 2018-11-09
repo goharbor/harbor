6fca17aff8b1991a83c70a3964f53686a61f4bbf
danfengliu <danfengl@vmware.com>
2018-11-09 15:32:14 +0800

    some import change of sign.py Signed-off-by: danfengliu <danfengl@vmware.com>

--- tests/apitests/python/library/sign.py
+++ tests/apitests/python/library/sign.py
@@ -1,12 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from subprocess import call
-import shlex
+from subprocess
 
 def set_sign_env(registry_ip, project_name, image, tag):
-    cmd = r"/home/travis/gopath/src/github.com/goharbor/harbor/tests/robot-cases/Group3-Upgrade/sign_image.sh", registry_ip, project_name, image, tag
-    cmd = shlex(cmd)
-    result_code = subprocess.call(cmd, shell=False)
+    result_code = subprocess.call(["/home/travis/gopath/src/github.com/goharbor/harbor/tests/robot-cases/Group3-Upgrade/sign_image.sh", registry_ip, project_name, image, tag], shell=False)
     if result_code != 0:
         raise Exception("Failed to sign image error code is {}.".format(result_code))
 
