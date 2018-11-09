e910c6bd83b38ebf9a050a2eb9bd96a4199bfa50
danfengliu <danfengl@vmware.com>
2018-11-09 13:47:01 +0800

    add debug information Signed-off-by: danfengliu <danfengl@vmware.com>

--- tests/apitests/python/library/sign.py
+++ tests/apitests/python/library/sign.py
@@ -6,5 +6,5 @@ import subprocess
 def set_sign_env(registry_ip, project_name, image, tag):
     result_code = subprocess.call(["/home/travis/gopath/src/github.com/goharbor/harbor/tests/robot-cases/Group3-Upgrade/sign_image.sh", registry_ip, project_name, image, tag])
     if result_code != 0:
-        raise Exception("Failed to sign image error code is {} error info is {} ".format(result_code, info))
+        raise Exception("Failed to sign image error code is {} error info is {}.".format(result_code, info))
 
