4dbbb225f215109dc79c4af44a004ea59e7adb7e
danfengliu <danfengl@vmware.com>
2018-11-09 10:26:52 +0800

    add full path for signimage.sh Signed-off-by: danfengliu <danfengl@vmware.com>

--- tests/apitests/python/library/sign.py
+++ tests/apitests/python/library/sign.py
@@ -5,7 +5,7 @@ import os
 
 def set_sign_env(registry_ip, project_name, image, tag):
     print "current path:", os.getcwd()
-    cmd = r"../../robot-cases/Group3-Upgrade/sign_image.sh {} {} {} {} ".format(registry_ip, project_name, image, tag)
+    cmd = r"/home/travis/gopath/src/github.com/goharbor/harbor/test/robot-cases/Group3-Upgrade/sign_image.sh {} {} {} {} ".format(registry_ip, project_name, image, tag)
     result_code, info = commands.getstatusoutput(cmd)
     if result_code != 0:
         raise Exception("image %s:%s exists" % (result_code, info))
