292ecd59bcc792716d54b9fcdde31726d3aa2b56
Daniel Jiang <jiangd@vmware.com>
2019-01-11 18:16:50 +0800

    Provide HTTP authenticator
    An HTTP authenticator verifies the credentials by sending a POST request
    to an HTTP endpoint.  After successful authentication he will be
    onboarded to Harbor's local DB and assigned a role in a project.
    
    This commit provides the initial implementation.
    Currently one limitation is that we don't have clear definition about
    how we would "search" a user via this HTTP authenticator, a flag for
    "alway onboard" is provided to skip the search, otherwise, a user has
    to login first before he can be assigned a role in Harbor.
    
    Signed-off-by: Daniel Jiang <jiangd@vmware.com>
    Signed-off-by: Wenkai Yin <yinw@vmware.com>

---
+++ src/core/auth/authproxy/test/server.go
@@ -0,0 +1,49 @@
+// Copyright Project Harbor Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package test
+
+import (
+	"net/http"
+	"net/http/httptest"
+)
+
+type authHandler struct {
+	m map[string]string
+}
+
+// ServeHTTP handles HTTP requests
+func (ah *authHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		http.Error(rw, "", http.StatusMethodNotAllowed)
+	}
+	if u, p, ok := req.BasicAuth(); !ok {
+		// Simulate a service error
+		http.Error(rw, "", http.StatusInternalServerError)
+	} else if pass, ok := ah.m[u]; !ok || pass != p {
+		http.Error(rw, "", http.StatusUnauthorized)
+	} else {
+		_, e := rw.Write([]byte(`{"session_id": "hgx59wuWI3b0jcbtidv5mU1YCp-DOQ9NKR1iYKACdKCvbVn7"}`))
+		if e != nil {
+			panic(e)
+		}
+	}
+}
+
+// NewMockServer creates the mock server for testing
+func NewMockServer(creds map[string]string) *httptest.Server {
+	mux := http.NewServeMux()
+	mux.Handle("/test/login", &authHandler{m: creds})
+	return httptest.NewTLSServer(mux)
+}
