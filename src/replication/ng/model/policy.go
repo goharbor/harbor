5cbd608fc47b3b39a3c1d090ce1dbe2d394b2d5a
Wenkai Yin <yinw@vmware.com>
2019-01-22 17:02:45 +0800

    Refine the replication job
    1. Refine the replication job
    2. Define the Transfer interface
    3. Define the common models
    
    Signed-off-by: Wenkai Yin <yinw@vmware.com>

---
+++ src/replication/ng/model/policy.go
@@ -0,0 +1,70 @@
+// Copyright Project Harbor Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package model
+
+import "time"
+
+// Policy defines the structure of a replication policy
+type Policy struct {
+	ID          int64  `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Creator     string `json:"creator"`
+	// source
+	SrcRegistryID int64    `json:"src_registry_id"`
+	SrcNamespaces []string `json:"src_namespaces"`
+	// destination
+	DestRegistryID int64 `json:"dest_registry_id"`
+	// Only support two dest namespace modes:
+	// Put all the src resources to the one single dest namespace
+	// or keep namespaces same with the source ones (under this case,
+	// the DestNamespace should be set to empty)
+	DestNamespace string `json:"dest_namespace"`
+	// Filters
+	Filters []*Filter `json:"filters"`
+	// Trigger
+	Trigger *Trigger `json:"trigger"`
+	// Settings
+	Deletion bool `json:"deletion"`
+	// If override the image tag
+	Override bool `json:"override"`
+	// Operations
+	Enabled      bool      `json:"enabled"`
+	CreationTime time.Time `json:"creation_time"`
+	UpdateTime   time.Time `json:"update_time"`
+}
+
+// FilterType represents the type info of the filter.
+type FilterType string
+
+// Filter holds the info of the filter
+type Filter struct {
+	Type  FilterType  `json:"type"`
+	Value interface{} `json:"value"`
+}
+
+// TriggerType represents the type of trigger.
+type TriggerType string
+
+// Trigger holds info fot a trigger
+type Trigger struct {
+	Type     TriggerType      `json:"type"`
+	Settings *TriggerSettings `json:"trigger_settings"`
+}
+
+// TriggerSettings is the setting about the trigger
+type TriggerSettings struct {
+	Cron string `json:"cron"`
+}
