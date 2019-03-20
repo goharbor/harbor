25d1e698028111715519047d67a4543b129085f0
wang yan <wangyan@vmware.com>
2019-03-20 18:27:18 +0800

    Remove unused files from vendor by dep v0.5.1
    As for dep upgrade to v0.5.1, it removes all of unused files for vendor.
    
    Signed-off-by: wang yan <wangyan@vmware.com>

--- src/vendor/github.com/golang-migrate/migrate/cli/build_google-cloud-storage.go
+++
@@ -1,7 +0,0 @@
-// +build google_cloud_storage
-
-package main
-
-import (
-	_ "github.com/golang-migrate/migrate/source/google_cloud_storage"
-)
--- src/vendor/github.com/golang-migrate/migrate/source/driver_test.go
+++
@@ -1,8 +0,0 @@
-package source
-
-func ExampleDriver() {
-	// see source/stub for an example
-
-	// source/stub/stub.go has the driver implementation
-	// source/stub/stub_test.go runs source/testing/test.go:Test
-}
