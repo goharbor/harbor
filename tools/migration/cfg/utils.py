c1e676ad9921e90aed759f80aa4784427a49de0f
Qian Deng <dengq@vmware.com>
2019-04-09 15:51:51 +0800

    Add migration script from 1.7.0 to 1.8.0
    Add jinja2 to migrator
    Add template to migrator
    Add config upgrading script
    
    Signed-off-by: Qian Deng <dengq@vmware.com>

--- tools/migration/cfg/utils.py
+++ tools/migration/cfg/utils.py
@@ -43,3 +43,25 @@ def render(src, dest, **kw):
     t = Template(open(src, 'r').read())
     with open(dest, 'w') as f:
         f.write(t.substitute(**kw))
+
+def get_storage_provider_info(provider_name, provider_config):
+    provider_config = provider_config.strip('" ')
+    if not provider_config.strip(" "):
+        return ''
+
+    storage_provider_cfg_map = {}
+    for k_v in provider_config.split(","):
+        if k_v > 0:
+            kvs = k_v.split(": ") # add space suffix to avoid existing ":" in the value
+            if len(kvs) == 2:
+                #key must not be empty
+                if kvs[0].strip() != "":
+                    storage_provider_cfg_map[kvs[0].strip()] = kvs[1].strip()
+
+    # generate storage configuration section in yaml format
+
+    storage_provider_conf_list = [provider_name + ':']
+    for config in storage_provider_cfg_map.items():
+        storage_provider_conf_list.append('{}: {}'.format(*config))
+    storage_provider_info = ('\n' + ' ' * 4).join(storage_provider_conf_list)
+    return storage_provider_info
