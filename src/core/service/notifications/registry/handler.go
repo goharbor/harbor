5dfc3f2402554a9df67c051dbff5e018d5b18c39
mmpei <peimingming@corp.netease.com>
2019-07-01 17:54:52 +0800

    Optimize fetch manifest loop when handling notification
    Signed-off-by: mmpei <peimingming@corp.netease.com>

--- src/core/service/notifications/registry/handler.go
+++ src/core/service/notifications/registry/handler.go
@@ -111,7 +111,7 @@ func (n *NotificationHandler) Post() {
 				}()
 			}
 
-			if !coreutils.WaitForManifestReady(repository, tag, 5) {
+			if !coreutils.WaitForManifestReady(repository, tag, 6) {
 				log.Errorf("Manifest for image %s:%s is not ready, skip the follow up actions.", repository, tag)
 				return
 			}
