4946c7bba77217c4aeb85b38e2c936b22d53b35c
peimingming <peimingming@corp.netease.com>
2019-07-09 11:15:29 +0800

    Add helm chart replication from helmhub
    Signed-off-by: peimingming <peimingming@corp.netease.com>

---
+++ src/replication/adapter/helmhub/adapter_test.go
@@ -0,0 +1,44 @@
+// Copyright Project Harbor Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package helmhub
+
+import (
+	"testing"
+
+	"github.com/goharbor/harbor/src/replication/model"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestInfo(t *testing.T) {
+	adapter := &adapter{}
+	info, err := adapter.Info()
+	require.Nil(t, err)
+	require.Equal(t, 1, len(info.SupportedResourceTypes))
+	assert.Equal(t, model.ResourceTypeChart, info.SupportedResourceTypes[0])
+}
+
+func TestPrepareForPush(t *testing.T) {
+	adapter := &adapter{}
+	err := adapter.PrepareForPush(nil)
+	require.NotNil(t, err)
+}
+
+func TestHealthCheck(t *testing.T) {
+	adapter := &adapter{}
+	status, err := adapter.HealthCheck()
+	require.Equal(t, model.Healthy, string(status))
+	require.Nil(t, err)
+}
---
+++ src/replication/adapter/helmhub/client.go
@@ -0,0 +1,97 @@
+package helmhub
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/goharbor/harbor/src/replication/model"
+	"github.com/goharbor/harbor/src/replication/util"
+	"github.com/pkg/errors"
+	"io/ioutil"
+	"net/http"
+)
+
+// ErrHTTPNotFound defines the return error when receiving 404 response code
+var ErrHTTPNotFound = errors.New("Not Found")
+
+// Client is a client to interact with HelmHub
+type Client struct {
+	client *http.Client
+}
+
+// NewClient creates a new HelmHub client.
+func NewClient(registry *model.Registry) *Client {
+	return &Client{
+		client: &http.Client{
+			Transport: util.GetHTTPTransport(false),
+		},
+	}
+}
+
+// fetchCharts fetches the chart list from helm hub.
+func (c *Client) fetchCharts() (*chartList, error) {
+	request, err := http.NewRequest(http.MethodGet, baseURL+listCharts, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := c.client.Do(request)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("fetch chart list error %d: %s", resp.StatusCode, string(body))
+	}
+
+	list := &chartList{}
+	err = json.Unmarshal(body, list)
+	if err != nil {
+		return nil, fmt.Errorf("unmarshal chart list response error: %v", err)
+	}
+
+	return list, nil
+}
+
+// fetchChartDetail fetches the chart detail of a chart from helm hub.
+func (c *Client) fetchChartDetail(chartName string) (*chartVersionList, error) {
+	request, err := http.NewRequest(http.MethodGet, baseURL+listVersions(chartName), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := c.client.Do(request)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK && resp.StatusCode != http.StatusNotFound {
+		return nil, fmt.Errorf("fetch chart detail error %d: %s", resp.StatusCode, string(body))
+	} else if resp.StatusCode == http.StatusNotFound {
+		return nil, ErrHTTPNotFound
+	}
+
+	list := &chartVersionList{}
+	err = json.Unmarshal(body, list)
+	if err != nil {
+		return nil, fmt.Errorf("unmarshal chart detail response error: %v", err)
+	}
+
+	return list, nil
+}
+
+// do work as a proxy of Do function from net.http
+func (c *Client) do(req *http.Request) (*http.Response, error) {
+	return c.client.Do(req)
+}
