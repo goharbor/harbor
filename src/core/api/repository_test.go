5f1d2bd6441b0e2b1b19727d75d8822fed336408
Wenkai Yin <yinw@vmware.com>
2019-07-19 13:11:11 +0800

    Fix package import cycle issue
    Fix package import cycle issue
    
    Signed-off-by: Wenkai Yin <yinw@vmware.com>

--- src/core/api/repository_test.go
+++ src/core/api/repository_test.go
@@ -96,7 +96,7 @@ func TestGetReposTags(t *testing.T) {
 		t.Errorf("failed to get tags of repository %s: %v", repository, err)
 	} else {
 		assert.Equal(int(200), code, "httpStatusCode should be 200")
-		if tg, ok := tags.([]TagResp); ok {
+		if tg, ok := tags.([]models.TagResp); ok {
 			assert.Equal(1, len(tg), fmt.Sprintf("there should be only one tag, but now %v", tg))
 			assert.Equal(tg[0].Name, "latest", "the tag should be latest")
 		} else {
@@ -207,19 +207,19 @@ func TestGetReposTop(t *testing.T) {
 
 func TestPopulateAuthor(t *testing.T) {
 	author := "author"
-	detail := &tagDetail{
+	detail := &models.TagDetail{
 		Author: author,
 	}
 	populateAuthor(detail)
 	assert.Equal(t, author, detail.Author)
 
-	detail = &tagDetail{}
+	detail = &models.TagDetail{}
 	populateAuthor(detail)
 	assert.Equal(t, "", detail.Author)
 
 	maintainer := "maintainer"
-	detail = &tagDetail{
-		Config: &cfg{
+	detail = &models.TagDetail{
+		Config: &models.TagCfg{
 			Labels: map[string]string{
 				"Maintainer": maintainer,
 			},
