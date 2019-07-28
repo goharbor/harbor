44a9e5d12739d589b2dc682d587e0e3e1b1a9f38
gklp <gokalpkuscu@gmail.com>
2019-07-28 21:00:06 +0300

    All redirect operations should have content-type as json
    Signed-off-by: gklp <gokalpkuscu@gmail.com>

--- src/common/api/base.go
+++ src/common/api/base.go
@@ -112,6 +112,7 @@ func (b *BaseAPI) Redirect(statusCode int, resouceID string) {
 	requestURI := b.Ctx.Request.RequestURI
 	resourceURI := requestURI + "/" + resouceID
 
+	b.Ctx.ResponseWriter.Header().Set("Content-Type", "application/json")
 	b.Ctx.Redirect(statusCode, resourceURI)
 }
 
