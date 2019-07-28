37b7ab61746d7a98e1163485af40d65d2181c610
Daniel Jiang <jiangd@vmware.com>
2019-07-29 00:24:02 +0800

    Update GetRolesByGroupID
    This commit fixes #8432
    When querying the role of group ID, all matched roles should be returned
    instead of the minimal role ID.
    
    Signed-off-by: Daniel Jiang <jiangd@vmware.com>

--- src/common/dao/project.go
+++ src/common/dao/project.go
@@ -302,20 +302,15 @@ func GetRolesByGroupID(projectID int64, groupIDs []int) ([]int, error) {
 	}
 	groupIDCondition := JoinNumberConditions(groupIDs)
 	o := GetOrmer()
-	// the role is in descent order (1-admin, 2-developer, 3-guest, 4-master), use min to select the max privilege role.
 	sql := fmt.Sprintf(
-		`select min(pm.role) from project_member pm 
+		`select distinct pm.role from project_member pm 
 		left join user_group ug on pm.entity_type = 'g' and pm.entity_id = ug.id 
 		where ug.id in ( %s ) and pm.project_id = ?`,
 		groupIDCondition)
-	log.Debugf("sql:%v", sql)
+	log.Debugf("sql for GetRolesByGroupID(project ID: %d, group ids: %v):%v", projectID, groupIDs, sql)
 	if _, err := o.Raw(sql, projectID).QueryRows(&roles); err != nil {
 		log.Warningf("Error in GetRolesByGroupID, error: %v", err)
 		return nil, err
-	}
-	// If there is no row selected, the min returns an empty row, to avoid return 0 as role
-	if len(roles) == 1 && roles[0] == 0 {
-		return []int{}, nil
 	}
 	return roles, nil
 }
