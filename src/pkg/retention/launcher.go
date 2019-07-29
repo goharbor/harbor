adc6f06f376a7e547730e66c2f96737b18e2d241
Wenkai Yin <yinw@vmware.com>
2019-07-29 15:32:35 +0800

    Register the scheduler job and fix bugs
    1. Register the scheduler job
    2. Fix retention bugs
    
    Signed-off-by: Wenkai Yin <yinw@vmware.com>

--- src/pkg/retention/launcher.go
+++ src/pkg/retention/launcher.go
@@ -91,8 +91,8 @@ type launcher struct {
 }
 
 type jobData struct {
-	repository *res.Repository
-	policy     *lwp.Metadata
+	repository res.Repository
+	policy     lwp.Metadata
 	taskID     int64
 }
 
@@ -195,8 +195,8 @@ func (l *launcher) Launch(ply *policy.Metadata, executionID int64, isDryRun bool
 			return 0, launcherError(err)
 		}
 		jobDatas = append(jobDatas, &jobData{
-			repository: &repo,
-			policy:     p,
+			repository: repo,
+			policy:     *p,
 			taskID:     taskID,
 		})
 	}
