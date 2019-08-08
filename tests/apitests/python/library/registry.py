1b0931c034788f7ee525741621fd7be24837f4d0
danfengliu <danfengl@vmware.com>
2019-08-08 16:16:02 +0800

    Since harbor 1.8 released, API test for replication was not update yet, so in this PR, replicaiton test will be adapt for harbor 1.8.
    Signed-off-by: danfengliu <danfengl@vmware.com>

--- tests/apitests/python/library/registry.py
+++ tests/apitests/python/library/registry.py
@@ -5,15 +5,19 @@ import base
 import swagger_client
 
 class Registry(base.Base):
-    def create_registry(self, endpoint, name=None, username="", 
-        password="", insecure=True, **kwargs):
-        if name is None:
-            name = base._random_name("registry")
+    def create_registry(self, url, registry_type= "harbor", description="", credentialType = "basic",
+            access_key = "admin", access_secret = "Harbor12345", name=base._random_name("registry"),
+            insecure=True, expect_status_code = 201, **kwargs):
+
         client = self._get_client(**kwargs)
-        registry = swagger_client.RepTargetPost(name=name, endpoint=endpoint, 
-            username=username, password=password, insecure=insecure)
-        _, _, header = client.targets_post_with_http_info(registry)
-        return base._get_id_from_header(header), name
+        registryCredential = swagger_client.RegistryCredential(type=credentialType, access_key=access_key, access_secret=access_secret)
+        registry = swagger_client.Registry(name=name, url=url,
+                                           description= description, type=registry_type,
+                                           insecure=insecure, credential=registryCredential)
+
+        _, status_code, header = client.registries_post_with_http_info(registry)
+        base._assert_status_code(expect_status_code, status_code)
+        return base._get_id_from_header(header), _
 
     def get_registry_id_by_endpoint(self, endpoint, **kwargs):
         client = self._get_client(**kwargs)
@@ -22,3 +26,8 @@ class Registry(base.Base):
             if registry.endpoint == endpoint:
                 return registry.id
         raise Exception("registry %s not found" % endpoint)
+
+    def delete_registry(self, registry_id, expect_status_code = 200, **kwargs):
+        client = self._get_client(**kwargs)
+        _, status_code, _  = client.registries_id_delete_with_http_info(registry_id)
+        base._assert_status_code(expect_status_code, status_code)
