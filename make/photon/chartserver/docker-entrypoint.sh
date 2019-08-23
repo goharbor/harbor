93fa9fdabf5d6ee6c6dd71bb690dc9ec91084d27
Hongliang Wang <hongliangwang@yunify.com>
2019-08-23 13:45:38 +0800

    Allow chartserver work with NFS
    NFS server doesn't allow change owner, which is the same as what we do in registry/entrypoint.sh.
    
    Signed-off-by: Hongliang Wang <hongliangwang@yunify.com>

--- make/photon/chartserver/docker-entrypoint.sh
+++ make/photon/chartserver/docker-entrypoint.sh
@@ -4,7 +4,12 @@ set -e
 #/chart_storage is the directory in the contaienr for storing the chart artifacts
 #if storage driver is set to 'local'
 if [ -d /chart_storage ]; then
-    chown 10000:10000 -R /chart_storage
+    if ! stat -c '%u:%g' /chart_storage | grep -q '10000:10000' ; then
+        # 10000 is the id of harbor user/group.
+        # Usually NFS Server does not allow changing owner of the export directory,
+        # so need to skip this step and requires NFS Server admin to set its owner to 10000.
+        chown 10000:10000 -R /chart_storage
+    fi
 fi
 
 /harbor/install_cert.sh
