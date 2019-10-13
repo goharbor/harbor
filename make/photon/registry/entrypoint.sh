1c76d52152aeda582c635cce78a806c76ac8ecd7
DQ <dengq@vmware.com>
2019-10-13 10:27:06 +0000

    Add registryctl to non-root
    And the install_cert.sh will changed for non-root too
    
    Signed-off-by: DQ <dengq@vmware.com>

--- make/photon/registry/entrypoint.sh
+++ make/photon/registry/entrypoint.sh
@@ -4,24 +4,10 @@ set -e
 
 # The directory /var/lib/registry is within the container, and used to store image in CI testing.
 # So for now we need to chown to it to avoid failure in CI.
-if [ -d /var/lib/registry ]; then
-    chown 10000:10000 -R /var/lib/registry
-fi
+# if [ -d /var/lib/registry ]; then
+#     chown 10000:10000 -R /var/lib/registry
+# fi
 
-if [ -d /storage ]; then
-    if ! stat -c '%u:%g' /storage | grep -q '10000:10000' ; then
-        # 10000 is the id of harbor user/group.
-        # Usually NFS Server does not allow changing owner of the export directory,
-        # so need to skip this step and requires NFS Server admin to set its owner to 10000.
-        chown 10000:10000 -R /storage
-    fi
-fi
+/home/harbor/install_cert.sh
 
-/harbor/install_cert.sh
-
-case "$1" in
-    *.yaml|*.yml) set -- registry serve "$@" ;;
-    serve|garbage-collect|help|-*) set -- registry "$@" ;;
-esac
-
-sudo -E -u \#10000 "$@"
+/usr/bin/registry serve /etc/registry/config.yml
