9f2f97827d9ff552a0aa6c6aba20c45bb6da98bf
Ziming Zhang <zziming@vmware.com>
2020-01-19 20:24:58 +0800

    test coverage2
    Signed-off-by: Ziming Zhang <zziming@vmware.com>
    Change-Id: Ida455d900cf8b88414190ff3a4775c369ff5ae81

--- tests/travis/ut_run.sh
+++ tests/travis/ut_run.sh
@@ -15,4 +15,4 @@ docker ps
 
 go test -race -i ./src/core ./src/jobservice
 sudo -E env "PATH=$PATH" "POSTGRES_MIGRATION_SCRIPTS_PATH=/home/travis/gopath/src/github.com/goharbor/harbor/make/migrations/postgresql/" ./tests/coverage4gotest.sh
-goveralls -coverprofile=profile.cov -service=travis-ci || true
+goveralls -coverprofile=profile.cov -service=travis-ci -debug || true
