0407f67bd16e5c8183573cbfcbad444efa268bbb
Yogi_Wang <yawang@vmware.com>
2020-02-06 20:15:23 +0800

    Add route Signed-off-by: Yogi_Wang <yawang@vmware.com>

--- src/portal/src/lib/components/artifact/tag-cards/tag.ts
+++
@@ -1,7 +0,0 @@
-import { Tag } from "../../../services";
-import { LabelState } from "../artifact-list-tab.component";
-
-export interface TagUi extends Tag {
-    showLabels: LabelState[];
-    labelFilterName: "";
-}
