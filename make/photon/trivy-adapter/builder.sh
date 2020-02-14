1b60bb255ce30f25fe0107970284ce72701cd46c
Daniel Pacak <pacak.daniel@gmail.com>
2020-02-14 11:58:51 +0100

    refactor(Makefile): Add variables for download URLs
    Signed-off-by: Daniel Pacak <pacak.daniel@gmail.com>

--- make/photon/trivy-adapter/builder.sh
+++ make/photon/trivy-adapter/builder.sh
@@ -11,25 +11,25 @@ VERSION="$1"
 
 set -e
 
-cd `dirname $0`
+cd $(dirname $0)
 cur=$PWD
 
-# the temp folder to store distribution source code...
-TEMP=`mktemp -d ${TMPDIR-/tmp}/trivy-adapter.XXXXXX`
+# The temporary directory to clone Trivy adapter source code
+TEMP=$(mktemp -d ${TMPDIR-/tmp}/trivy-adapter.XXXXXX)
 git clone https://github.com/aquasecurity/harbor-scanner-trivy.git $TEMP
 cd $TEMP; git checkout $VERSION; cd -
 
-echo 'build the trivy adapter binary bases on the golang:1.13.4'
+echo "Building Trivy adapter binary based on golang:1.13.4..."
 cp Dockerfile.binary $TEMP
 docker build -f $TEMP/Dockerfile.binary -t trivy-adapter-golang $TEMP
 
-echo 'copy the trivy adapter binary to local...'
+echo "Copying Trivy adapter binary from the container to the local directory..."
 ID=$(docker create trivy-adapter-golang)
 docker cp $ID:/go/src/github.com/aquasecurity/harbor-scanner-trivy/scanner-trivy binary
 
 docker rm -f $ID
 docker rmi -f trivy-adapter-golang
 
-echo "Build trivy adapter binary success, then to build photon image..."
+echo "Building Trivy adapter binary finished successfully"
 cd $cur
 rm -rf $TEMP
