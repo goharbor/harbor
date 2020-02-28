c33d554c4283730616d87ab7b1a829a7e9584954
peimingming <peimingming@corp.netease.com>
2020-02-28 22:08:27 +0800

    add support kraken preheat
    Signed-off-by: peimingming <peimingming@corp.netease.com>

--- src/pkg/p2p/preheat/provider/factory.go
+++ src/pkg/p2p/preheat/provider/factory.go
@@ -11,3 +11,8 @@ type Factory func(meta *models.Metadata) (Driver, error)
 func DragonflyFactory(meta *models.Metadata) (Driver, error) {
 	return &DragonflyDriver{meta}, nil
 }
+
+// KrakenFactory creates kraken driver
+func KrakenFactory(meta *models.Metadata) (Driver, error) {
+	return &KrakenDriver{meta}, nil
+}
