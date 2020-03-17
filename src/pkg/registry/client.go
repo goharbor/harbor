e8935dd80492de4d0d21c7258be04dc361df1172
Wenkai Yin <yinw@vmware.com>
2020-03-17 16:32:11 +0800

    Filter the pulling manifest request from replication service
    Filter the pulling manifest request from replication service so that the audit log will not record the pulling action
    
    Signed-off-by: Wenkai Yin <yinw@vmware.com>

--- src/pkg/registry/client.go
+++ src/pkg/registry/client.go
@@ -55,6 +55,11 @@ var (
 	}
 )
 
+// const definition
+const (
+	UserAgent = "harbor-registry-client"
+)
+
 // Client defines the methods that a registry client should implements
 type Client interface {
 	// Ping the base API endpoint "/v2/"
@@ -503,6 +508,7 @@ func (c *client) do(req *http.Request) (*http.Response, error) {
 			return nil, err
 		}
 	}
+	req.Header.Set(http.CanonicalHeaderKey("User-Agent"), UserAgent)
 	resp, err := c.client.Do(req)
 	if err != nil {
 		return nil, err
