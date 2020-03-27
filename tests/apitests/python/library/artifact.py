eccb8aa708f0a8f8aa1c9a0d5db7c5636aeff869
Wang Yan <wangyan@vmware.com>
2020-03-27 17:10:04 +0800

    append pull permission for push policy (#11303)
    Fixes #11225
    As registry changes to basic auth, the push action lost the pull permission.
    Add it in the robot security context.
    
    Signed-off-by: wang yan <wangyan@vmware.com>

--- tests/apitests/python/library/artifact.py
+++ tests/apitests/python/library/artifact.py
@@ -82,6 +82,7 @@ class Artifact(base.Base, object):
             if (timeout_count == 0):
                 break
             artifact = self.get_reference_info(project_name, repo_name, reference, **kwargs)
+            print "artifact", artifact
             scan_status = artifact[0].scan_overview['application/vnd.scanner.adapter.vuln.report.harbor+json; version=1.0']["scan_status"]
             if scan_status == expected_scan_status:
                 return
