34d05dae5807c485a6696b03a6630d45405a63c9
Wang Yan <wangyan@vmware.com>
2020-03-31 11:12:21 +0800

    fix content trust middleware bypass scanner pull (#11321)
    Fixes #11206
    1, fix middleware doesn't work for docker pull without auth
    2, fix middleware doesn't bypass scanner pull
    
    Signed-off-by: wang yan <wangyan@vmware.com>

--- src/server/middleware/contenttrust/contenttrust.go
+++ src/server/middleware/contenttrust/contenttrust.go
@@ -1,83 +1,66 @@
 package contenttrust
 
 import (
-	"net/http"
-	"net/http/httptest"
-
+	"fmt"
 	"github.com/goharbor/harbor/src/common/rbac"
 	"github.com/goharbor/harbor/src/common/security"
 	"github.com/goharbor/harbor/src/controller/project"
+	"github.com/goharbor/harbor/src/jobservice/logger"
 	"github.com/goharbor/harbor/src/lib"
 	"github.com/goharbor/harbor/src/lib/errors"
 	"github.com/goharbor/harbor/src/pkg/signature"
-	serror "github.com/goharbor/harbor/src/server/error"
 	"github.com/goharbor/harbor/src/server/middleware"
+	"net/http"
 )
 
-// NotaryEndpoint ...
-var NotaryEndpoint = ""
+var (
+	// isArtifactSigned use the sign manager to check the signature, it could handle pull by tag or digtest
+	// if pull by digest, any tag of the artifact is signed, will return true.
+	isArtifactSigned = func(req *http.Request, art lib.ArtifactInfo) (bool, error) {
+		checker, err := signature.GetManager().GetCheckerByRepo(req.Context(), art.Repository)
+		if err != nil {
+			return false, err
+		}
+		return checker.IsArtifactSigned(art.Digest), nil
+	}
+)
 
 // Middleware handle docker pull content trust check
 func Middleware() func(http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-			doContentTrustCheck, mf := validate(req)
-			if !doContentTrustCheck {
-				next.ServeHTTP(rw, req)
-				return
-			}
-			rec := httptest.NewRecorder()
-			next.ServeHTTP(rec, req)
-			if rec.Result().StatusCode == http.StatusOK {
-				match, err := isArtifactSigned(req, mf)
-				if err != nil {
-					serror.SendError(rw, err)
-					return
-				}
-				if !match {
-					pkgE := errors.New(nil).WithCode(errors.PROJECTPOLICYVIOLATION).WithMessage("The image is not signed in Notary.")
-					serror.SendError(rw, pkgE)
-					return
-				}
-			}
-			middleware.CopyResp(rec, rw)
-		})
-	}
-}
+	return middleware.BeforeRequest(func(r *http.Request) error {
+		ctx := r.Context()
+		none := lib.ArtifactInfo{}
+		if err := middleware.EnsureArtifactDigest(ctx); err != nil {
+			return err
+		}
+		af := lib.GetArtifactInfo(ctx)
+		if af == none {
+			return fmt.Errorf("artifactinfo middleware required before this middleware")
+		}
+		pro, err := project.Ctl.GetByName(ctx, af.ProjectName)
+		if err != nil {
+			return err
+		}
+		securityCtx, ok := security.FromContext(ctx)
+		// only authenticated robot account with scanner pull access can bypass.
+		if ok && securityCtx.IsAuthenticated() &&
+			securityCtx.Name() == "robot" &&
+			securityCtx.Can(rbac.ActionScannerPull, rbac.NewProjectNamespace(pro.ProjectID).Resource(rbac.ResourceRepository)) {
+			// the artifact is pulling by the scanner, skip the checking
+			logger.Debugf("artifact %s@%s is pulling by the scanner, skip the checking", af.Repository, af.Digest)
+			return nil
+		}
 
-func validate(req *http.Request) (bool, lib.ArtifactInfo) {
-	none := lib.ArtifactInfo{}
-	if err := middleware.EnsureArtifactDigest(req.Context()); err != nil {
-		return false, none
-	}
-	af := lib.GetArtifactInfo(req.Context())
-	if af == none {
-		return false, none
-	}
-	pro, err := project.Ctl.GetByName(req.Context(), af.ProjectName)
-	if err != nil {
-		return false, none
-	}
-	resource := rbac.NewProjectNamespace(pro.ProjectID).Resource(rbac.ResourceRepository)
-	securityCtx, ok := security.FromContext(req.Context())
-	if !ok {
-		return false, none
-	}
-	if !securityCtx.Can(rbac.ActionScannerPull, resource) {
-		return false, none
-	}
-	if !middleware.GetPolicyChecker().ContentTrustEnabled(af.ProjectName) {
-		return false, af
-	}
-	return true, af
-}
-
-// isArtifactSigned use the sign manager to check the signature, it could handle pull by tag or digtest
-// if pull by digest, any tag of the artifact is signed, will return true.
-func isArtifactSigned(req *http.Request, art lib.ArtifactInfo) (bool, error) {
-	checker, err := signature.GetManager().GetCheckerByRepo(req.Context(), art.Repository)
-	if err != nil {
-		return false, err
-	}
-	return checker.IsArtifactSigned(art.Digest), nil
+		if pro.ContentTrustEnabled() {
+			match, err := isArtifactSigned(r, af)
+			if err != nil {
+				return err
+			}
+			if !match {
+				pkgE := errors.New(nil).WithCode(errors.PROJECTPOLICYVIOLATION).WithMessage("The image is not signed in Notary.")
+				return pkgE
+			}
+		}
+		return nil
+	})
 }
