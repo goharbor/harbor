572ebef685c3d07ee92dea9beccabaca12e0ec23
Ziming Zhang <zziming@vmware.com>
2020-04-01 19:46:45 +0800

    feat(cicd) parameterize docker base image and external url
    Signed-off-by: Ziming Zhang <zziming@vmware.com>

--- tests/ci/ut_install.sh
+++ tests/ci/ut_install.sh
@@ -21,7 +21,7 @@ sudo rm -rf /data/*
 sudo -E env "PATH=$PATH" make go_check
 sudo ./tests/hostcfg.sh
 sudo ./tests/generateCerts.sh
-sudo make -f make/photon/Makefile _build_db _build_registry _build_prepare -e VERSIONTAG=dev -e REGISTRYVERSION=${REG_VERSION} -e BASEIMAGETAG=dev
+sudo make build -e BUILDTARGET="_build_db _build_registry _build_prepare"
 docker run --rm -v /:/hostfs:z goharbor/prepare:dev gencert -p /etc/harbor/tls/internal
 sudo MAKEPATH=$(pwd)/make ./make/prepare
 sudo mkdir -p "/data/redis"
