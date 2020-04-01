b2e1905e7a744afc928c9cc37fe5d6ef4f171732
DQ <dengq@vmware.com>
2020-04-01 15:35:49 +0800

    Enhance: Stop upgrade when input version less then 1.9.0
    The migration script should failure early when version is not supported
    
    Signed-off-by: DQ <dengq@vmware.com>

--- make/photon/prepare/commands/migrate.py
+++ make/photon/prepare/commands/migrate.py
@@ -1,15 +1,21 @@
 import os, sys, importlib, shutil, glob
+from packaging import version
 
 import click
 
 from utils.misc import get_realpath
 from utils.migration import read_conf, search
+from migrations import accept_versions
 
 @click.command()
-@click.option('-i', '--input', 'input_', default='', help="The path of original config file")
+@click.option('-i', '--input', 'input_', help="The path of original config file")
 @click.option('-o', '--output', default='', help="the path of output config file")
 @click.option('-t', '--target', default='2.0.0', help="target version of input path")
 def migrate(input_, output, target):
+    if target not in accept_versions:
+        click.echo('target version {} not supported'.format(target))
+        sys.exit(-1)
+
     if not output:
         output = input_
     input_path = get_realpath(input_)
@@ -18,6 +24,8 @@ def migrate(input_, output, target):
     configs = read_conf(input_path)
 
     input_version = configs.get('_version')
+    if version.parse(input_version) < version.parse('1.9.0'):
+        click.echo('the version {} not supported, make sure the version in input file above 1.8.0'.format(input_version))
     
     if input_version == target:
         click.echo("Version of input harbor.yml is identical to target {}, no need to upgrade".format(input_version))
