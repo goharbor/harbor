4f49c0f29b36bdb7ffdb64de23daa67d5c5a15e0
DQ <dengq@vmware.com>
2020-05-22 17:35:35 +0800

    Fix unconsistent error issue
    if chart's namespace not exist it should rturn 401
    
    Signed-off-by: DQ <dengq@vmware.com>

--- src/core/api/chart_repository.go
+++ src/core/api/chart_repository.go
@@ -442,7 +442,7 @@ func (cra *ChartRepositoryAPI) requireNamespace(namespace string) bool {
 
 	// Not existing
 	if !existing {
-		cra.SendBadRequestError(fmt.Errorf("namespace %s is not existing", namespace))
+		cra.SendUnAuthorizedError(errors.New("UnAuthorized"))
 		return false
 	}
 
