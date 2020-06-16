1d960d6e139a5c6190112f8d10cb9a13cc8df878
liangfb <liang.fb@gmail.com>
2020-06-16 14:48:55 +0000

    Add aws China regions ECR support, both of cn-north-1 and cn-northwest-1 endpoints.
    Signed-off-by: liangfb <liang.fb@gmail.com>

--- src/portal/src/lib/components/endpoint/endpoint.component.spec.ts
+++ src/portal/src/lib/components/endpoint/endpoint.component.spec.ts
@@ -185,7 +185,16 @@ describe("EndpointComponent (inline template)", () => {
           {
             "key": "sa-east-1",
             "value": "https://api.ecr.sa-east-1.amazonaws.com"
+          },
+          {
+            "key": "cn-north-1",
+            "value": "https://api.ecr.cn-north-1.amazonaws.com.cn"
+          },
+          {
+            "key": "cn-northwest-1",
+            "value": "https://api.ecr.cn-northwest-1.amazonaws.com.cn"
           }
+
         ]
       },
       "credential_pattern": null
