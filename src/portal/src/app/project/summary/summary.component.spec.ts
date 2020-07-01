a53968d4623c5719f3b2d4b0257765db38ebf914
AllForNothing <sshijun@vmware.com>
2020-07-01 17:55:26 +0800

    Add proxy cache ui
    Signed-off-by: AllForNothing <sshijun@vmware.com>
    Signed-off-by: molinber <bertrand.molin@credit-agricole.net>

--- src/portal/src/app/project/summary/summary.component.spec.ts
+++ src/portal/src/app/project/summary/summary.component.spec.ts
@@ -6,14 +6,23 @@ import { ActivatedRoute } from '@angular/router';
 import { of } from 'rxjs';
 import { AppConfigService } from "../../services/app-config.service";
 import { SummaryComponent } from './summary.component';
-import { ProjectService, UserPermissionService } from "../../../lib/services";
+import { EndpointDefaultService, EndpointService, ProjectService, UserPermissionService } from '../../../lib/services';
 import { ErrorHandler } from "../../../lib/utils/error-handler";
+import { IServiceConfig, SERVICE_CONFIG } from '../../../lib/entities/service.config';
+import { CURRENT_BASE_HREF } from '../../../lib/utils/utils';
+import { SessionService } from '../../shared/session.service';
 
 
 describe('SummaryComponent', () => {
   let component: SummaryComponent;
   let fixture: ComponentFixture<SummaryComponent>;
-  let fakeAppConfigService = null;
+  let fakeAppConfigService = {
+    getConfig() {
+      return {
+        with_chartmuseum: false
+      };
+    }
+  };
   let fakeProjectService = {
     getProjectSummary: function () {
       return of();
@@ -24,6 +33,34 @@ describe('SummaryComponent', () => {
     hasProjectPermissions: function() {
       return of([true, true]);
     }
+  };
+  const config: IServiceConfig = {
+    systemInfoEndpoint: CURRENT_BASE_HREF + "/endpoints/testing"
+  };
+
+  const fakedSessionService = {
+    getCurrentUser() {
+      return {
+        has_admin_role: true
+      };
+    }
+  };
+
+  const fakedEndpointService = {
+    getEndpoint() {
+      return of({
+        name: "test",
+        url: "https://test.com"
+      });
+    }
+  };
+
+  const mockedSummaryInformation = {
+    repo_count: 0,
+    chart_count: 0,
+    project_admin_count: 1,
+    master_count: 0,
+    developer_count: 0
   };
 
   beforeEach(async(() => {
@@ -42,14 +79,19 @@ describe('SummaryComponent', () => {
         { provide: ProjectService, useValue: fakeProjectService },
         { provide: ErrorHandler, useValue: fakeErrorHandler },
         { provide: UserPermissionService, useValue: fakeUserPermissionService },
+        { provide: EndpointService, useValue: fakedEndpointService },
+        { provide: SERVICE_CONFIG, useValue: config },
+        { provide: SessionService, useValue:  fakedSessionService},
         {
           provide: ActivatedRoute, useValue: {
             paramMap: of({ get: (key) => 'value' }),
             snapshot: {
               parent: {
-                params: { id: 1 }
+                params: { id: 1 },
+                data: {
+                  projectResolver: {registry_id: 3}
+                }
               },
-              data: 1
             }
           }
         },
@@ -65,5 +107,14 @@ describe('SummaryComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show proxy cache endpoint', async () => {
+    component.summaryInformation = mockedSummaryInformation;
+    fixture.detectChanges();
+    await fixture.whenStable();
+    const endpoint: HTMLElement = fixture.nativeElement.querySelector("#endpoint");
+    expect(endpoint).toBeTruthy();
+    expect(endpoint.innerText).toEqual("test-https://test.com");
   });
 });
