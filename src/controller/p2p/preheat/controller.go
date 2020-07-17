56caee2c36f419c3fe7b42656d452d3368948b87
Steven Zou <szou@vmware.com>
2020-07-17 16:11:43 +0800

    fix(preheat):remove health property check when saving instance
    Signed-off-by: Steven Zou <szou@vmware.com>

--- src/controller/p2p/preheat/controller.go
+++ src/controller/p2p/preheat/controller.go
@@ -165,10 +165,8 @@ func (c *controller) CreateInstance(ctx context.Context, instance *providerModel
 		return 0, ErrorConflict
 	}
 
-	// !WARN: Check healthy status at fronted.
-	if instance.Status != "healthy" {
-		return 0, ErrorUnhealthy
-	}
+	// !WARN: We don't check the health of the instance here.
+	// That is ok because the health of instance will be checked before enforcing the policy each time.
 
 	instance.SetupTimestamp = time.Now().Unix()
 
