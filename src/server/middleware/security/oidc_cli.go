14b47cf05cdcd4faab90d5e662d2d7a24f5d241f
Daniel Jiang <jiangd@vmware.com>
2020-07-30 00:21:27 +0800

    Refine request checking for OIDC CLI secret (#12596)
    This commit makes OIDC CLI secret filter allow more URLs so that the
    OIDC CLI secret can be used for replication
    
    Signed-off-by: Daniel Jiang <jiangd@vmware.com>

--- src/server/middleware/security/oidc_cli.go
+++ src/server/middleware/security/oidc_cli.go
@@ -17,6 +17,7 @@ package security
 import (
 	"fmt"
 	"net/http"
+	"regexp"
 	"strings"
 
 	"github.com/goharbor/harbor/src/common"
@@ -29,31 +30,68 @@ import (
 	"github.com/goharbor/harbor/src/lib/log"
 )
 
+var (
+	base           = fmt.Sprintf("/api/%s", api.APIVersion)
+	sysInfoAPI     = base + "/systeminfo"
+	apiVersionAPI  = "/api/version"
+	labelsAPI      = base + "/labels"
+	projectsAPI    = base + "/projects"
+	reposAPIRe     = regexp.MustCompile(fmt.Sprintf(`^%s/projects/.*/repositories$`, regexp.QuoteMeta(base)))
+	artifactsAPIRe = regexp.MustCompile(fmt.Sprintf(`^%s/projects/.*/repositories/.*/artifacts$`, regexp.QuoteMeta(base)))
+	tagsAPIRe      = regexp.MustCompile(fmt.Sprintf(`^%s/projects/.*/repositories/.*/artifacts/.*/tags/.*$`, regexp.QuoteMeta(base)))
+)
+
 type oidcCli struct{}
 
 func (o *oidcCli) Generate(req *http.Request) security.Context {
-	log := log.G(req.Context())
-	path := req.URL.Path
-	// only handles request by docker CLI or helm CLI
-	if path != "/service/token" &&
-		!strings.HasPrefix(path, "/v2") &&
-		!strings.HasPrefix(path, "/chartrepo/") &&
-		!strings.HasPrefix(path, "/api/chartrepo/") &&
-		!strings.HasPrefix(path, fmt.Sprintf("/api/%s/chartrepo/", api.APIVersion)) {
-		return nil
-	}
 	if lib.GetAuthMode(req.Context()) != common.OIDCAuth {
 		return nil
 	}
+	logger := log.G(req.Context())
 	username, secret, ok := req.BasicAuth()
 	if !ok {
 		return nil
 	}
-	user, err := oidc.VerifySecret(req.Context(), username, secret)
-	if err != nil {
-		log.Errorf("failed to verify secret: %v", err)
+	if !o.valid(req) {
 		return nil
 	}
-	log.Debugf("an OIDC CLI security context generated for request %s %s", req.Method, req.URL.Path)
+	user, err := oidc.VerifySecret(req.Context(), username, secret)
+	if err != nil {
+		logger.Errorf("failed to verify secret: %v", err)
+		return nil
+	}
+	logger.Debugf("an OIDC CLI security context generated for request %s %s", req.Method, req.URL.Path)
 	return local.NewSecurityContext(user, config.GlobalProjectMgr)
+}
+
+func (o *oidcCli) valid(req *http.Request) bool {
+
+	path := strings.TrimSuffix(req.URL.Path, "/")
+
+	if path == "/service/token" ||
+		strings.HasPrefix(path, "/v2") ||
+		strings.HasPrefix(path, "/chartrepo") ||
+		strings.HasPrefix(path, "/api/chartrepo") {
+		// The request was sent by CLI to upload/download artifacts
+		return true
+	}
+
+	// additional requests for replication
+	if req.Method == http.MethodPost && path == projectsAPI { // creating project
+		return true
+	}
+
+	if req.Method == http.MethodGet && (path == projectsAPI || // list projects
+		path == sysInfoAPI || // query sys info
+		path == apiVersionAPI || // api version
+		path == labelsAPI || // list labels
+		reposAPIRe.MatchString(path) || // list repos
+		artifactsAPIRe.MatchString(path)) { // list artifacts
+		return true
+	}
+
+	if req.Method == http.MethodDelete && tagsAPIRe.MatchString(path) { // deleting tags
+		return true
+	}
+	return false
 }
