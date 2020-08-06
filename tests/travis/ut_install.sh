8583ae22e54f86f9f1549af6be443ced6d633b3c
wang yan <wangyan@vmware.com>
2020-08-06 15:31:17 +0800

    fix ci issue
    Signed-off-by: wang yan <wangyan@vmware.com>

--- tests/travis/ut_install.sh
+++ tests/travis/ut_install.sh
@@ -15,7 +15,6 @@ go get -d github.com/lib/pq
 go get golang.org/x/lint/golint
 go get github.com/GeertJohan/fgt
 go get github.com/dghubble/sling
-go get github.com/stretchr/testify
 go get golang.org/x/tools/cmd/cover
 go get github.com/mattn/goveralls
 go get -u github.com/client9/misspell/cmd/misspell
