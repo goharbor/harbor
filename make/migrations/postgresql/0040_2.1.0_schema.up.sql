98428b9cd998a843f147502f495723b9936a848b
guanxiatao <guanxiatao@corp.netease.com>
2020-08-11 16:01:02 +0800

    Fix for duplicate webhook policy name
    Signed-off-by: guanxiatao <guanxiatao@corp.netease.com>

--- make/migrations/postgresql/0040_2.1.0_schema.up.sql
+++ make/migrations/postgresql/0040_2.1.0_schema.up.sql
@@ -116,3 +116,8 @@ ALTER TABLE schedule DROP COLUMN IF EXISTS status;
 UPDATE registry SET type = 'quay' WHERE type = 'quay-io';
 
 ALTER TABLE artifact ADD COLUMN icon varchar(255);
+
+/*remove the constraint for name in table 'notification_policy'*/
+ALTER TABLE notification_policy DROP CONSTRAINT notification_policy_name_key;
+/*add union unique constraint for name and project_id in table 'notification_policy'*/
+ALTER TABLE notification_policy ADD UNIQUE(name,project_id);
