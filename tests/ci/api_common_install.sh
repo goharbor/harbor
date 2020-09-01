16bc87e6909d2e60989cfafea77855e62920a91d
danfengliu <danfengl@vmware.com>
2020-09-01 18:48:56 +0800

    Fix issue of missing swagger package for python API test
    1. Python API test can't import packages from swagger maker, the reason is that python install packages to site-packages instead of dist-packages,
       so the current solution is to copy pakages from site back to dist, I will try to find a way of using ENV variable to fix this;
    
    2. Remove a python library no longer be used.
    
    Signed-off-by: danfengliu <danfengl@vmware.com>

---
+++ tests/ci/api_common_install.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+set -x
+
+set +e
+sudo rm -fr /data/*
+sudo mkdir -p /data
+DIR="$(cd "$(dirname "$0")" && pwd)"
+
+set -e
+if [ -z "$1" ]; then echo no ip specified; exit 1;fi
+# prepare cert ...
+sudo ./tests/generateCerts.sh $1
+sudo mkdir -p /etc/docker/certs.d/$1 && sudo cp ./tests/harbor_ca.crt /etc/docker/certs.d/$1/ && rm -rf ~/.docker/ &&  mkdir -p ~/.docker/tls/$1:4443/ && sudo cp ./tests/harbor_ca.crt ~/.docker/tls/$1:4443/
+
+sudo ./tests/hostcfg.sh
+
+if [ "$2" = 'LDAP' ]; then
+    cd tests && sudo ./ldapprepare.sh && cd ..
+fi
+
+# prepare a chart file for API_DB test...
+sudo curl -o $DIR/../../tests/apitests/python/mariadb-4.3.1.tgz https://storage.googleapis.com/harbor-builds/bin/charts/mariadb-4.3.1.tgz
+
+sudo wget https://bootstrap.pypa.io/get-pip.py && sudo python ./get-pip.py && sudo pip install --ignore-installed urllib3 chardet requests && sudo pip install robotframework==3.2.1 robotframework-httplibrary requests --upgrade
+sudo make swagger_client
+#TODO: Swagger python package used to installed into dist-packages, but it's changed into site-packages all in a sudden, we havn't found the root cause.
+#      so current workround is to copy swagger packages from site-packages to dist-packages.
+sudo cp -r /usr/lib/python3.7/site-packages/* /usr/local/lib/python3.7/dist-packages
+
+if [ $GITHUB_TOKEN ];
+then
+    sed "s/# github_token: xxx/github_token: $GITHUB_TOKEN/" -i make/harbor.yml
+fi
+
+sudo make build_base_docker compile build prepare COMPILETAG=compile_golangimage GOBUILDTAGS="include_oss include_gcs" NOTARYFLAG=true CLAIRFLAG=true TRIVYFLAG=true CHARTFLAG=true GEN_TLS=true
+
+# set the debugging env
+echo "GC_TIME_WINDOW_HOURS=0" | sudo tee -a ./make/common/config/core/env
+sudo make start
+
+# waiting 5 minutes to start
+for((i=1;i<=30;i++)); do
+  echo $i waiting 10 seconds...
+  sleep 10
+  curl -k -L -f 127.0.0.1/api/v2.0/systeminfo && break
+  docker ps
+done
