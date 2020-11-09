9c8377909be8ae6d9f1ee1f781ce1c1e3f266514
He Weiwei <hweiwei@vmware.com>
2020-11-09 07:50:17 +0000

    fix: compute artifact size from db for schema1 manifest
    Closes #11892
    
    Signed-off-by: He Weiwei <hweiwei@vmware.com>

--- make/migrations/postgresql/0050_2.2.0_schema.up.sql
+++ make/migrations/postgresql/0050_2.2.0_schema.up.sql
@@ -1,1 +1,13 @@
 ALTER TABLE schedule ADD COLUMN IF NOT EXISTS cron_type varchar(64);
+
+DO $$
+DECLARE
+    art RECORD;
+    art_size integer;
+BEGIN
+    FOR art IN SELECT * FROM artifact WHERE size = 0
+    LOOP
+      SELECT sum(size) INTO art_size FROM blob WHERE digest IN (SELECT digest_blob FROM artifact_blob WHERE digest_af=art.digest);
+      UPDATE artifact SET size=art_size WHERE id = art.id;
+    END LOOP;
+END $$;
