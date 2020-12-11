9e3ab202e6e9431f004c8cd64aaa12b0ba950b34
inductor <kohei.ota@hpe.com>
2020-12-11 17:15:24 +0900

    Add Japanese
    Signed-off-by: inductor <kohei.ota@hpe.com>

--- src/portal/src/app/shared/shared.const.ts
+++ src/portal/src/app/shared/shared.const.ts
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-export const supportedLangs = ['en-us', 'zh-cn', 'zh-tw', 'es-es', 'fr-fr', 'pt-br', 'tr-tr', 'de-de'];
+export const supportedLangs = ['en-us', 'zh-cn', 'zh-tw', 'es-es', 'fr-fr', 'pt-br', 'tr-tr', 'de-de', 'ja-jp'];
 export const enLang = "en-us";
 export const languageNames = {
   "en-us": "English",
@@ -21,7 +21,8 @@ export const languageNames = {
   "fr-fr": "Français",
   "pt-br": "Português do Brasil",
   "tr-tr": "Türkçe",
-  "de-de": "Deutsch"
+  "de-de": "Deutsch",
+  "ja-jp": "日本語"
 };
 export const enum AlertType {
   DANGER, WARNING, INFO, SUCCESS
