7d05c8e51309629d28dd492b46deaecbc852ccbe
danfengliu <danfengl@vmware.com>
2021-01-28 18:03:40 +0800

    Reschedule docker login policy in base image build process
    Signed-off-by: danfengliu <danfengl@vmware.com>

--- make/pushimage.sh
+++ make/pushimage.sh
@@ -99,21 +99,6 @@ set -e
 #  - https://docs.docker.com/reference/commandline/cli/#logout
 # ---------------------------
 
-# Login to the registry
-h2 "Login to the Docker registry"
-
-DOCKER_LOGIN="docker login --username $USERNAME --password $PASSWORD $REGISTRY"
-info "docker login --username $USERNAME --password *******"
-DOCKER_LOGIN_OUTPUT=$($DOCKER_LOGIN)
-
-if [ $? -ne 0 ]; then
-  warn "$DOCKER_LOGIN_OUTPUT"
-  error "Login to Docker registry $REGISTRY failed"
-  exit 1
-else
-  success "Login to Docker registry $REGISTRY succeeded";
-fi
-
 # Push the docker image
 h2 "Pushing image to Docker registry"
 
@@ -126,17 +111,4 @@ if [ $? -ne 0 ];then
   error "Pushing image $IMAGE failed";
 else
   success "Pushing image $IMAGE succeeded";
-fi
-
-# Logout from the registry
-h2 "Logout from the docker registry"
-DOCKER_LOGOUT="docker logout $REGISTRY"
-DOCKER_LOGOUT_OUTPUT=$($DOCKER_LOGOUT)
-
-if [ $? -ne 0 ]; then
-  warn "$DOCKER_LOGOUT_OUTPUT"
-  error "Logout from Docker registry $REGISTRY failed"
-  exit 1
-else
-  success "Logout from Docker registry $REGISTRY succeeded"
 fi
