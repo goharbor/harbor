934bcd5012dfa15416398650e82dbc615924bc6f
danfengl <danfengl@vmware.com>
2021-03-04 06:12:44 +0000

    Remove python3.7 deployment in CI installtion phase.
    Signed-off-by: danfengl <danfengl@vmware.com>

--- tests/ci/api_common_install.sh
+++ tests/ci/api_common_install.sh
@@ -15,10 +15,6 @@ python --version
 pip -V
 cat /etc/issue
 cat /proc/version
-sudo apt-get update -y && sudo apt-get install -y zbar-tools libzbar-dev python-zbar python3.7
-sudo rm /usr/bin/python && sudo ln -s /usr/bin/python3.7 /usr/bin/python
-sudo apt-get install -y python3-pip
-pip -V
 sudo -H pip install --ignore-installed urllib3 chardet requests --upgrade
 python --version
 
