43df3bf8a41f4db090f0ab077060eff313628434
Wenkai Yin <yinw@vmware.com>
2021-03-12 19:29:25 +0800

    Add upgrade sql file introduced in 2.1.4
    1. Add upgrade sql file introduced in 2.1.4
    2. Minor improvement for task/execution to cover corner cases
    
    Signed-off-by: Wenkai Yin <yinw@vmware.com>

--- src/pkg/task/task_test.go
+++ src/pkg/task/task_test.go
@@ -94,10 +94,12 @@ func (t *taskManagerTestSuite) TestStop() {
 	t.jsClient.On("PostAction", mock.Anything, mock.Anything).Return(cjob.ErrJobNotFound)
 	t.dao.On("Update", mock.Anything, mock.Anything,
 		mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(nil)
+	t.execDAO.On("RefreshStatus", mock.Anything, mock.Anything).Return(true, "", nil)
 	err := t.mgr.Stop(nil, 1)
 	t.Require().Nil(err)
 	t.dao.AssertExpectations(t.T())
 	t.jsClient.AssertExpectations(t.T())
+	t.execDAO.AssertExpectations(t.T())
 
 	// reset mock
 	t.SetupTest()
