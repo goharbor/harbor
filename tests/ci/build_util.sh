e17205781c73ff68efc8e91f03a3ee533d3f3d35
danfengliu <danfengl@vmware.com>
2021-04-08 15:32:31 +0800

    Fix build base related issues
    1. Add build base image step in build package git action workflow;
    2. Add build base step to UT test in CI, base image used by UI test should be built before building harbor image in the same runtime;
    3. In build package workflow, trigger build base image step in condition of changing both in Dockerfile.base and VERSION;
    4. Add tag for setup nightly test.
    
    Signed-off-by: danfengliu <danfengl@vmware.com>

--- tests/ci/build_util.sh
+++ tests/ci/build_util.sh
@@ -7,3 +7,22 @@ function uploader {
     gsutil cp $1 gs://$2/$1
     gsutil -D setacl public-read gs://$2/$1 &> /dev/null
 }
+
+function publishImage {
+    echo "Publishing images to Docker Hub..."
+    echo "The images on the host:"
+    # for master, will use 'dev' as the tag name
+    # for release-*, will use 'release-*-dev' as the tag name, like release-v1.8.0-dev
+    if [[ $1 == "master" ]]; then
+      image_tag=dev
+    fi
+    if [[ $1 == "release-"* ]]; then
+      image_tag=$2-dev
+    fi
+    # rename the images with tag "dev" and push to Docker Hub
+    docker images
+    docker login -u $3 -p $4
+    docker images | grep goharbor | grep -v "\-base" | sed -n "s|\(goharbor/[-._a-z0-9]*\)\s*\(.*$2\).*|docker tag \1:\2 \1:$image_tag;docker push \1:$image_tag|p" | bash
+    echo "Images are published successfully"
+    docker images
+}
