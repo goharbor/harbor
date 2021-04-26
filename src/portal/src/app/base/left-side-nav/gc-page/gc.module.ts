705cb5b55d52f798134c6f96031ac8cbde3e1ee8
Will Sun <30999793+AllForNothing@users.noreply.github.com>
2021-04-26 10:25:36 +0800

    Refactor gc and gi history page (#14728)
    Signed-off-by: AllForNothing <sshijun@vmware.com>

--- src/portal/src/app/base/left-side-nav/gc-page/gc.module.ts
+++ src/portal/src/app/base/left-side-nav/gc-page/gc.module.ts
@@ -3,10 +3,7 @@ import { RouterModule, Routes } from "@angular/router";
 import { GcPageComponent } from "./gc-page.component";
 import { GcComponent } from "./gc/gc.component";
 import { GcHistoryComponent } from "./gc/gc-history/gc-history.component";
-import { GcRepoService } from "./gc/gc.service";
 import { SharedModule } from "../../../shared/shared.module";
-import { GcApiDefaultRepository, GcApiRepository } from "./gc/gc.api.repository";
-import { GcViewModelFactory } from "./gc/gc.viewmodel.factory";
 
 const routes: Routes = [
     {
@@ -24,10 +21,5 @@ const routes: Routes = [
         GcComponent,
         GcHistoryComponent
     ],
-    providers: [
-        GcRepoService,
-        {provide: GcApiRepository, useClass: GcApiDefaultRepository },
-        GcViewModelFactory
-    ]
 })
 export class GcModule {}
