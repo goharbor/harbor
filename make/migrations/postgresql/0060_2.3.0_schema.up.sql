ac5ea9e80dd3838678c757dbad8396223c6e6d85
Wang Yan <wangyan@vmware.com>
2021-05-20 18:34:00 +0800

    create index for audit
    Signed-off-by: Wang Yan <wangyan@vmware.com>

--- make/migrations/postgresql/0060_2.3.0_schema.up.sql
+++ make/migrations/postgresql/0060_2.3.0_schema.up.sql
@@ -5,3 +5,4 @@ CREATE INDEX IF NOT EXISTS idx_artifact_push_time ON artifact (push_time);
 CREATE INDEX IF NOT EXISTS idx_tag_push_time ON tag (push_time);
 CREATE INDEX IF NOT EXISTS idx_tag_artifact_id ON tag (artifact_id);
 CREATE INDEX IF NOT EXISTS idx_artifact_reference_child_id ON artifact_reference (child_id);
+CREATE INDEX IF NOT EXISTS idx_audit_log_op_time ON audit_log (op_time);
