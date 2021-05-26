c585f9203481f199689fcb575f0309416a7096ad
Wenkai Yin <yinw@vmware.com>
2021-05-26 18:57:23 +0800

    Fix bug of migration sql for replication policy
    Fix bug of migration sql for replication policy
    
    Signed-off-by: Wenkai Yin <yinw@vmware.com>

--- make/migrations/postgresql/0060_2.3.0_schema.up.sql
+++ make/migrations/postgresql/0060_2.3.0_schema.up.sql
@@ -1,5 +1,5 @@
 ALTER TABLE replication_policy ADD COLUMN IF NOT EXISTS dest_namespace_replace_count int;
-UPDATE replication_policy SET dest_namespace_replace_count=-1 WHERE dest_namespace IS NULL;
+UPDATE replication_policy SET dest_namespace_replace_count=-1 WHERE dest_namespace IS NOT NULL;
 
 CREATE INDEX IF NOT EXISTS idx_artifact_push_time ON artifact (push_time);
 CREATE INDEX IF NOT EXISTS idx_tag_push_time ON tag (push_time);
