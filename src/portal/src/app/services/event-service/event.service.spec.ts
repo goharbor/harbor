1d8ae8ff64900894acc37c6b6f7eedd7bca19536
孙世军 <30999793+AllForNothing@users.noreply.github.com>
2021-07-26 12:16:49 +0800

    Improve event service (#15353)
    Signed-off-by: AllForNothing <sshijun@vmware.com>
    Signed-off-by: Andreas Lindhé <andreas@lindhe.io>

--- src/portal/src/app/services/event-service/event.service.spec.ts
+++ src/portal/src/app/services/event-service/event.service.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { EventService } from "./event.service";
+import { Subscription } from "rxjs";
 
 
 describe('EventServiceService', () => {
@@ -12,5 +13,19 @@ describe('EventServiceService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+
+  it('able to subscribe', () => {
+    let result: string;
+    const sub1 = service.subscribe('testEvent', data => {
+      result = data;
+    });
+    expect(sub1 ).toBeTruthy();
+    expect((sub1 instanceof Subscription)).toEqual(true);
+    service.publish('testEvent', 'resultString');
+    sub1.unsubscribe();
+    expect(result).toEqual('resultString');
+
   });
 });
