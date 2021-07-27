538266c51a99c3f8626f249c1bdef209c93f42fe
孙世军 <30999793+AllForNothing@users.noreply.github.com>
2021-07-27 08:18:20 +0800

    Handle invalid date for HarborDatePipe (#15359)
    Signed-off-by: AllForNothing <sshijun@vmware.com>

--- src/portal/src/app/shared/pipes/harbor-datetime.pipe.ts
+++ src/portal/src/app/shared/pipes/harbor-datetime.pipe.ts
@@ -2,18 +2,23 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { DatePipe } from "@angular/common";
 import { DEFAULT_LANG_LOCALSTORAGE_KEY, DeFaultLang } from "../entities/shared.const";
 
+const baseTimeLine: Date = new Date('1970-1-1');
+
 @Pipe({
-  name: 'harborDatetime',
-  pure: false
+    name: 'harborDatetime',
+    pure: false
 })
 export class HarborDatetimePipe implements PipeTransform {
 
-  transform(value: any, format?: string): string {
-    let lang: string = DeFaultLang;
-    if (localStorage && localStorage.getItem(DEFAULT_LANG_LOCALSTORAGE_KEY)) {
-      lang = localStorage.getItem(DEFAULT_LANG_LOCALSTORAGE_KEY);
+    transform(value: any, format?: string): string {
+        let lang: string = DeFaultLang;
+        if (localStorage && localStorage.getItem(DEFAULT_LANG_LOCALSTORAGE_KEY)) {
+            lang = localStorage.getItem(DEFAULT_LANG_LOCALSTORAGE_KEY);
+        }
+        if (value && value <= baseTimeLine) {// invalid date
+            return '-';
+        }
+        // default format medium
+        return new DatePipe(lang).transform(value, format ? format : 'medium');
     }
-    // default format medium
-    return new DatePipe(lang).transform(value, format ? format : 'medium');
-  }
 }
