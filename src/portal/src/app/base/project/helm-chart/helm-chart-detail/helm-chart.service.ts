0e218fef6fe18c5998e9b941e7191538aa8c81c2
孙世军 <30999793+AllForNothing@users.noreply.github.com>
2021-08-24 17:05:13 +0800

    Fix chart download issue (#15472)
    Signed-off-by: AllForNothing <sshijun@vmware.com>
    Signed-off-by: Andreas Lindhé <andreas@lindhe.io>

--- src/portal/src/app/base/project/helm-chart/helm-chart-detail/helm-chart.service.ts
+++ src/portal/src/app/base/project/helm-chart/helm-chart-detail/helm-chart.service.ts
@@ -169,9 +169,9 @@ export class HelmChartDefaultService extends HelmChartService {
     let chartFileRegexPattern = new RegExp('^http.*/chartrepo/(.*)');
     if (chartFileRegexPattern.test(filename)) {
       let match = filename.match('^http.*/chartrepo/(.*)');
-      url = `${V1_BASE_HREF + "/chartrepo"}/${match[1]}`;
+      url = `${DOWNLOAD_CHART_ENDPOINT}/${match[1]}`;
     } else {
-      url = `${V1_BASE_HREF + "/chartrepo"}/${projectName}/${filename}`;
+      url = `${DOWNLOAD_CHART_ENDPOINT}/${projectName}/${filename}`;
     }
     return this.http.get(url, {
       responseType: 'blob',
@@ -209,3 +209,6 @@ export class HelmChartDefaultService extends HelmChartService {
     .pipe(catchError(this.handleErrorObservable));
   }
 }
+
+
+export const DOWNLOAD_CHART_ENDPOINT: string = "/chartrepo";
