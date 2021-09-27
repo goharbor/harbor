38e0910fc775a9c81e8846355bc722457d48357c
孙世军 <30999793+AllForNothing@users.noreply.github.com>
2021-09-27 18:46:27 +0800

    Auto-focus for filter component (#15672)
    Signed-off-by: AllForNothing <sshijun@vmware.com>

---
+++ src/portal/src/app/shared/components/filter/filter.component.spec.ts
@@ -0,0 +1,36 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { SharedTestingModule } from "../../shared.module";
+import { FilterComponent } from "./filter.component";
+
+describe('FilterComponent', () => {
+  let component: FilterComponent;
+  let fixture: ComponentFixture<FilterComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        SharedTestingModule,
+      ],
+      declarations: [FilterComponent]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FilterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+  it('should be focused', async () => {
+    await fixture.whenStable();
+    const searchIcon: HTMLElement = fixture.nativeElement.querySelector('.search-btn');
+    searchIcon.click();
+    fixture.detectChanges();
+    await fixture.whenStable();
+    const input: HTMLInputElement = fixture.nativeElement.querySelector('.filter-input:focus');
+    expect(input).toBeTruthy();
+  });
+});
