018871324c41e0a8ded054cabf45cad13c0f7a79
He Weiwei <hweiwei@vmware.com>
2021-10-25 18:48:08 +0800

    chore: bump up trivy adapter to v0.24.0 (#15872)
    Signed-off-by: He Weiwei <hweiwei@vmware.com>
    Signed-off-by: anyiwei <anyiwei@didichuxing.com>

--- src/pkg/p2p/preheat/provider/dragonfly.go
+++ src/pkg/p2p/preheat/provider/dragonfly.go
@@ -2,7 +2,6 @@ package provider
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"net/http"
 	"strings"
@@ -11,6 +10,7 @@ import (
 	"github.com/goharbor/harbor/src/pkg/p2p/preheat/models/provider"
 	"github.com/goharbor/harbor/src/pkg/p2p/preheat/provider/auth"
 	"github.com/goharbor/harbor/src/pkg/p2p/preheat/provider/client"
+	"github.com/goharbor/harbor/src/lib/errors"
 )
 
 const (
@@ -18,6 +18,7 @@ const (
 	preheatEndpoint     = "/preheats"
 	preheatTaskEndpoint = "/preheats/{task_id}"
 	dragonflyPending    = "WAITING"
+	dragonflyFailed     = "FAILED"
 )
 
 type dragonflyPreheatCreateResp struct {
@@ -28,6 +29,7 @@ type dragonflyPreheatInfo struct {
 	ID         string `json:"ID"`
 	StartTime  string `json:"startTime,omitempty"`
 	FinishTime string `json:"finishTime,omitempty"`
+	ErrorMsg   string `json:"errorMsg"`
 	Status     string
 }
 
@@ -104,6 +106,51 @@ func (dd *DragonflyDriver) Preheat(preheatingImage *PreheatImage) (*PreheatingSt
 
 // CheckProgress implements @Driver.CheckProgress.
 func (dd *DragonflyDriver) CheckProgress(taskID string) (*PreheatingStatus, error) {
+	status, err := dd.getProgressStatus(taskID)
+	if err != nil {
+		return nil, err
+	}
+
+	// If preheat job already exists
+	if strings.Index(status.ErrorMsg, "preheat task already exists, id:") >= 0 {
+		if taskID, err = getTaskExistedFromErrMsg(status.ErrorMsg); err != nil {
+			return nil, err
+		}
+		if status, err = dd.getProgressStatus(taskID); err != nil {
+			return nil, err
+		}
+	}
+
+	if status.Status == dragonflyPending {
+		status.Status = provider.PreheatingStatusPending
+	} else if status.Status == dragonflyFailed {
+		status.Status = provider.PreheatingStatusFail
+	}
+
+	res := &PreheatingStatus{
+		Status: status.Status,
+		TaskID: taskID,
+	}
+	if status.StartTime != "" {
+		res.StartTime = status.StartTime
+	}
+	if status.FinishTime != "" {
+		res.FinishTime = status.FinishTime
+	}
+
+	return res, nil
+}
+
+func getTaskExistedFromErrMsg(msg string) (string, error) {
+	begin := strings.Index(msg, "preheat task already exists, id:") + 32
+	end := strings.LastIndex(msg, "\\\"}")
+	if end - begin <= 0 {
+		return "", errors.Errorf("can't find existed task id by error msg:%s", msg)
+	}
+	return msg[begin:end], nil
+}
+
+func (dd *DragonflyDriver) getProgressStatus(taskID string) (*dragonflyPreheatInfo, error) {
 	if dd.instance == nil {
 		return nil, errors.New("missing instance metadata")
 	}
@@ -123,23 +170,7 @@ func (dd *DragonflyDriver) CheckProgress(taskID string) (*PreheatingStatus, erro
 	if err := json.Unmarshal(bytes, status); err != nil {
 		return nil, err
 	}
-
-	if status.Status == dragonflyPending {
-		status.Status = provider.PreheatingStatusPending
-	}
-
-	res := &PreheatingStatus{
-		Status: status.Status,
-		TaskID: taskID,
-	}
-	if status.StartTime != "" {
-		res.StartTime = status.StartTime
-	}
-	if status.FinishTime != "" {
-		res.FinishTime = status.FinishTime
-	}
-
-	return res, nil
+	return status, nil
 }
 
 func (dd *DragonflyDriver) getCred() *auth.Credential {
