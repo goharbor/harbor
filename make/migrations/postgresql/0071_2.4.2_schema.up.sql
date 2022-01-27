575fab228658667f67b127819c2a17216ecdb7d7
Shengwen Yu <yshengwen@vmware.com>
2022-01-27 13:03:25 +0800

    fix: vulnerability_record & report_vulnerability_record table "integer out of range" error
    Signed-off-by: Shengwen Yu <yshengwen@vmware.com>

--- make/migrations/postgresql/0071_2.4.2_schema.up.sql
+++ make/migrations/postgresql/0071_2.4.2_schema.up.sql
@@ -1,2 +1,8 @@
 /* Remove old version scan reports of trivy */
 DELETE FROM scan_report WHERE mime_type='application/vnd.scanner.adapter.vuln.report.harbor+json; version=1.0' AND registration_uuid IN (SELECT uuid FROM scanner_registration WHERE name='Trivy' AND immutable='true');
+
+/* Change vulnerability_record.id and report_vulnerability_record.id to BIGINT */
+ALTER TABLE vulnerability_record ALTER COLUMN id TYPE BIGINT;
+ALTER SEQUENCE vulnerability_record_id_seq AS BIGINT;
+ALTER TABLE report_vulnerability_record ALTER COLUMN id TYPE BIGINT;
+ALTER SEQUENCE report_vulnerability_record_id_seq AS BIGINT;
