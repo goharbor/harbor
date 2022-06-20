5de1a62002ec5953ff58258144a1bf0ed1d7190d
Chenyu Zhang <chenyuzh@vmware.com>
2022-06-20 16:01:04 +0800

    migrate: add db index on artifact repository name (#17035)
    Signed-off-by: chlins <chenyuzh@vmware.com>

--- make/migrations/postgresql/0090_2.6.0_schema.up.sql
+++ make/migrations/postgresql/0090_2.6.0_schema.up.sql
@@ -16,3 +16,5 @@ Github proposal link : https://github.com/goharbor/community/pull/181
         extra_attrs text NOT NULL DEFAULT '' ,
         UNIQUE ("repository", "digest",  "vendor")
 );
+
+CREATE INDEX IF NOT EXISTS idx_artifact_repository_name ON artifact (repository_name);
