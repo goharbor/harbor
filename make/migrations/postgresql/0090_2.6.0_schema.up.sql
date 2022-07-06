6e9251f6d64bda735cd96ecb3549bd9bf276c7b6
chlins <chenyuzh@vmware.com>
2022-07-06 12:00:46 +0800

    fix: repair execution status when it inconsistent
    Add migrations sql to repair the execution status when it does not
    consistent with task status.
    
    Closes: #17114
    
    Signed-off-by: chlins <chenyuzh@vmware.com>

--- make/migrations/postgresql/0090_2.6.0_schema.up.sql
+++ make/migrations/postgresql/0090_2.6.0_schema.up.sql
@@ -22,3 +22,32 @@ CREATE INDEX IF NOT EXISTS idx_artifact_repository_name ON artifact (repository_
 CREATE INDEX IF NOT EXISTS idx_execution_vendor_type_vendor_id ON execution (vendor_type, vendor_id);
 CREATE INDEX IF NOT EXISTS idx_execution_start_time ON execution(start_time);
 CREATE INDEX IF NOT EXISTS idx_audit_log_project_id_optime ON audit_log (project_id, op_time);
+
+/* repair execution status */
+DO $$
+DECLARE
+    exec RECORD;
+    status_group RECORD;
+    status_count int;
+    final_status varchar(32);
+BEGIN
+    /* iterate all executions */
+    FOR exec IN SELECT * FROM execution WHERE status='Running'
+    LOOP
+        /* identify incorrect execution status, group tasks belong it by status */
+        status_count = 0;
+        final_status = '';
+        FOR status_group IN SELECT status FROM task WHERE execution_id=exec.id GROUP BY status
+        /* loop here to ensure all the tasks belong to the execution are success */
+        LOOP
+            status_count = status_count + 1;
+            final_status = status_group.status;
+        END LOOP;
+        /* update status and end_time when the tasks are all
+        success but itself status is not success */
+        IF status_count=1 AND final_status='Success' THEN
+            UPDATE execution SET status='Success', revision=revision+1 WHERE id=exec.id;
+            UPDATE execution SET end_time=(SELECT MAX(end_time) FROM task WHERE execution_id=exec.id) WHERE id=exec.id;
+        END IF;
+    END LOOP;
+END $$;
