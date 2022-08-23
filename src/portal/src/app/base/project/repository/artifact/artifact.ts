a55ce0c35424a4665fb88970e73408c4723b3adb
AllForNothing <sshijun@vmware.com>
2022-08-23 12:37:54 +0800

    Add WASM filter for artifacts list
    Signed-off-by: AllForNothing <sshijun@vmware.com>

--- src/portal/src/app/base/project/repository/artifact/artifact.ts
+++ src/portal/src/app/base/project/repository/artifact/artifact.ts
@@ -35,6 +35,10 @@ export const mutipleFilter = [
                 filterText: 'CNAB',
                 showItem: 'ARTIFACT.CNAB',
             },
+            {
+                filterText: 'WASM',
+                showItem: 'ARTIFACT.WASM',
+            },
         ],
     },
     {
