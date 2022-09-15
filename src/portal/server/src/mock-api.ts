a09a1a08e418a029dd51c7d04ceb98be6b17ff31
AllForNothing <sshijun@vmware.com>
2022-09-15 11:45:21 +0800

    Simplify proxy config
    Signed-off-by: AllForNothing <sshijun@vmware.com>

--- src/portal/server/src/mock-api.ts
+++ src/portal/server/src/mock-api.ts
@@ -1,19 +1,19 @@
-import express from "express";
+import express from 'express';
 import { Express } from 'express';
 import * as Controllers from '../controllers';
-import { CURRENT_BASE_HREF } from "../../src/app/shared/units/utils";
-
 
 const mockApi: Express = express();
 
-mockApi.get('/',  (req, res) => {
-  res.send('Hello World!');
+const CURRENT_BASE_HREF: string = '/api/v2.0';
+
+mockApi.get('/', (req, res) => {
+    res.send('Hello World!');
 });
 
 mockApi.get(CURRENT_BASE_HREF + '/scanners', Controllers.getScanner);
 
-mockApi.listen(3000,  ()  => {
-  console.log('Api server listening on port 3000!');
+mockApi.listen(3000, () => {
+    console.log('Api server listening on port 3000!');
 });
 
 export default mockApi;
