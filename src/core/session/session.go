e51b508965cee4c47126e9bb37c57da91ec25a91
yminer <yminer@vmware.com>
2022-09-20 07:26:53 +0000

    beego v2 session refactor
    Signed-off-by: yminer <yminer@vmware.com>
    
    ctx updaate
    
    context update
    
    Signed-off-by: yminer <yminer@vmware.com>

--- src/core/session/session.go
+++ src/core/session/session.go
@@ -46,7 +46,7 @@ type Store struct {
 }
 
 // Set value in redis session
-func (rs *Store) Set(key, value interface{}) error {
+func (rs *Store) Set(ctx context.Context, key, value interface{}) error {
 	rs.lock.Lock()
 	defer rs.lock.Unlock()
 	rs.values[key] = value
@@ -54,7 +54,7 @@ func (rs *Store) Set(key, value interface{}) error {
 }
 
 // Get value in redis session
-func (rs *Store) Get(key interface{}) interface{} {
+func (rs *Store) Get(ctx context.Context, key interface{}) interface{} {
 	rs.lock.RLock()
 	defer rs.lock.RUnlock()
 	if v, ok := rs.values[key]; ok {
@@ -64,7 +64,7 @@ func (rs *Store) Get(key interface{}) interface{} {
 }
 
 // Delete value in redis session
-func (rs *Store) Delete(key interface{}) error {
+func (rs *Store) Delete(ctx context.Context, key interface{}) error {
 	rs.lock.Lock()
 	defer rs.lock.Unlock()
 	delete(rs.values, key)
@@ -72,7 +72,7 @@ func (rs *Store) Delete(key interface{}) error {
 }
 
 // Flush clear all values in redis session
-func (rs *Store) Flush() error {
+func (rs *Store) Flush(ctx context.Context) error {
 	rs.lock.Lock()
 	defer rs.lock.Unlock()
 	rs.values = make(map[interface{}]interface{})
@@ -80,19 +80,19 @@ func (rs *Store) Flush() error {
 }
 
 // SessionID get redis session id
-func (rs *Store) SessionID() string {
+func (rs *Store) SessionID(ctx context.Context) string {
 	return rs.sid
 }
 
 // SessionRelease save session values to redis
-func (rs *Store) SessionRelease(w http.ResponseWriter) {
+func (rs *Store) SessionRelease(ctx context.Context, w http.ResponseWriter) {
 	b, err := session.EncodeGob(rs.values)
 	if err != nil {
 		return
 	}
 
 	if rdb, ok := rs.c.(*redis.Cache); ok {
-		cmd := rdb.Client.Set(context.TODO(), rs.sid, string(b), time.Duration(rs.maxlifetime))
+		cmd := rdb.Client.Set(ctx, rs.sid, string(b), time.Duration(rs.maxlifetime))
 		if cmd.Err() != nil {
 			log.Debugf("release session error: %v", err)
 		}
@@ -106,20 +106,20 @@ type Provider struct {
 }
 
 // SessionInit init redis session
-func (rp *Provider) SessionInit(maxlifetime int64, url string) (err error) {
+func (rp *Provider) SessionInit(ctx context.Context, maxlifetime int64, url string) (err error) {
 	rp.maxlifetime = maxlifetime * int64(time.Second)
 	rp.c, err = redis.New(cache.Options{Address: url, Codec: codec})
 	if err != nil {
 		return err
 	}
 
-	return rp.c.Ping(context.TODO())
+	return rp.c.Ping(ctx)
 }
 
 // SessionRead read redis session by sid
-func (rp *Provider) SessionRead(sid string) (session.Store, error) {
+func (rp *Provider) SessionRead(ctx context.Context, sid string) (session.Store, error) {
 	kv := make(map[interface{}]interface{})
-	err := rp.c.Fetch(context.TODO(), sid, &kv)
+	err := rp.c.Fetch(ctx, sid, &kv)
 	if err != nil && !strings.Contains(err.Error(), goredis.Nil.Error()) {
 		return nil, err
 	}
@@ -129,14 +129,13 @@ func (rp *Provider) SessionRead(sid string) (session.Store, error) {
 }
 
 // SessionExist check redis session exist by sid
-func (rp *Provider) SessionExist(sid string) bool {
-	return rp.c.Contains(context.TODO(), sid)
+func (rp *Provider) SessionExist(ctx context.Context, sid string) (bool, error) {
+	return rp.c.Contains(ctx, sid), nil
 }
 
 // SessionRegenerate generate new sid for redis session
-func (rp *Provider) SessionRegenerate(oldsid, sid string) (session.Store, error) {
-	ctx := context.TODO()
-	if !rp.SessionExist(oldsid) {
+func (rp *Provider) SessionRegenerate(ctx context.Context, oldsid, sid string) (session.Store, error) {
+	if isExist, _ := rp.SessionExist(ctx, oldsid); !isExist {
 		err := rp.c.Save(ctx, sid, "", time.Duration(rp.maxlifetime))
 		if err != nil {
 			log.Debugf("failed to save sid=%s, where oldsid=%s, error: %s", sid, oldsid, err)
@@ -164,20 +163,20 @@ func (rp *Provider) SessionRegenerate(oldsid, sid string) (session.Store, error)
 		}
 	}
 
-	return rp.SessionRead(sid)
+	return rp.SessionRead(ctx, sid)
 }
 
 // SessionDestroy delete redis session by id
-func (rp *Provider) SessionDestroy(sid string) error {
-	return rp.c.Delete(context.TODO(), sid)
+func (rp *Provider) SessionDestroy(ctx context.Context, sid string) error {
+	return rp.c.Delete(ctx, sid)
 }
 
 // SessionGC Implement method, no used.
-func (rp *Provider) SessionGC() {
+func (rp *Provider) SessionGC(ctx context.Context) {
 }
 
 // SessionAll return all activeSession
-func (rp *Provider) SessionAll() int {
+func (rp *Provider) SessionAll(ctx context.Context) int {
 	return 0
 }
 
