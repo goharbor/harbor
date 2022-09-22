e809aa630e720d8fc6c64929fc0ab66a5ae4d032
yminer <yminer@vmware.com>
2022-09-22 02:26:05 +0000

    Bump up golang version to v1.19
    Signed-off-by: yminer <yminer@vmware.com>
    
    update

--- make/photon/trivy-adapter/builder.sh
+++ make/photon/trivy-adapter/builder.sh
@@ -19,7 +19,7 @@ TEMP=$(mktemp -d ${TMPDIR-/tmp}/trivy-adapter.XXXXXX)
 git clone https://github.com/aquasecurity/harbor-scanner-trivy.git $TEMP
 cd $TEMP; git checkout $VERSION; cd -
 
-echo "Building Trivy adapter binary based on golang:1.18.5..."
+echo "Building Trivy adapter binary based on golang:1.19.1..."
 cp Dockerfile.binary $TEMP
 docker build -f $TEMP/Dockerfile.binary -t trivy-adapter-golang $TEMP
 
