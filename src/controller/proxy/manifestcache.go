e16a087122a8a4db3b5f18f0b5461e25ce3f4a54
stonezdj <stonezdj@gmail.com>
2022-11-11 22:46:21 +0800

    Fix issue related to redhat registry proxy cache
      fixes #16495
    
    Signed-off-by: stonezdj <stonezdj@gmail.com>

--- src/controller/proxy/manifestcache.go
+++ src/controller/proxy/manifestcache.go
@@ -69,18 +69,20 @@ func (m *ManifestListCache) CacheContent(ctx context.Context, remoteRepo string,
 		log.Errorf("failed to get payload, error %v", err)
 		return
 	}
-	key := manifestListKey(art.Repository, art.Digest)
+	if len(getReference(art)) == 0 {
+		log.Errorf("failed to get reference, reference is empty, skip to cache manifest list")
+		return
+	}
+	// some registry will not return the digest in the HEAD request, if no digest returned, cache manifest list content with tag
+	key := manifestListKey(art.Repository, art)
 	log.Debugf("cache manifest list with key=cache:%v", key)
-	err = m.cache.Save(ctx, manifestListContentTypeKey(art.Repository, art.Digest), contentType, manifestListCacheInterval)
-	if err != nil {
+	if err := m.cache.Save(ctx, manifestListContentTypeKey(art.Repository, art), contentType, manifestListCacheInterval); err != nil {
 		log.Errorf("failed to cache content type, error %v", err)
 	}
-	err = m.cache.Save(ctx, key, payload, manifestListCacheInterval)
-	if err != nil {
+	if err := m.cache.Save(ctx, key, payload, manifestListCacheInterval); err != nil {
 		log.Errorf("failed to cache payload, error %v", err)
 	}
-	err = m.push(ctx, art.Repository, getReference(art), man)
-	if err != nil {
+	if err := m.push(ctx, art.Repository, getReference(art), man); err != nil {
 		log.Errorf("error when push manifest list to local :%v", err)
 	}
 }
