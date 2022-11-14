f07e1f8c5dedd606c595d17d80cf63ffc322b72e
Yang Jiao <jiaoya@vmware.com>
2022-11-14 07:34:52 +0000

    Add metrics test case
    Add more metrics validation
    
    Signed-off-by: Yang Jiao <jiaoya@vmware.com>

--- tests/apitests/python/test_verify_metrics_enabled.py
+++ tests/apitests/python/test_verify_metrics_enabled.py
@@ -6,7 +6,16 @@ import unittest
 import requests
 import testutils
 
+from testutils import suppress_urllib3_warning
+from library.statistic import Statistic
+
 class TestMetricsExist(unittest.TestCase):
+
+    @suppress_urllib3_warning
+    def setUp(self):
+        statistic = Statistic()
+        self.statistic_data = statistic.get_statistic()
+
     golang_basic_metrics = ["go_gc_duration_seconds", "go_goroutines", "go_info", "go_memstats_alloc_bytes"]
 
     metrics = {
@@ -14,7 +23,21 @@ class TestMetricsExist(unittest.TestCase):
             "harbor_core_http_request_total",
             "harbor_core_http_request_duration_seconds",
             "harbor_core_http_inflight_requests"],
-        'registry': golang_basic_metrics + ["registry_http_in_flight_requests"],
+        'registry': golang_basic_metrics + [
+            "registry_http_in_flight_requests",
+            "registry_http_request_duration_seconds_bucket",
+            "registry_http_request_duration_seconds_sum",
+            "registry_http_request_duration_seconds_count",
+            "registry_http_request_size_bytes_bucket",
+            "registry_http_request_size_bytes_sum",
+            "registry_http_request_size_bytes_count",
+            "registry_http_requests_total",
+            "registry_http_response_size_bytes_bucket",
+            "registry_http_response_size_bytes_sum",
+            "registry_http_response_size_bytes_count",
+            "registry_storage_action_seconds_bucket",
+            "registry_storage_action_seconds_sum",
+            "registry_storage_action_seconds_count"],
         'exporter': golang_basic_metrics + [
             "artifact_pulled",
             "harbor_project_artifact_total",
@@ -26,7 +49,12 @@ class TestMetricsExist(unittest.TestCase):
             "harbor_task_concurrency",
             "harbor_task_queue_latency",
             "harbor_task_queue_size",
-            "harbor_task_scheduled_total"],
+            "harbor_task_scheduled_total",
+            "harbor_project_quota_usage_byte",
+            "harbor_artifact_pulled",
+            "harbor_health",
+            "harbor_system_info",
+            "harbor_up"],
         'jobservice': golang_basic_metrics + [
             "harbor_jobservice_info",
             "harbor_jobservice_task_process_time_seconds",
@@ -43,12 +71,18 @@ class TestMetricsExist(unittest.TestCase):
 
     def testMetricsExist(self):
         for k, metric_text in self.get_metrics():
-
-
-
             for metric_name in self.metrics[k]:
                 print("Metric {} should exist in {} ".format(metric_name, k))
-                self.assertTrue(metric_name in metric_text)
+                self.verifyMetrics(metric_name, metric_text)
+
+    def verifyMetrics(self, metric_name, metric_text):
+        if metric_name == "harbor_project_total":
+            self.assertTrue('harbor_project_total{public="false"} ' + str(self.statistic_data.private_project_count) in metric_text)
+            self.assertTrue('harbor_project_total{public="true"} ' + str(self.statistic_data.public_project_count) in metric_text)
+        else:
+            self.assertTrue(metric_name in metric_text)
+
+
 
 if __name__ == '__main__':
     unittest.main()
