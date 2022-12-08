8dac344a48a8b7122779167fc74043d1682637bb
Wang Yan <wangyan@vmware.com>
2022-12-08 22:13:47 +0800

    remove the redundant data from table artifact_blob (#17959)
    use sql in the migration process to delete all the useless data of table artifact_blob
    
    Signed-off-by: Wang Yan <wangyan@vmware.com>
    
    Signed-off-by: Wang Yan <wangyan@vmware.com>

--- make/migrations/postgresql/0100_2.7.0_schema.up.sql
+++ make/migrations/postgresql/0100_2.7.0_schema.up.sql
@@ -7,3 +7,6 @@ CREATE TABLE IF NOT EXISTS job_queue_status (
         update_time timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
         UNIQUE ("job_type")
 );
+
+/* remove the redundant data from table artifact_blob */
+delete from artifact_blob afb where not exists (select digest from blob b where b.digest = afb.digest_af);
