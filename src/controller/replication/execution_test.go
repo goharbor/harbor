3aad8e25a3f2e8688918a5a965b6c5d8167be83d
chlins <chenyuzh@vmware.com>
2023-02-09 16:13:17 +0800

    fix: update the replication API handler
    1. Check execution before stop replication execution.
    2. Check execution before list replication tasks.
    
    Signed-off-by: chlins <chenyuzh@vmware.com>

--- src/controller/replication/execution_test.go
+++ src/controller/replication/execution_test.go
@@ -105,6 +105,21 @@ func (r *replicationTestSuite) TestStart() {
 }
 
 func (r *replicationTestSuite) TestStop() {
+	r.execMgr.On("List", mock.Anything, mock.Anything).Return([]*task.Execution{
+		{
+			ID:         1,
+			VendorType: job.Replication,
+			VendorID:   1,
+			Status:     job.RunningStatus.String(),
+			Metrics: &dao.Metrics{
+				TaskCount:        1,
+				RunningTaskCount: 1,
+			},
+			Trigger:   task.ExecutionTriggerManual,
+			StartTime: time.Time{},
+			EndTime:   time.Time{},
+		},
+	}, nil)
 	r.execMgr.On("Stop", mock.Anything, mock.Anything).Return(nil)
 	err := r.ctl.Stop(nil, 1)
 	r.Require().Nil(err)
