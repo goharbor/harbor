9973d99f3e255e002873df590bff52e18c23de69
Shijun Sun <30999793+AllForNothing@users.noreply.github.com>
2023-02-24 14:47:42 +0800

    Remove chart api (#18265)
    * remove chart api from swagger
    
    Delete the api path for chart in the swagger
    
    Signed-off-by: Wang Yan <wangyan@vmware.com>
    
    * Remove chart APIs
       1. Remove some chart-related steps for Dockerfile for portal  container
       2. Remove chart APIs for API center
    
    Signed-off-by: AllForNothing <sshijun@vmware.com>
    
    ---------
    
    Signed-off-by: Wang Yan <wangyan@vmware.com>
    Signed-off-by: AllForNothing <sshijun@vmware.com>
    Co-authored-by: Wang Yan <wangyan@vmware.com>

--- src/portal/app-swagger-ui/src/index.js
+++ src/portal/app-swagger-ui/src/index.js
@@ -6,23 +6,16 @@ const helpInfo =
     ' please logout Harbor first or manually delete the cookies under the current domain.';
 const SAFE_METHODS = ['GET', 'HEAD', 'OPTIONS', 'TRACE'];
 
-// get swagger.json and swagger2.json from portal container then render swagger ui
+// get swagger.json from portal container then render swagger ui
 // before rendering, the ui shows a loading style
-Promise.all([
-    fetch('/swagger.json').then(value => value.json()),
-    fetch('/swagger2.json').then(value => value.json())
-])
-    .then(value => {
-        // merger swagger.json and swagger2.json
-        const json = {};
-        mergeDeep(json, value[0], value[1]);
-        json['host'] = window.location.host;
-        const protocal = window.location.protocol;
-        json['schemes'] = [protocal.replace(':', '')];
-        json.info.description = json.info.description + helpInfo;
+fetch('/swagger.json').then(value => value.json()).then(res => {
+    res['host'] = window.location.host;
+    const protocal = window.location.protocol;
+    res['schemes'] = [protocal.replace(':', '')];
+    res.info.description = res.info.description + helpInfo;
         // start to render
         SwaggerUI({
-            spec: json,
+            spec: res,
             dom_id: '#swagger-ui-container',
             deepLinking: true,
             presets: [SwaggerUI.presets.apis],
@@ -60,34 +53,3 @@ Promise.all([
     .catch((err) => {
         console.error(err);
     });
-
-
-
-
-
-
-
-function mergeDeep(target, ...sources) {
-    if (!sources.length) {
-        return target;
-    }
-    const source = sources.shift();
-
-    if (isObject(target) && isObject(source)) {
-        for (const key in source) {
-            if (isObject(source[key])) {
-                if (!target[key]) {
-                    Object.assign(target, { [key]: {} });
-                }
-                mergeDeep(target[key], source[key]);
-            } else {
-                Object.assign(target, { [key]: source[key] });
-            }
-        }
-    }
-    return mergeDeep(target, ...sources);
-}
-
-function isObject(item) {
-    return item && typeof item === 'object' && !Array.isArray(item);
-}
