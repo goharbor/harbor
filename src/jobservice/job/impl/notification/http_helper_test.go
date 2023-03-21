67d3f9add8819a28edd90ffae67a0fdd6e62f365
Chlins Zhang <chenyuzh@vmware.com>
2023-03-21 11:54:10 +0800

    feat: support configurate the http client timeout for webhook job (#18382)
    Signed-off-by: chlins <chenyuzh@vmware.com>

--- src/jobservice/job/impl/notification/http_helper_test.go
+++ src/jobservice/job/impl/notification/http_helper_test.go
@@ -2,6 +2,7 @@ package notification
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -9,9 +10,11 @@ import (
 func TestHttpHelper(t *testing.T) {
 	c1 := httpHelper.clients[insecure]
 	assert.NotNil(t, c1)
+	assert.Equal(t, 3*time.Second, c1.Timeout)
 
 	c2 := httpHelper.clients[secure]
 	assert.NotNil(t, c2)
+	assert.Equal(t, 3*time.Second, c1.Timeout)
 
 	_, ok := httpHelper.clients["notExists"]
 	assert.False(t, ok)
