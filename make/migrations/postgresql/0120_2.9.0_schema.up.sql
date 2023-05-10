36c2b93fe651a90bab8ec93465cb134874ca843f
Chlins Zhang <chenyuzh@vmware.com>
2023-05-10 13:23:54 +0800

    fix: sweep executions of image scan job (#18649)
    1. Change the SCAN_ALL job execution retain counts from 5 to 1(per
       current design, only one report be stored for every artifact, so
       retain latest 1 is enough).
    2. Enable the sweep for IMAGE_SCAN job(retain latest 1).
    
    Fixes: #18633
    
    Signed-off-by: chlins <chenyuzh@vmware.com>

--- make/migrations/postgresql/0120_2.9.0_schema.up.sql
+++ make/migrations/postgresql/0120_2.9.0_schema.up.sql
@@ -1,1 +1,7 @@
 CREATE INDEX IF NOT EXISTS idx_task_extra_attrs_report_uuids ON task USING gin ((extra_attrs::jsonb->'report_uuids'));
+
+/* Set the vendor_id of IMAGE_SCAN to the artifact id instead of scanner id, which facilitates execution sweep */
+UPDATE execution SET vendor_id = (extra_attrs -> 'artifact' ->> 'id')::integer
+WHERE jsonb_path_exists(extra_attrs::jsonb, '$.artifact.id')
+AND vendor_id IN (SELECT id FROM scanner_registration)
+AND vendor_type = 'IMAGE_SCAN';
