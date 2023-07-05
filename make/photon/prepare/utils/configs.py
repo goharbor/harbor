d61770a96d2a95695b2e8571a1d5a6b754de1130
yminer <yminer@vmware.com>
2023-07-05 08:45:58 +0000

    [cherry-pick] [fix issue-18892]disable external redis using acl username auth
    Signed-off-by: yminer <yminer@vmware.com>

--- make/photon/prepare/utils/configs.py
+++ make/photon/prepare/utils/configs.py
@@ -386,9 +386,8 @@ def get_redis_url(db, redis=None):
     kwargs['db_part'] = db and ("/%s" % db) or ""
     kwargs['sentinel_part'] = kwargs.get('sentinel_master_set', None) and ("/" + kwargs['sentinel_master_set']) or ''
     kwargs['password_part'] = quote(str(kwargs.get('password', None)), safe='') and (':%s@' % quote(str(kwargs['password']), safe='')) or ''
-    kwargs['username_part'] = kwargs.get('username', None) or ''
 
-    return "{scheme}://{username_part}{password_part}{host}{sentinel_part}{db_part}".format(**kwargs) + get_redis_url_param(kwargs)
+    return "{scheme}://{password_part}{host}{sentinel_part}{db_part}".format(**kwargs) + get_redis_url_param(kwargs)
 
 
 def get_redis_url_param(redis=None):
