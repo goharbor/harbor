1af6ae96d6bcbb7c98d48ee2ce4caedb82172de2
yminer <yminer@vmware.com>
2023-08-09 02:17:53 +0000

    try prune builder cache
    Signed-off-by: yminer <yminer@vmware.com>

--- tests/ci/distro_installer.sh
+++ tests/ci/distro_installer.sh
@@ -3,7 +3,5 @@ set -x
 
 set -e
 
-sudo docker system prune -af
-
 sudo make package_online GOBUILDTAGS="include_oss include_gcs" VERSIONTAG=dev-gitaction PKGVERSIONTAG=dev-gitaction UIVERSIONTAG=dev-gitaction GOBUILDIMAGE=goharbor/golang:1.19.11 COMPILETAG=compile_golangimage NOTARYFLAG=true CHARTFLAG=true TRIVYFLAG=true HTTPPROXY= PULL_BASE_FROM_DOCKERHUB=false
 sudo make package_offline GOBUILDTAGS="include_oss include_gcs" VERSIONTAG=dev-gitaction PKGVERSIONTAG=dev-gitaction UIVERSIONTAG=dev-gitaction GOBUILDIMAGE=goharbor/golang:1.19.11 COMPILETAG=compile_golangimage NOTARYFLAG=true CHARTFLAG=true TRIVYFLAG=true HTTPPROXY= PULL_BASE_FROM_DOCKERHUB=false
