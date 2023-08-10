4e53e2c2dae9b2b2e8307d066d143c41078de63b
stonezdj <daojunz@vmware.com>
2023-08-10 18:52:15 +0800

    Change config cache from redis to memory
      Because config is widely used, change to memory cache instead of redis to reduce traffic
    
    Signed-off-by: stonezdj <daojunz@vmware.com>

--- src/core/main.go
+++ src/core/main.go
@@ -62,7 +62,6 @@ import (
 	_ "github.com/goharbor/harbor/src/pkg/accessory/model/nydus"
 	_ "github.com/goharbor/harbor/src/pkg/accessory/model/subject"
 	"github.com/goharbor/harbor/src/pkg/audit"
-	dbCfg "github.com/goharbor/harbor/src/pkg/config/db"
 	_ "github.com/goharbor/harbor/src/pkg/config/inmemory"
 	"github.com/goharbor/harbor/src/pkg/notification"
 	_ "github.com/goharbor/harbor/src/pkg/notifier/topic"
@@ -144,9 +143,6 @@ func main() {
 		if err := cache.Initialize(u.Scheme, redisURL); err != nil {
 			log.Fatalf("failed to initialize cache: %v", err)
 		}
-		// when config/db init function is called, the cache is not ready,
-		// enable config cache explicitly when the cache is ready
-		dbCfg.EnableConfigCache()
 	}
 	web.AddTemplateExt("htm")
 
