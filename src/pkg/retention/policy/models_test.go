fbdc03bcabb8eb758dfe844e74acd73c32b8fbfa
Shengwen YU <yshengwen@vmware.com>
2023-08-11 08:31:13 +0800

    [Cherry-pick] fix: cron string validation (the 1st field of a cron string must be 0… (#19145)
    fix: cron string validation (the 1st field of a cron string must be 0 when there are 6 fields)
    
    Signed-off-by: Shengwen Yu <yshengwen@vmware.com>

--- src/pkg/retention/policy/models_test.go
+++ src/pkg/retention/policy/models_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/beego/beego/v2/core/validation"
 	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
 
 	"github.com/goharbor/harbor/src/pkg/retention/policy/rule"
 )
@@ -34,6 +35,47 @@ func TestAlgorithm(t *testing.T) {
 // 		fmt.Print(e.Field, e.Message)
 // 	}
 // }
+
+type PolicyTestSuite struct {
+	suite.Suite
+
+	policy *Metadata
+}
+
+// TestRetentionPolicy is the entry method of running PolicyTestSuite.
+func TestRetentionPolicy(t *testing.T) {
+	suite.Run(t, &PolicyTestSuite{})
+}
+
+// SetupSuite prepares the env for PolicyTestSuite.
+func (p *PolicyTestSuite) SetupSuite() {
+	p.policy = &Metadata{}
+	p.policy.Trigger = &Trigger{}
+}
+
+// TearDownSuite clears the env for PolicyTestSuite.
+func (p *PolicyTestSuite) TearDownSuite() {
+	p.policy = nil
+}
+
+func (p *PolicyTestSuite) TestValidateRetentionPolicy() {
+	p.policy.Trigger.Kind = TriggerKindSchedule
+
+	// cron is not in the map of trigger setting
+	p.NoError(p.policy.ValidateRetentionPolicy())
+
+	// cron value is an empty string
+	p.policy.Trigger.Settings = map[string]interface{}{"cron": ""}
+	p.NoError(p.policy.ValidateRetentionPolicy())
+
+	//  the 1st field of cron value is not 0
+	p.policy.Trigger.Settings = map[string]interface{}{"cron": "1 0 0 1 1 *"}
+	p.Error(p.policy.ValidateRetentionPolicy())
+
+	// valid cron value
+	p.policy.Trigger.Settings = map[string]interface{}{"cron": "0 0 0 1 1 *"}
+	p.NoError(p.policy.ValidateRetentionPolicy())
+}
 
 func TestRule(t *testing.T) {
 	p := &Metadata{
