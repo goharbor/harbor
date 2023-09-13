ed370a496b56b05d5fea0ae78b3ef990b4b67553
Wang Yan <wangyan@vmware.com>
2023-09-14 01:25:52 +0800

    deprecate resource label (#19349)
    There is no api is using the DAO, remove it from the source code.
    
    Signed-off-by: wang yan <wangyan@vmware.com>

--- src/pkg/label/model/model.go
+++ src/pkg/label/model/model.go
@@ -77,19 +77,3 @@ type Reference struct {
 func (r *Reference) TableName() string {
 	return "label_reference"
 }
-
-// ResourceLabel records the relationship between resource and label
-type ResourceLabel struct {
-	ID           int64     `orm:"pk;auto;column(id)"`
-	LabelID      int64     `orm:"column(label_id)"`
-	ResourceID   int64     `orm:"column(resource_id)"`
-	ResourceName string    `orm:"column(resource_name)"`
-	ResourceType string    `orm:"column(resource_type)"`
-	CreationTime time.Time `orm:"column(creation_time);auto_now_add"`
-	UpdateTime   time.Time `orm:"column(update_time);auto_now"`
-}
-
-// TableName ...
-func (r *ResourceLabel) TableName() string {
-	return "harbor_resource_label"
-}
