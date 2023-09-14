b1be47372d6c79d43beea3b0b92c64d1249ca5ce
Michael Werner <xaseron@googlemail.com>
2023-09-14 17:22:31 +0200

    Update username in api_test.go
    Changed the Name entry under projDeveloper in the api_test.go file for testing rbac corner cases.

--- src/core/api/api_test.go
+++ src/core/api/api_test.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//    http://www.apache.org/licenses/LICENSE-2.0
+//	http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -62,7 +62,7 @@ var (
 		Passwd: "Harbor12345",
 	}
 	projDeveloper = &usrInfo{
-		Name:   "proj_developer",
+		Name:   "proj_developer,lol",
 		Passwd: "Harbor12345",
 	}
 	projGuest = &usrInfo{
