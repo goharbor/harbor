5d27ccf7bbeb649aeeaf2d39688955829c8a8781
Wang Yan <wangyan@vmware.com>
2024-01-25 12:54:05 +0800

    [cherry-pick] ] support accessory in either order (#19906)
    * remove the log for ScannerSkipUpdatePullTime
    
    fixes #19795, remove the noise in the log
    
    Signed-off-by: wang yan <wangyan@vmware.com>
    
    * support accessor in either order
    
    In certain cases, the OCI client may push the subject artifact and accessory in either order.
    Therefore, it is necessary to handle situations where the client pushes the accessory ahead of the subject artifact.
    
    Signed-off-by: wang yan <wangyan@vmware.com>
    
    * fix issue 19392
    
    Needs to set the repo when to handle the accessory before subject manifest.
    
    Signed-off-by: wang yan <wangyan@vmware.com>
    
    * fix the landing accessory data (#19661)
    
    Fix the keywords when to list accessories belong to the subject manifest.
    
    Signed-off-by: wang yan <wangyan@vmware.com>
    
    ---------
    
    Signed-off-by: wang yan <wangyan@vmware.com>

--- src/testing/pkg/accessory/manager.go
+++ src/testing/pkg/accessory/manager.go
@@ -158,6 +158,20 @@ func (_m *Manager) List(ctx context.Context, query *q.Query) ([]model.Accessory,
 	return r0, r1
 }
 
+// Update provides a mock function with given fields: ctx, _a1
+func (_m *Manager) Update(ctx context.Context, _a1 model.AccessoryData) error {
+	ret := _m.Called(ctx, _a1)
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(context.Context, model.AccessoryData) error); ok {
+		r0 = rf(ctx, _a1)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
 type mockConstructorTestingTNewManager interface {
 	mock.TestingT
 	Cleanup(func())
