158718c93ba99b2d52cb474409f46c7305acb6a3
MinerYang <yminer@vmware.com>
2024-02-02 20:25:25 +0800

    add ip_family config in harbor.yml on release-2.10 (#19939)
    add ipFamily config in values.yaml
    
    Signed-off-by: yminer <yminer@vmware.com>
    
    update name
    
    update prepare and migration
    
    update comments
    
    Signed-off-by: yminer <yminer@vmware.com>
    
    remove print msg
    
    update migrate template
    
    update default value
    
    update migrate template

--- make/photon/prepare/utils/portal.py
+++ make/photon/prepare/utils/portal.py
@@ -14,5 +14,6 @@ def prepare_portal(config_dict):
         str(portal_conf_template_path),
         portal_conf,
         internal_tls=config_dict['internal_tls'],
+        ip_family=config_dict['ip_family'],
         uid=DEFAULT_UID,
         gid=DEFAULT_GID)
