393ac84b5187d71b92b413022ea3fda1d1c156eb
stonezdj(Daojun Zhang) <stonezdj@163.com>
2024-02-23 16:18:28 +0800

    Revert "Limit url to local site (#20013)"
    This reverts commit 54819ba8cd973e77505843424bd86e836b3442ea.

--- src/core/controllers/oidc.go
+++ src/core/controllers/oidc.go
@@ -63,13 +63,7 @@ func (oc *OIDCController) RedirectLogin() {
 		oc.SendInternalServerError(err)
 		return
 	}
-	redirectURL := oc.Ctx.Request.URL.Query().Get("redirect_url")
-	if strings.HasPrefix(redirectURL, "//") {
-		log.Errorf("invalid redirect url: %v", redirectURL)
-		oc.SendBadRequestError(fmt.Errorf("cannot redirect to other site"))
-		return
-	}
-	if err := oc.SetSession(redirectURLKey, redirectURL); err != nil {
+	if err := oc.SetSession(redirectURLKey, oc.Ctx.Request.URL.Query().Get("redirect_url")); err != nil {
 		log.Errorf("failed to set session for key: %s, error: %v", redirectURLKey, err)
 		oc.SendInternalServerError(err)
 		return
