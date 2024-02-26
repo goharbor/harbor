2b6608fb52cf7b44f48ef792078635bce46256e8
stonezdj(Daojun Zhang) <stonezdj@gmail.com>
2024-02-26 13:08:35 +0800

    Move strong_ssl_ciphers to top level in harbor.yaml (#19914)
    fixes #19912
    
    Signed-off-by: stonezdj <stonezdj@gmail.com>

--- make/photon/prepare/utils/configs.py
+++ make/photon/prepare/utils/configs.py
@@ -299,7 +299,15 @@ def parse_yaml_config(config_file_path, with_trivy):
             external_database=config_dict['external_database'])
     else:
         config_dict['internal_tls'] = InternalTLS()
-    
+    # the configure item apply to internal and external tls communication
+    # for compatibility, user could configure the strong_ssl_ciphers either in https section or under internal_tls section,
+    # but it is more reasonable to configure it in https_config
+    if https_config:
+        config_dict['strong_ssl_ciphers'] =  https_config.get('strong_ssl_ciphers') or internal_tls_config.get('strong_ssl_ciphers')
+    else:
+        config_dict['strong_ssl_ciphers'] = False
+
+
     # ip_family config
     config_dict['ip_family'] = configs.get('ip_family') or {'ipv4': {'enabled': True}, 'ipv6': {'enabled': False}}
 
