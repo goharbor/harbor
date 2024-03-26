17c32f0e850103482582cd101959bb3e9d3faae9
stonezdj(Daojun Zhang) <stonezdj@gmail.com>
2024-03-26 14:08:05 +0800

    [cherry-pick] panic due to mark retention task error (#20169)
    panic due to mark retention task error
    
    fixes #20129
    
    Signed-off-by: stonezdj <daojunz@vmware.com>

--- src/controller/retention/controller.go
+++ src/controller/retention/controller.go
@@ -280,12 +280,8 @@ func (r *defaultController) TriggerRetentionExec(ctx context.Context, policyID i
 		if num, err := r.launcher.Launch(ctx, p, id, dryRun); err != nil {
 			logger.Errorf("failed to launch the retention jobs, err: %v", err)
 
-			if err = r.execMgr.StopAndWait(ctx, id, 10*time.Second); err != nil {
-				logger.Errorf("failed to stop the retention execution %d: %v", id, err)
-			}
-
-			if err = r.execMgr.MarkError(ctx, id, err.Error()); err != nil {
-				logger.Errorf("failed to mark error for the retention execution %d: %v", id, err)
+			if e := r.execMgr.StopAndWaitWithError(ctx, id, 10*time.Second, err); e != nil {
+				logger.Errorf("failed to stop the retention execution %d: %v", id, e)
 			}
 		} else if num == 0 {
 			// no candidates, mark the execution as done directly
