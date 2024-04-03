a4f2dfba2128da4dc893574c6ba136fe24df187c
Shengwen YU <yshengwen@vmware.com>
2024-04-03 15:28:21 +0800

    fix: close file (#20222)
    Signed-off-by: Shengwen Yu <yshengwen@vmware.com>

--- src/pkg/scan/export/digest_calculator.go
+++ src/pkg/scan/export/digest_calculator.go
@@ -1,3 +1,17 @@
+// Copyright Project Harbor Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package export
 
 import (
@@ -21,6 +35,7 @@ func (calc *SHA256ArtifactDigestCalculator) Calculate(fileName string) (digest.D
 	if err != nil {
 		return "", err
 	}
+	defer file.Close()
 	hash := sha256.New()
 	if _, err := io.Copy(hash, file); err != nil {
 		return "", err
