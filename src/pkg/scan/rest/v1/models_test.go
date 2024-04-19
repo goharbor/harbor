ed811f3a85f96610840deb8ff27f50e097d3cc42
stonezdj <stone.zhang@broadcom.com>
2024-04-19 16:06:07 +0800

    Set default capability for old scanners
    Signed-off-by: stonezdj <stone.zhang@broadcom.com>

--- src/pkg/scan/rest/v1/models_test.go
+++ src/pkg/scan/rest/v1/models_test.go
@@ -13,3 +13,29 @@ func TestIsSupportedMimeType(t *testing.T) {
 	// Test with an unsupported mime type
 	assert.False(t, isSupportedMimeType("unsupported/mime-type"), "isSupportedMimeType should return false for unsupported mime types")
 }
+
+func TestConvertCapability(t *testing.T) {
+	md := &ScannerAdapterMetadata{
+		Capabilities: []*ScannerCapability{
+			{Type: ScanTypeSbom},
+			{Type: ScanTypeVulnerability},
+		},
+	}
+	result := md.ConvertCapability()
+	assert.Equal(t, result[supportSBOM], true)
+	assert.Equal(t, result[supportVulnerability], true)
+}
+
+func TestConvertCapabilityOldScaner(t *testing.T) {
+	md := &ScannerAdapterMetadata{
+		Capabilities: []*ScannerCapability{
+			{
+				ConsumesMimeTypes: []string{"application/vnd.oci.image.manifest.v1+json", "application/vnd.docker.distribution.manifest.v2+json"},
+				ProducesMimeTypes: []string{MimeTypeNativeReport},
+			},
+		},
+	}
+	result := md.ConvertCapability()
+	assert.Equal(t, result[supportSBOM], false)
+	assert.Equal(t, result[supportVulnerability], true)
+}
