06571b788143c820f5648a0013eb27d623c26a30
Ethan Howell <ethan.c.howell@hotmail.com>
2024-06-21 22:30:50 -0400

    Extend regex for c2s support
    Signed-off-by: Ethan Howell <ethan.c.howell@hotmail.com>

--- src/pkg/reg/adapter/awsecr/adapter.go
+++ src/pkg/reg/adapter/awsecr/adapter.go
@@ -29,7 +29,7 @@ import (
 )
 
 const (
-	ecrPattern = "https://(?:api|(\\d+)\\.dkr)\\.ecr\\.([\\w\\-]+)\\.amazonaws\\.com"
+	ecrPattern = "https://(?:api|(\\d+)\\.dkr)\\.ecr(\\-fips)?\\.([\\w\\-]+)\\.(amazonaws\\.com(\\.cn)?|sc2s\\.sgov\\.gov|c2s\\.ic\\.gov)"
 )
 
 var (
