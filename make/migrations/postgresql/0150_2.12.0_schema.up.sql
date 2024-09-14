438d4c03bb53000a304243fa06e5a174a2ee119d
Wang Yan <wangyan@vmware.com>
2024-09-14 12:49:32 +0800

    update robot creator database scheme (#20918)
    Signed-off-by: wang yan <wangyan@vmware.com>

--- make/migrations/postgresql/0150_2.12.0_schema.up.sql
+++ make/migrations/postgresql/0150_2.12.0_schema.up.sql
@@ -1,5 +1,5 @@
 /*
-Add new column creator for robot table to add a new column to record the creator of the robot
+Add new column creator_ref and creator_type for robot table to record the creator information of the robot
 */
-ALTER TABLE robot ADD COLUMN IF NOT EXISTS creator varchar(255);
-UPDATE robot SET creator = 'unknown' WHERE creator IS NULL;
+ALTER TABLE robot ADD COLUMN IF NOT EXISTS creator_ref integer default 0;
+ALTER TABLE robot ADD COLUMN IF NOT EXISTS creator_type varchar(255);
