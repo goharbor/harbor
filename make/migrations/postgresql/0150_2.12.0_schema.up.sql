c97253f66034edf826a62cfe34be2fc47c30ebb3
Chlins Zhang <chlins.zhang@gmail.com>
2024-09-21 11:05:01 +0800

    Refactor: p2p preheat dragonfly driver (#20922)

--- make/migrations/postgresql/0150_2.12.0_schema.up.sql
+++ make/migrations/postgresql/0150_2.12.0_schema.up.sql
@@ -3,3 +3,5 @@ Add new column creator_ref and creator_type for robot table to record the creato
 */
 ALTER TABLE robot ADD COLUMN IF NOT EXISTS creator_ref integer default 0;
 ALTER TABLE robot ADD COLUMN IF NOT EXISTS creator_type varchar(255);
+
+ALTER TABLE p2p_preheat_policy ADD COLUMN IF NOT EXISTS scope varchar(255);
