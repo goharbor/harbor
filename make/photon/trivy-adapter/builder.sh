7574e9df80d292fac6155599fd674490a8305d31
Mitsuru Kariya <mitsuru.kariya@nttdata.com>
2024-10-02 02:04:19 +0900

    Add DOCKER_BUILDKIT=1 to docker build command
    Add DOCKER_BUILDKIT=1 to the docker build command because the -o option
    of the docker build command and the --mount option of the RUN instruction
    require buildkit.
    
    Signed-off-by: Mitsuru Kariya <mitsuru.kariya@nttdata.com>

--- make/photon/trivy-adapter/builder.sh
+++ make/photon/trivy-adapter/builder.sh
@@ -16,7 +16,7 @@ TEMP=$(mktemp -d ${TMPDIR-/tmp}/trivy-adapter.XXXXXX)
 git clone -b $VERSION --depth 1 https://github.com/aquasecurity/harbor-scanner-trivy.git $TEMP
 
 echo "Building Trivy adapter binary based on golang:1.22.3..."
-docker build -f Dockerfile.binary -o binary/ $TEMP
+DOCKER_BUILDKIT=1 docker build -f Dockerfile.binary -o binary/ $TEMP
 
 echo "Building Trivy adapter binary finished successfully"
 rm -rf $TEMP
