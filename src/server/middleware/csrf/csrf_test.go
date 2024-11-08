b733cf850cd90a27bcff8847bafd391b68d8bda0
wang yan <wangyan@vmware.com>
2024-11-08 17:28:59 +0800

    update csrf key generation
    Fixes #21060
    
    Do not generate a random key if the provided key has an invalid length.
    
    Signed-off-by: wang yan <wangyan@vmware.com>

--- src/server/middleware/csrf/csrf_test.go
+++ src/server/middleware/csrf/csrf_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/stretchr/testify/assert"
 
 	"github.com/goharbor/harbor/src/common"
+	"github.com/goharbor/harbor/src/common/utils"
 	"github.com/goharbor/harbor/src/common/utils/test"
 	"github.com/goharbor/harbor/src/lib/config"
 	_ "github.com/goharbor/harbor/src/pkg/config/inmemory"
@@ -32,48 +33,55 @@ func (h *handler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 }
 
 func TestMiddleware(t *testing.T) {
-	srv := Middleware()(&handler{})
 	cases := []struct {
 		req         *http.Request
 		statusCode  int
 		returnToken bool
+		validKey    bool
 	}{
 		{
 			req:         httptest.NewRequest(http.MethodGet, "/", nil),
 			statusCode:  http.StatusOK,
 			returnToken: true,
+			validKey:    true,
 		},
 		{
 			req:         httptest.NewRequest(http.MethodDelete, "/", nil),
 			statusCode:  http.StatusForbidden,
 			returnToken: true,
+			validKey:    true,
 		},
 		{
 			req:         httptest.NewRequest(http.MethodGet, "/api/2.0/projects", nil), // should be skipped
 			statusCode:  http.StatusOK,
 			returnToken: false,
+			validKey:    true,
 		},
 		{
 			req:         httptest.NewRequest(http.MethodDelete, "/v2/library/hello-world/manifests/latest", nil), // should be skipped
 			statusCode:  http.StatusOK,
 			returnToken: false,
+			validKey:    true,
+		},
+		{
+			req:         httptest.NewRequest(http.MethodGet, "/", nil),
+			statusCode:  http.StatusInternalServerError,
+			returnToken: false,
+			validKey:    false,
 		},
 	}
 	for _, c := range cases {
+		if c.validKey {
+			os.Setenv(csrfKeyEnv, utils.GenerateRandomStringWithLen(32))
+		} else {
+			os.Setenv(csrfKeyEnv, utils.GenerateRandomStringWithLen(10))
+		}
+		srv := Middleware()(&handler{})
 		rec := httptest.NewRecorder()
 		srv.ServeHTTP(rec, c.req)
 		assert.Equal(t, c.statusCode, rec.Result().StatusCode)
 		assert.Equal(t, c.returnToken, rec.Result().Header.Get(tokenHeader) != "")
 	}
-}
-
-func hasCookie(resp *http.Response, name string) bool {
-	for _, c := range resp.Cookies() {
-		if c != nil && c.Name == name {
-			return true
-		}
-	}
-	return false
 }
 
 func TestSecureCookie(t *testing.T) {
