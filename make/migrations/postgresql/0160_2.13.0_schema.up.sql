12afc5ca22e5ec90c9100880bad35dee39097f6d
bupd <bupdprasanth@gmail.com>
2024-12-22 22:04:20 +0530

    Add Skip if running checkbox in replication & update swagger
    * Adds Checkbox in replication policy UI
    * Updates swagger & sql schema
    
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- make/migrations/postgresql/0160_2.13.0_schema.up.sql
+++ make/migrations/postgresql/0160_2.13.0_schema.up.sql
@@ -1,2 +1,3 @@
 ALTER TABLE p2p_preheat_policy DROP COLUMN IF EXISTS scope;
 ALTER TABLE p2p_preheat_policy ADD COLUMN IF NOT EXISTS extra_attrs text;
+ALTER TABLE replication_policy ADD COLUMN IF NOT EXISTS skip_if_running boolean;
