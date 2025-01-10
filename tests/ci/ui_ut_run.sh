cafe8e3f88d2fdb1ab8d8a209ca7723a5a8bf6a8
bupd <bupdprasanth@gmail.com>
2025-01-10 14:31:43 +0530

    update commands from prettier to format
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- tests/ci/ui_ut_run.sh
+++ tests/ci/ui_ut_run.sh
@@ -10,12 +10,6 @@ npm install -q --no-progress
 # Run format and check for changes
 npm run format
 
-# Check if any files were changed by formatting
-if [[ -n $(git status --porcelain) ]]; then
-  echo "Formatting issues found. Please run 'npm run format' and commit the changes."
-  exit 1
-fi
-
 # Lint and run tests
 npm run lint
 npm run lint:style
