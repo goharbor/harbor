623196152430334d5d226ae927d2aafc5ca9c4f9
stonezdj <stone.zhang@broadcom.com>
2025-03-06 17:34:20 +0800

    try build with lint:ignore
    Signed-off-by: stonezdj <stone.zhang@broadcom.com>

--- src/lib/trace/trace.go
+++ src/lib/trace/trace.go
@@ -20,6 +20,8 @@ import (
 
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
+
+	//lint:ignore SA1019 reason
 	"go.opentelemetry.io/otel/exporters/jaeger"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
 	"go.opentelemetry.io/otel/propagation"
