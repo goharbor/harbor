f453db3287843306b5dc907ce7c3b1ac08677219
bupd <bupdprasanth@gmail.com>
2025-04-30 01:43:34 +0530

    add db nightly test
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- tests/ci/api_run.sh
+++ tests/ci/api_run.sh
@@ -19,6 +19,8 @@ set +e
 
 docker ps
 # run db auth api cases
+if [ "$1" = 'DB_NIGHTLY' ]; then
+    docker run -i --privileged -v $DIR/../../:/drone -v $DIR/../:/ca -w /drone $E2E_IMAGE robot --exclude proxy_cache -v DOCKER_USER:${DOCKER_USER} -v DOCKER_PWD:${DOCKER_PWD} -v ip:$2  -v ip1: -v http_get_ca:false -v HARBOR_PASSWORD:Harbor12345 /drone/tests/robot-cases/Group1-Nightly/Setup.robot /drone/tests/robot-cases/Group1-Nightly/DB.robot
 if [ "$1" = 'DB' ]; then
     docker run -i --privileged -v $DIR/../../:/drone -v $DIR/../:/ca -w /drone $E2E_IMAGE robot --exclude proxy_cache -v DOCKER_USER:"${DOCKER_USER}" -v DOCKER_PWD:${DOCKER_PWD} -v ip:$2  -v ip1: -v http_get_ca:false -v HARBOR_PASSWORD:${HARBOR_ADMIN_PASSWD} -v HARBOR_ADMIN:${HARBOR_ADMIN} /drone/tests/robot-cases/Group1-Nightly/Setup.robot /drone/tests/robot-cases/Group0-BAT/API_DB.robot
 elif [ "$1" = 'PROXY_CACHE' ]; then
