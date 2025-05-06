3ddc40c8defe10b2e2b878d4d98364d8a415e712
bupd <bupdprasanth@gmail.com>
2025-05-06 13:58:41 +0530

    fix: hide admin only alert messages from users in UI
    - hides alert messages irrelevant to users
    
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- src/portal/src/app/shared/components/app-level-alerts/app-level-alerts.component.ts
+++ src/portal/src/app/shared/components/app-level-alerts/app-level-alerts.component.ts
@@ -280,4 +280,11 @@ export class AppLevelAlertsComponent implements OnInit, OnDestroy {
         }
         return true;
     }
+
+    hasAdminRole(): boolean {
+        return (
+            this.session.getCurrentUser() &&
+            this.session.getCurrentUser().has_admin_role
+        );
+    }
 }
