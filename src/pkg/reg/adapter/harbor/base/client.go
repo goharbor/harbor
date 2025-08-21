0526f5bbc1ff7aa691be25036b649944016832bc
Nick Hindley <nicholas.hindley@yoti.com>
2025-08-21 11:00:42 +0100

    Fix to allow repo replication when using non-standard port for core
    Use CORE_LOCAL_URL instead of hardcoded setting
    
    Signed-off-by: Nick Hindley <nicholas.hindley@yoti.com>

--- src/pkg/reg/adapter/harbor/base/client.go
+++ src/pkg/reg/adapter/harbor/base/client.go
@@ -147,7 +147,10 @@ func (c *Client) GetURL() string {
 	// inside core, returns the "127.0.0.1" as URL to avoid the issue:
 	// https://github.com/goharbor/harbor-helm/issues/222
 	// when harbor is deployed on Kubernetes with hairpin mode disabled
-	url := "http://127.0.0.1:8080"
+
+	// get the URL from the environment so can use non-standard port
+        url := os.Getenv("CORE_LOCAL_URL")
+
 	if common_http.InternalTLSEnabled() {
 		url = "https://127.0.0.1:8443"
 	}
