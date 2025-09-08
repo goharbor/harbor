fcb40a9dc776f6035ab8376e7c57a8f5a50a13a5
bupd <bupdprasanth@gmail.com>
2025-09-08 06:03:50 +0530

    fix: add local registry ns
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- tests/ci/api_run.sh
+++ tests/ci/api_run.sh
@@ -11,6 +11,7 @@ DIR="$(cd "$(dirname "$0")" && pwd)"
 E2E_IMAGE="goharbor/harbor-e2e-engine:latest-api"
 E2E_IMAGE_UI="goharbor/harbor-e2e-engine:latest-ui"
 LOCAL_REGISTRY="registry.goharbor.io"
+LOCAL_REGISTRY_NAMESPACE="harbor-ci"
 
 # GS util
 function uploader {
@@ -24,7 +25,7 @@ docker ps
 if [ "$1" = 'DB' ]; then
     docker run -i --privileged -v $DIR/../../:/drone -v $DIR/../:/ca -w /drone $E2E_IMAGE robot --exclude proxy_cache -v DOCKER_USER:"${DOCKER_USER}" -v DOCKER_PWD:${DOCKER_PWD} -v ip:$2  -v ip1: -v http_get_ca:false -v HARBOR_PASSWORD:${HARBOR_ADMIN_PASSWD} -v HARBOR_ADMIN:${HARBOR_ADMIN} /drone/tests/robot-cases/Group1-Nightly/Setup.robot /drone/tests/robot-cases/Group0-BAT/API_DB.robot
 elif [ "$1" = 'COMMON_GC_NIGHTLY' ]; then
-    docker run -i --privileged -v $DIR/../../:/drone -v $DIR/../:/ca -w /drone $E2E_IMAGE_UI robot --exclude proxy_cache -v DOCKER_USER:"${DOCKER_USER}" -v DOCKER_PWD:${DOCKER_PWD} -v ip:$2  -v ip1: -v http_get_ca:false -e LOCAL_REGISTRY:${LOCAL_REGISTRY} -v HARBOR_PASSWORD:${HARBOR_ADMIN_PASSWD} -v HARBOR_ADMIN:${HARBOR_ADMIN} /drone/tests/robot-cases/Group1-Nightly/Setup.robot /drone/tests/robot-cases/Group1-Nightly/Common_GC.robot
+    docker run -i --privileged -v $DIR/../../:/drone -v $DIR/../:/ca -w /drone $E2E_IMAGE_UI robot --exclude proxy_cache -v DOCKER_USER:"${DOCKER_USER}" -v DOCKER_PWD:${DOCKER_PWD} -v ip:$2  -v ip1: -v http_get_ca:false -e LOCAL_REGISTRY:${LOCAL_REGISTRY} -v LOCAL_REGISTRY_NAMESPACE:${LOCAL_REGISTRY_NAMESPACE} -v HARBOR_PASSWORD:${HARBOR_ADMIN_PASSWD} -v HARBOR_ADMIN:${HARBOR_ADMIN} /drone/tests/robot-cases/Group1-Nightly/Setup.robot /drone/tests/robot-cases/Group1-Nightly/Common_GC.robot
 elif [ "$1" = 'PROXY_CACHE' ]; then
     docker run -i --privileged -v $DIR/../../:/drone -v $DIR/../:/ca -w /drone $E2E_IMAGE robot --include setup  --include proxy_cache -v DOCKER_USER:"${DOCKER_USER}" -v DOCKER_PWD:${DOCKER_PWD} -v ip:$2  -v ip1: -v http_get_ca:false -v HARBOR_PASSWORD:${HARBOR_ADMIN_PASSWD} -v HARBOR_ADMIN:${HARBOR_ADMIN} /drone/tests/robot-cases/Group1-Nightly/Setup.robot /drone/tests/robot-cases/Group0-BAT/API_DB.robot
 elif [ "$1" = 'LDAP' ]; then
