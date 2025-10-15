ce207d4cd210b7fbbbc64d5833dbe9c8ffa557dd
bupd <bupdprasanth@gmail.com>
2025-10-15 16:46:41 +0530

    feat: correct test
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- src/portal/e2e/login-logout.spec.ts
+++ src/portal/e2e/login-logout.spec.ts
@@ -1,10 +1,10 @@
-import { expect, test } from '@playwright/test';
+import { test } from '@playwright/test';
 
 test('login and logout', async ({ page }) => {
     // Recording...await page.getByRole('button', { name: 'Advanced' }).click();
     await page.goto('/');
     await page.getByRole('textbox', { name: 'Username' }).click();
-    await page.getByRole('textbox', { name: 'Username' }).fill('harbor-cli');
+    await page.getByRole('textbox', { name: 'Username' }).fill('admin');
     await page.getByRole('textbox', { name: 'Password' }).click();
     await page.getByRole('textbox', { name: 'Password' }).fill('Harbor12345');
 
@@ -12,11 +12,6 @@ test('login and logout', async ({ page }) => {
 
     await page.waitForLoadState('networkidle');
     await page.waitForTimeout(5000);
-    const userButton = page.getByRole('button', { name: 'harbor-cli' });
-    await expect(userButton).toBeVisible();
-    await expect(userButton).toBeEnabled();
-
-    await page.getByRole('button', { name: 'harbor-cli' }).click();
-    await page.getByRole('button', { name: 'harbor-cli' }).dblclick();
+    await page.getByRole('button', { name: 'admin', exact: true }).click();
     await page.getByRole('menuitem', { name: 'Log Out' }).dblclick();
 });
