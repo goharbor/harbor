42cd7c5b87036149b28520466c810ed5c19b5b4d
ramu-nukavarapu <ramunukavarapu2004@gmail.com>
2025-10-18 22:25:31 +0530

    refactor: create new user testcase
    Signed-off-by: ramu-nukavarapu <ramunukavarapu2004@gmail.com>

--- src/portal/e2e/db-testcases.spec.ts
+++ src/portal/e2e/db-testcases.spec.ts
@@ -1,5 +1,27 @@
 import { test } from '@playwright/test';
 
+async function createUser(page) {
+    await page.goto('/');
+    await page.getByRole('link', { name: 'Sign up for an account' }).click();
+    await page.locator('#username').click();
+    await page.locator('#username').fill('user');
+    await page.locator('#username').press('Home');
+    const timestamp = Date.now();
+    const username = "harbor-user" + timestamp
+    await page.locator('#username').fill(username);
+    await page.locator('#username').press('ArrowDown');
+    await page.locator('new-user-form div').filter({ hasText: 'Email is only used for' }).nth(3).click();
+    const email = username + "@example.com"
+    await page.locator('#email').fill(email);
+    await page.locator('clr-input-container').filter({ hasText: 'First and last name' }).locator('div').nth(1).click();
+    await page.getByRole('textbox', { name: 'First and last name*' }).fill(username);
+    await page.getByRole('textbox', { name: 'Password*', exact: true }).click();
+    await page.getByRole('textbox', { name: 'Password*', exact: true }).fill('Harbor12345');
+    await page.getByRole('textbox', { name: 'Confirm Password*' }).click();
+    await page.getByRole('textbox', { name: 'Confirm Password*' }).fill('Harbor12345');
+    await page.getByRole('button', { name: 'SIGN UP' }).click();
+}
+
 
 test('Create An New User', async ({ page }) => {
     // Login
@@ -28,23 +50,5 @@ test('Create An New User', async ({ page }) => {
     await page.getByRole('menuitem', { name: 'Log Out' }).dblclick();
 
     //Creating user
-    await page.goto('/');
-    await page.getByRole('link', { name: 'Sign up for an account' }).click();
-    await page.locator('#username').click();
-    await page.locator('#username').fill('user');
-    await page.locator('#username').press('Home');
-    const timestamp = Date.now();
-    const username = "harbor-user" + timestamp
-    await page.locator('#username').fill(username);
-    await page.locator('#username').press('ArrowDown');
-    await page.locator('new-user-form div').filter({ hasText: 'Email is only used for' }).nth(3).click();
-    const email = username + "@example.com"
-    await page.locator('#email').fill(email);
-    await page.locator('clr-input-container').filter({ hasText: 'First and last name' }).locator('div').nth(1).click();
-    await page.getByRole('textbox', { name: 'First and last name*' }).fill(username);
-    await page.getByRole('textbox', { name: 'Password*', exact: true }).click();
-    await page.getByRole('textbox', { name: 'Password*', exact: true }).fill('Harbor12345');
-    await page.getByRole('textbox', { name: 'Confirm Password*' }).click();
-    await page.getByRole('textbox', { name: 'Confirm Password*' }).fill('Harbor12345');
-    await page.getByRole('button', { name: 'SIGN UP' }).click();
+    await createUser(page)
 });
