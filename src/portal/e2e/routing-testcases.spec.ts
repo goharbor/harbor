c7daf898cb3e108c21f4fb2f93d845f1b52a0332
ramu-nukavarapu <ramunukavarapu2004@gmail.com>
2025-10-27 23:45:44 +0530

    feat: Add Main menu routing testcase
    Signed-off-by: ramu-nukavarapu <ramunukavarapu2004@gmail.com>

---
+++ src/portal/e2e/routing-testcases.spec.ts
@@ -0,0 +1,45 @@
+import { test, expect, Page } from '@playwright/test';
+
+async function Login(page: Page) {
+    // Login
+    await page.goto('/');
+    await page.getByRole('textbox', { name: 'Username' }).click();
+    await page.getByRole('textbox', { name: 'Username' }).fill('admin');
+    await page.getByRole('textbox', { name: 'Password' }).click();
+    await page.getByRole('textbox', { name: 'Password' }).fill('Harbor12345');
+
+    await page.getByRole('button', { name: 'LOG IN' }).click();
+}
+
+test('Main Menu Routing', async ({ page }) => {
+    test.setTimeout(60_000);
+    await Login(page);
+    const pageChecks: Record<string, (page: Page) => any> = {
+        'harbor/projects': (page: Page) => page.locator('projects div h2:has-text("Projects")'),
+        'harbor/logs': (page: Page) => page.locator('app-logs h2:has-text("Logs")'),
+        'harbor/users': (page: Page) => page.locator('harbor-user div h2:has-text("Users")'),
+        'harbor/robot-accounts': (page: Page) => page.locator('system-robot-accounts h2:has-text("Robot Accounts")'),
+        'harbor/registries': (page: Page) => page.locator('hbr-endpoint h2:has-text("Registries")'),
+        'harbor/replications': (page: Page) => page.locator('total-replication h2:has-text("Replications")'),
+        'harbor/distribution/instances': (page: Page) => page.locator('dist-instances div h2:has-text("Instances")'),
+        'harbor/labels': (page: Page) => page.locator('app-labels h2:has-text("Labels")'),
+        'harbor/project-quotas': (page: Page) => page.locator('app-project-quotas h2:has-text("Project Quotas")'),
+        'harbor/interrogation-services/scanners': (page: Page) => page.locator('config-scanner div h4:has-text("Image Scanners")'),
+        'harbor/interrogation-services/vulnerability': (page: Page) => page.locator('vulnerability-config div button:has-text("SCAN NOW")'),
+        'harbor/interrogation-services/security-hub': (page: Page) => page.locator('h1:has-text("Vulnerabilities")'),
+        'harbor/clearing-job/gc': (page: Page) => page.locator('gc-history h5:has-text("GC History")'),
+        'harbor/clearing-job/audit-log-purge': (page: Page) => page.locator('app-purge-history  h5:has-text("Purge History")'),
+        'harbor/job-service-dashboard/pending-jobs': (page: Page) => page.locator('app-pending-job-list button span:has-text("STOP")'),
+        'harbor/job-service-dashboard/schedules': (page: Page) => page.locator('app-schedule-list clr-dg-cell:has-text("SYSTEM_ARTIFACT_CLEANUP")'),
+        'harbor/job-service-dashboard/workers': (page: Page) => page.locator('app-worker-list button span:has-text("Free")'),
+        'harbor/configs/auth': (page: Page) => page.locator('config config-auth label:has-text("Auth Mode")'),
+        'harbor/configs/security': (page: Page) => page.locator('config app-security span:has-text("CVE allowlist")'),
+        'harbor/configs/setting': (page: Page) => page.locator('config system-settings label:has-text("Project Creation")'),
+    };
+
+    for (const [path, locatorFn] of Object.entries(pageChecks)) {
+        await page.goto(`/${path}`);
+        const element = locatorFn(page);
+        await expect(element).toBeVisible();
+    }
+});
