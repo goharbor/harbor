7d635732cc85aac42c92115ae0e7174c4648edac
Ross Golder <ross@golder.org>
2025-11-06 12:18:29 +0700

    Add Discord notification handler
    - Add DiscordJobVendorType and register DiscordJob
    - Add DiscordTopic and NotifyTypeDiscord
    - Implement DiscordHandler to send embeds to Discord webhooks
    - Implement DiscordJob for job service
    - Update webhook controller to handle Discord jobs
    - Add tests for Discord handler and job

---
+++ src/pkg/notifier/handler/notification/discord_handler_test.go
@@ -0,0 +1,106 @@
+package notification
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/goharbor/harbor/src/pkg/notification"
+	policy_model "github.com/goharbor/harbor/src/pkg/notification/policy/model"
+	"github.com/goharbor/harbor/src/pkg/notifier/event"
+	"github.com/goharbor/harbor/src/pkg/notifier/model"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestDiscordHandler_Handle(t *testing.T) {
+	hookMgr := notification.HookManager
+	defer func() {
+		notification.HookManager = hookMgr
+	}()
+	notification.HookManager = &fakedHookManager{}
+
+	handler := &DiscordHandler{}
+
+	type args struct {
+		event *event.Event
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "DiscordHandler_Handle Want Error 1",
+			args: args{
+				event: &event.Event{
+					Topic: "discord",
+					Data:  nil,
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "DiscordHandler_Handle Want Error 2",
+			args: args{
+				event: &event.Event{
+					Topic: "discord",
+					Data:  &model.EventData{},
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "DiscordHandler_Handle 1",
+			args: args{
+				event: &event.Event{
+					Topic: "discord",
+					Data: &model.HookEvent{
+						PolicyID:  1,
+						EventType: "pushImage",
+						Target: &policy_model.EventTarget{
+							Type:    "discord",
+							Address: "http://127.0.0.1:8080",
+						},
+						Payload: &model.Payload{
+							OccurAt:  time.Now().Unix(),
+							Type:     "pushImage",
+							Operator: "admin",
+							EventData: &model.EventData{
+								Resources: []*model.Resource{
+									{
+										Tag: "v9.0",
+									},
+								},
+								Repository: &model.Repository{
+									Name: "library/debian",
+								},
+							},
+						},
+					},
+				},
+			},
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := handler.Handle(context.TODO(), tt.args.event.Data)
+			if tt.wantErr {
+				require.NotNil(t, err, "Error: %s", err)
+				return
+			}
+		})
+	}
+}
+
+func TestDiscordHandler_IsStateful(t *testing.T) {
+	handler := &DiscordHandler{}
+	assert.False(t, handler.IsStateful())
+}
+
+func TestDiscordHandler_Name(t *testing.T) {
+	handler := &DiscordHandler{}
+	assert.Equal(t, "Discord", handler.Name())
+}
