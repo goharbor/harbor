dd1e0c038c7ab09a6bc482d08d38174fc033174c
Ross Golder <ross@golder.org>
2025-11-06 12:21:12 +0700

    Add Email notification handler
    - Add EmailJobVendorType and register EmailJob
    - Add NotifyTypeEmail
    - Implement EmailHandler to format email content
    - Implement EmailJob to send emails via SMTP using Harbor config
    - Update webhook controller to handle Email jobs
    - Add tests for Email handler and job

--- src/pkg/notifier/topic/topics.go
+++ src/pkg/notifier/topic/topics.go
@@ -26,6 +26,7 @@ func init() {
 	handlersMap := map[string][]notifier.NotificationHandler{
 		model.WebhookTopic: {&notification.HTTPHandler{}},
 		model.SlackTopic:   {&notification.SlackHandler{}},
+		model.EmailTopic:   {&notification.EmailHandler{}},
 	}
 
 	for t, handlers := range handlersMap {
