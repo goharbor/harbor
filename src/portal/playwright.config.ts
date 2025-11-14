360848e5d2b327e67c6b17fc84e16b6277607848
bupd <bupdprasanth@gmail.com>
2025-11-14 19:50:43 +0530

    feat: update playwright global config
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- src/portal/playwright.config.ts
+++ src/portal/playwright.config.ts
@@ -12,6 +12,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+    globalTimeout: 60 * 60 * 1000,
     testDir: './e2e',
     /* Run tests in files in parallel */
     fullyParallel: false,
