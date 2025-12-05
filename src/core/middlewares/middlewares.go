f047068d0c11a1da1a383f427306272429ca9b95
Vadim Bauer <vb@container-registry.com>
2025-12-05 16:09:07 +0100

    fix: as requested merge validation into parse.go, validate also keys,

--- src/core/middlewares/middlewares.go
+++ src/core/middlewares/middlewares.go
@@ -31,7 +31,6 @@ import (
 	"github.com/goharbor/harbor/src/server/middleware/orm"
 	"github.com/goharbor/harbor/src/server/middleware/readonly"
 	"github.com/goharbor/harbor/src/server/middleware/requestid"
-	"github.com/goharbor/harbor/src/server/middleware/validate"
 	"github.com/goharbor/harbor/src/server/middleware/security"
 	"github.com/goharbor/harbor/src/server/middleware/session"
 	"github.com/goharbor/harbor/src/server/middleware/trace"
@@ -88,7 +87,6 @@ func MiddleWares() []web.MiddleWare {
 	return []web.MiddleWare{
 		url.Middleware(),
 		mergeslash.Middleware(),
-		validate.Middleware(), // validate query params, reject null bytes and invalid UTF-8
 		trace.Middleware(),
 		metric.Middleware(),
 		requestid.Middleware(),
