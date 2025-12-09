d1134545fcc033b780c188242fe076dce1d86587
vg006 <devvg006@gmail.com>
2025-12-09 10:23:53 +0530

    feat(test): Add few basic test cases and fixtures
    Signed-off-by: vg006 <devvg006@gmail.com>

---
+++ src/portal/e2e/fixtures/harbor.ts
@@ -0,0 +1,57 @@
+import { test as base, Page } from '@playwright/test';
+
+type HarborUser = {
+    username: string;
+    password: string;
+};
+
+type HarborFixtures = {
+    harborPage: Page;
+    harborUser: HarborUser;
+};
+
+async function login(page: Page, baseURL: string | undefined, creds: HarborUser) {
+    await page.goto(baseURL);
+    await page.getByRole('textbox', { name: 'Username' }).fill(creds.username);
+    await page.getByRole('textbox', { name: 'Password' }).fill(creds.password);
+    await page.getByRole('button', { name: 'LOG IN' }).click();
+}
+
+async function logout(page: Page, username: string) {
+    await page.getByRole('button', { name: username, exact: true }).waitFor({ state: 'visible', timeout: 5000 });
+    await page.getByRole('button', { name: username, exact: true }).click();
+    await page.getByRole('menuitem', { name: 'Log Out' }).click();
+}
+
+async function logoutIfPossible(page: Page, username: string) {
+    try {
+        await page.getByRole('button', { name: username, exact: true }).waitFor({ state: 'visible', timeout: 2000 });
+    } catch {
+        return;
+    }
+
+    try {
+        await logout(page, username);
+    } catch {
+        return;
+    }
+}
+
+const harborTest = base.extend<HarborFixtures>({
+    harborUser: async ({}, use) => {
+        await use({
+            username: process.env.HARBOR_USERNAME || 'admin',
+            password: process.env.HARBOR_PASSWORD || 'Harbor12345',
+        });
+    },
+
+    harborPage: async ({ page, harborUser }, use) => {
+        let baseURL = process.env.HARBOR_BASE_URL;
+        await login(page, baseURL, harborUser);
+        await use(page);
+        await logoutIfPossible(page, harborUser.username);
+    },
+});
+
+export const test = harborTest;
+export const expect = harborTest.expect;
