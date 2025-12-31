36721bc529fe335ba3133ba93e3a0c10a6c6d22f
Aditya Chopra <adityachopra2912@gmail.com>
2025-12-31 18:52:22 +0530

    feat: create createProject function
    Signed-off-by: Aditya Chopra <adityachopra2912@gmail.com>

--- src/portal/e2e/common-gc.spec.ts
+++ src/portal/e2e/common-gc.spec.ts
@@ -8,6 +8,22 @@ async function loginAsAdmin(page: Page) {
     await expect(page.getByRole('link', { name: 'Projects' })).toBeVisible();
 }
 
+async function createProject(page: Page, projectName: string, isPublic: boolean = false) {
+    await page.getByRole("link", { name: "Projects" }).click();
+    await page.getByRole("button", { name: "NEW PROJECT" }).click();
+    await page.locator("//*[@id='create_project_name']").fill(projectName);
+    if (isPublic) {
+        await page.locator(`xpath=//input[@name='public']/..//label[contains(@class,'clr-control-label')]`).check();
+    }
+    await page.getByRole('button', { name: 'OK' }).click();
+    await expect(page.getByRole('link', {name: projectName})).toBeVisible()
+}
+
 test('Project Quota Sorting', async ({ page }) => {
     await loginAsAdmin(page);
+
+    const timestamp1 = Date.now();
+    const project1 = `project${timestamp1}`;
+    console.log(project1);
+    await createProject(page, project1);
 })
