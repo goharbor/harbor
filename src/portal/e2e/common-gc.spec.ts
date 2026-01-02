d0b66dfa643df30cb58924d62c5a9d57f35d0dca
Aditya Chopra <adityachopra2912@gmail.com>
2026-01-02 19:43:46 +0530

    fix indentation
    Signed-off-by: Aditya Chopra <adityachopra2912@gmail.com>

--- src/portal/e2e/common-gc.spec.ts
+++ src/portal/e2e/common-gc.spec.ts
@@ -21,22 +21,22 @@ const localRegistryName = process.env.LOCAL_REGISTRY || 'docker.io';
 const localRegistryNamespace = process.env.LOCAL_REGISTRY_NAMESPACE || 'library';
 
 async function loginAsAdmin(page: Page) {
-    await page.goto(harborIp);
-    await page.getByRole('textbox', { name: 'Username' }).fill('admin');
-    await page.getByRole('textbox', { name: 'Password' }).fill('Harbor12345');
-    await page.getByRole('button', { name: 'LOG IN' }).click();
-    await expect(page.getByRole('link', { name: 'Projects' })).toBeVisible();
+  await page.goto(harborIp);
+  await page.getByRole('textbox', { name: 'Username' }).fill('admin');
+  await page.getByRole('textbox', { name: 'Password' }).fill('Harbor12345');
+  await page.getByRole('button', { name: 'LOG IN' }).click();
+  await expect(page.getByRole('link', { name: 'Projects' })).toBeVisible();
 }
 
 async function createProject(page: Page, projectName: string, isPublic: boolean = false) {
-    await page.getByRole("link", { name: "Projects" }).click();
-    await page.getByRole("button", { name: "NEW PROJECT" }).click();
-    await page.locator("//*[@id='create_project_name']").fill(projectName);
-    if (isPublic) {
-        await page.locator(`xpath=//input[@name='public']/..//label[contains(@class,'clr-control-label')]`).check();
-    }
-    await page.getByRole('button', { name: 'OK' }).click();
-    await expect(page.getByRole('link', {name: projectName})).toBeVisible()
+  await page.getByRole("link", { name: "Projects" }).click();
+  await page.getByRole("button", { name: "NEW PROJECT" }).click();
+  await page.locator("//*[@id='create_project_name']").fill(projectName);
+  if (isPublic) {
+      await page.locator(`xpath=//input[@name='public']/..//label[contains(@class,'clr-control-label')]`).check();
+  }
+  await page.getByRole('button', { name: 'OK' }).click();
+  await expect(page.getByRole('link', {name: projectName})).toBeVisible()
 }
 
 async function pushImage(options: PushImageOptions) {
@@ -80,19 +80,19 @@ async function pushImage(options: PushImageOptions) {
 }
 
 async function goIntoProject(page: Page, projectName: string) {
-    await page.getByRole('link', { name: 'Projects' }).click();
-    await expect(page.getByRole('link', {name: projectName})).toBeVisible()
-    await page.getByRole('link', {name: projectName}).click();
+  await page.getByRole('link', { name: 'Projects' }).click();
+  await expect(page.getByRole('link', {name: projectName})).toBeVisible()
+  await page.getByRole('link', {name: projectName}).click();
 }
 
 async function deleteRepo(page: Page, projectName: string, repoName: string) {
-    await goIntoProject(page, projectName);
-    const repoRow = page.locator(` xpath=//clr-dg-row[contains(.,'${projectName}/${repoName}')]//div[contains(@class,'clr-checkbox-wrapper')]//label[contains(@class,'clr-control-label')]`);
-    await repoRow.check();
+  await goIntoProject(page, projectName);
+  const repoRow = page.locator(` xpath=//clr-dg-row[contains(.,'${projectName}/${repoName}')]//div[contains(@class,'clr-checkbox-wrapper')]//label[contains(@class,'clr-control-label')]`);
+  await repoRow.check();
 
-    await page.getByRole('button', { name: 'DELETE' }).click();
-    await page.locator("xpath=//button[contains(.,'DELETE')]").click();
-    await expect(repoRow).not.toBeVisible();
+  await page.getByRole('button', { name: 'DELETE' }).click();
+  await page.locator("xpath=//button[contains(.,'DELETE')]").click();
+  await expect(repoRow).not.toBeVisible();
 
 }
 
@@ -138,68 +138,68 @@ async function checkProjectQuotaSorting(
 }
 
 async function runGC(page: Page, workers?: number, deleteUntagged: boolean = false, gc_now: boolean = false) {
-    await page.locator(" //clr-main-container//clr-vertical-nav-group//span[contains(.,'Clean Up')]").click();
-    await page.getByRole('link', { name: 'Garbage Collection' }).click();
+  await page.locator(" //clr-main-container//clr-vertical-nav-group//span[contains(.,'Clean Up')]").click();
+  await page.getByRole('link', { name: 'Garbage Collection' }).click();
 
-    if (workers) {
-        await page.selectOption('#workers', workers.toString())
-    }
+  if (workers) {
+      await page.selectOption('#workers', workers.toString())
+  }
 
-    if (deleteUntagged) {
-        await page.locator('#delete_untagged').click();
-    }
+  if (deleteUntagged) {
+      await page.locator('#delete_untagged').click();
+  }
 
-    if (gc_now) {
-        await page.getByRole("button", { name: 'DRY RUN' }).click()
-    } else {
-        await page.getByRole('button', { name: 'GC NOW' }).click();
-    }
+  if (gc_now) {
+      await page.getByRole("button", { name: 'DRY RUN' }).click()
+  } else {
+      await page.getByRole('button', { name: 'GC NOW' }).click();
+  }
 }
 
 test('Project Quota Sorting', async ({ page }) => {
-    await loginAsAdmin(page);
+  await loginAsAdmin(page);
 
-    const timestamp1 = Date.now();
-    const project1 = `project${timestamp1}`;
-    console.log(project1);
-    await createProject(page, project1);
+  const timestamp1 = Date.now();
+  const project1 = `project${timestamp1}`;
+  console.log(project1);
+  await createProject(page, project1);
 
-    const smaller_repo = 'alpine';
-    const smaller_repo_tag = 'latest';
-    const larger_repo = 'photon';
-    const larger_repo_tag = 'latest';
+  const smaller_repo = 'alpine';
+  const smaller_repo_tag = 'latest';
+  const larger_repo = 'photon';
+  const larger_repo_tag = 'latest';
 
-    await pushImage({
-      ip: harborIp,
-      user: harborUser,
-      pwd: harborPassword,
-      project: project1,
-      image: smaller_repo,
-      needPullFirst: true,
-      tag: smaller_repo_tag,
-      tag1: 'latest',
-    });
+  await pushImage({
+    ip: harborIp,
+    user: harborUser,
+    pwd: harborPassword,
+    project: project1,
+    image: smaller_repo,
+    needPullFirst: true,
+    tag: smaller_repo_tag,
+    tag1: 'latest',
+  });
 
-    const timestamp2 = Date.now();
-    const project2 = `project${timestamp2}`;
-    console.log(project2);
-    await createProject(page, project2);
+  const timestamp2 = Date.now();
+  const project2 = `project${timestamp2}`;
+  console.log(project2);
+  await createProject(page, project2);
 
-    await pushImage({
-      ip: harborIp,
-      user: harborUser,
-      pwd: harborPassword,
-      project: project2,
-      image: larger_repo,
-      needPullFirst: true,
-      tag: larger_repo_tag,
-      tag1: 'latest'
-    });
+  await pushImage({
+    ip: harborIp,
+    user: harborUser,
+    pwd: harborPassword,
+    project: project2,
+    image: larger_repo,
+    needPullFirst: true,
+    tag: larger_repo_tag,
+    tag1: 'latest'
+  });
 
-    await switchToProjectQuotas(page);
-    await checkProjectQuotaSorting(page, project1, project2);
+  await switchToProjectQuotas(page);
+  await checkProjectQuotaSorting(page, project1, project2);
 
-    await deleteRepo(page, project1, smaller_repo);
-    await deleteRepo(page, project2, larger_repo);
-    await runGC(page)
+  await deleteRepo(page, project1, smaller_repo);
+  await deleteRepo(page, project2, larger_repo);
+  await runGC(page)
 })
