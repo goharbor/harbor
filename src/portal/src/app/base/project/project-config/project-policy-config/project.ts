355bb773a23f76aa3166eb69cc5052bf3c69d819
stonezdj <stone.zhang@broadcom.com>
2026-01-22 17:10:17 +0800

    Add UI option to enable proxy cache referrer API
    Signed-off-by: stonezdj <stone.zhang@broadcom.com>

--- src/portal/src/app/base/project/project-config/project-policy-config/project.ts
+++ src/portal/src/app/base/project/project-config/project-policy-config/project.ts
@@ -37,6 +37,7 @@ export class Project {
         reuse_sys_cve_allowlist?: string;
         proxy_speed_kb?: number | null;
         max_upstream_conn?: number | null;
+        proxy_referrer_api?: string | boolean;
     };
     cve_allowlist?: object;
     constructor() {
