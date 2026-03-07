81415eea6d22ce2cc762a5ee0b0a54f4677cff37
HARSHVARANDANI <hpvarandani@gmail.com>
2026-03-07 15:52:40 +0530

    added new routes in controller test
    Signed-off-by: HARSHVARANDANI <hpvarandani@gmail.com>

--- src/core/controllers/controllers_test.go
+++ src/core/controllers/controllers_test.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//    http://www.apache.org/licenses/LICENSE-2.0
+//	http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -48,6 +48,8 @@ func init() {
 	web.Router("/c/login", &CommonController{}, "post:Login")
 	web.Router("/c/log_out", &CommonController{}, "get:LogOut")
 	web.Router("/c/userExists", &CommonController{}, "post:UserExists")
+	web.Router("/c/setup/status", &CommonController{}, "get:SetupStatus")
+	web.Router("/c/setup", &CommonController{}, "post:Setup")
 }
 
 func TestMain(m *testing.M) {
