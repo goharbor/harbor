d7615ecf6d2e544bf27fb0c9f2079f04fbb28266
Prasanth Baskar <bupdprasanth@gmail.com>
2026-06-12 15:57:08 +0000

    ci: route docker hub pulls through proxy
    Signed-off-by: Prasanth Baskar <bupdprasanth@gmail.com>

--- tests/pushimage.sh
+++ tests/pushimage.sh
@@ -3,14 +3,14 @@ set -x
 set -e
 
 IP=$(hostname -I | awk '{print $1}')
-docker pull hello-world
-docker pull busybox
+docker pull registry.goharbor.io/dockerhub/library/hello-world:latest
+docker pull registry.goharbor.io/dockerhub/library/busybox:latest
 docker login -u admin -p Harbor12345 $IP:5000  
 
-docker tag hello-world $IP:5000/library/hello-world:latest
+docker tag registry.goharbor.io/dockerhub/library/hello-world:latest $IP:5000/library/hello-world:latest
 docker push $IP:5000/library/hello-world:latest
 echo "$? pushed hello world"
 
-docker tag busybox $IP:5000/library/busybox:latest
+docker tag registry.goharbor.io/dockerhub/library/busybox:latest $IP:5000/library/busybox:latest
 docker push $IP:5000/library/busybox:latest
 echo "$? pushed busybox"
