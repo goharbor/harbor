cd7bf5feb913f8950c8d5478c926497a0f1a6eec
Ross Golder <ross@golder.org>
2026-07-01 13:18:15 +0700

    fix(lint): resolve prettier formatting issues in artifact-additions components
    Signed-off-by: Ross Golder <ross@golder.org>

--- src/portal/src/app/base/project/repository/artifact/artifact-additions/artifact-additions.component.ts
+++ src/portal/src/app/base/project/repository/artifact/artifact-additions/artifact-additions.component.ts
@@ -141,7 +141,10 @@ export class ArtifactAdditionsComponent implements AfterViewChecked, OnInit {
     }
 
     hasBuildHistory(): boolean {
-        return this.additionLinks && !!this.additionLinks[ADDITIONS.BUILD_HISTORY];
+        return (
+            this.additionLinks &&
+            !!this.additionLinks[ADDITIONS.BUILD_HISTORY]
+        );
     }
 
     actionTab(tab: string): void {
