1184f003fd5ba27f777c75e7634626aff22d783f
stonezdj <stone.zhang@broadcom.com>
2026-07-10 14:54:59 +0800

    feat(ui): support advanced proxy cache configurations in UI
    - Support repository filter configuration (pattern and kind) for proxy cache projects.
    - Ensure the local on not found and proxy referrer API checkbox editability is controlled by project configuration update permissions.
    
    Signed-off-by: stonezdj <stone.zhang@broadcom.com>

--- src/portal/src/app/base/project/project.ts
+++ src/portal/src/app/base/project/project.ts
@@ -38,7 +38,9 @@ export class Project {
         bandwidth: number;
         max_upstream_conn: number;
         proxy_cache_local_on_not_found?: string | boolean;
-        proxy_referrer_api: string | boolean;
+        proxy_referrer_api?: string | boolean;
+        proxy_cache_filter_pattern?: string;
+        proxy_cache_filter_kind?: string;
     };
     constructor() {
         this.metadata = <any>{};
@@ -46,5 +48,11 @@ export class Project {
         this.metadata.max_upstream_conn = -1;
         this.metadata.proxy_cache_local_on_not_found = false;
         this.metadata.proxy_referrer_api = false;
+        this.metadata.proxy_cache_filter_pattern = '';
+        this.metadata.proxy_cache_filter_kind =
+            REPOSITORY_FILTER_KIND_DOUBLESTAR;
     }
 }
+
+export const REPOSITORY_FILTER_KIND_REGEX = 'regex';
+export const REPOSITORY_FILTER_KIND_DOUBLESTAR = 'doublestar';
